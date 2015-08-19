aca24e0edb5c259287e274d30024478b9a947732
ysoftman <ysoftman@gmail.com>
2015-08-20 08:52:53 +0900

    로그 수정 날짜 기준으로 삭제하도록 수정.

--- logEraser.go
+++ logEraser.go
@@ -34,36 +34,19 @@ func main() {
 	// 대상 경로의 디렉토리 및 파일 리스트 파악
 	fileinfo, _ := ioutil.ReadDir(targetpath)
 	for _, value := range fileinfo {
-		//		log.Printf("Name(%v), Size(%v), IsDir(%v), Mode(%v), ModTime(%v)\n", value.Name(), value.Size(), value.IsDir(), value.Mode(), value.ModTime())
+		log.Printf("Name(%v), Size(%v), IsDir(%v), Mode(%v), ModTime(%v)\n", value.Name(), value.Size(), value.IsDir(), value.Mode(), value.ModTime())
 
-		strYear := value.Name()[0:4]
-		strMonth := value.Name()[4:6]
-		strDay := value.Name()[6:]
-		//		log.Printf("strYear(%v) strMonth(%v) strDay(%v)", strYear, strMonth, strDay)
-		nYear, err := strconv.Atoi(strYear)
-		if err != nil {
-			log.Printf("Can't read dir year, skip dir(%v)", value.Name())
-			continue
-		}
-		nMonth, err := strconv.Atoi(strMonth)
-		if err != nil {
-			log.Printf("Can't read dir month, skip dir(%v)", value.Name())
-			continue
-		}
-		nDay, err := strconv.Atoi(strDay)
-		if err != nil {
-			log.Printf("Can't read dir day, skip dir(%v)", value.Name())
-			continue
-		}
-		log.Printf("nYear(%v) nMonth(%v) nDay(%v)\n", nYear, nMonth, nDay)
+		//		// 폴더이름 시간을 계산
+		//		dirtime := GetDirNameToDate(value.Name())
+		//		if dirtime == 0 {
+		//			continue;
+		//		}
 
-		dirtime := time.Date(nYear, time.Month(nMonth), nDay, 0, 0, 0, 0, time.Local)
-		log.Println(dirtime)
-		log.Println(dirtime.Unix())
 		// 기간 경과한 대상 경로의 파일(디렉토리) 삭제
 		delpath := ""
-		if dirtime.Unix() < curtime.Unix() && curtime.Unix()-dirtime.Unix() < int64(diffsec) {
-			delpath = targetpath+"/"+value.Name()
+		//		if dirtime.Unix() < curtime.Unix() && curtime.Unix()-dirtime.Unix() < int64(diffsec) {
+		if value.ModTime().Unix() < curtime.Unix() && curtime.Unix()-value.ModTime().Unix() < int64(diffsec) {
+			delpath = targetpath + "/" + value.Name()
 			log.Println("delete directory... %v", delpath)
 			err := os.RemoveAll(delpath)
 			if err != nil {
@@ -73,4 +56,33 @@ func main() {
 
 	}
 
+}
+
+func GetDirNameToDate(dirname string) int64 {
+
+	// 폴더 이름이 날짜인 경우 시간 계산
+	strYear := dirname[0:4]
+	strMonth := dirname[4:6]
+	strDay := dirname[6:]
+	nYear, err := strconv.Atoi(strYear)
+	if err != nil {
+		log.Printf("Can't read dir year, skip dir(%v)", dirname)
+		return 0
+	}
+	nMonth, err := strconv.Atoi(strMonth)
+	if err != nil {
+		log.Printf("Can't read dir month, skip dir(%v)", dirname)
+		return 0
+	}
+	nDay, err := strconv.Atoi(strDay)
+	if err != nil {
+		log.Printf("Can't read dir day, skip dir(%v)", dirname)
+		return 0
+	}
+	log.Printf("nYear(%v) nMonth(%v) nDay(%v)\n", nYear, nMonth, nDay)
+	dirtime := time.Date(nYear, time.Month(nMonth), nDay, 0, 0, 0, 0, time.Local)
+	log.Println(dirtime)
+	log.Println(dirtime.Unix())
+
+	return dirtime.Unix()
 }
