04928b1dea27b06114f75543b4f37fbfae565d8d
ysoftman <ysoftman@gmail.com>
2016-02-04 10:01:26 +0900

    시간 측정 기능 추가.

--- infiniteWriter.go
+++ infiniteWriter.go
@@ -1,12 +1,15 @@
 // author : ysoftman
 // encoding : utf-8
 // title : Infinite Writer
-// desc : 디스크(USB)에 무한 쓰기 작업을 수행하는 스트레스 테스터 프로그램
+// desc :
+// 디스크(USB)에 무한 쓰기 작업을 수행하는 스트레스 테스터 프로그램
+// 성능(시간) 측정 추가
 package main
 
 import "fmt"
 import "os"
 import "strconv"
+import "time"
 
 func main() {
 	fmt.Println("[Usage] " + os.Args[0] + " limit-MBytes loop-cnt")
@@ -38,6 +41,8 @@ func main() {
 	limit *= 1000 * 1000
 	data := make([]byte, limit)
 
+	startSec := time.Now().UnixNano()
+
 	// 반복해서 파일 생성(다시쓰기)
 	for i := 1; i <= loopcnt; i++ {
 		fmt.Printf("%d/%d\n", i, loopcnt)
@@ -52,5 +57,19 @@ func main() {
 		fpDummy.Close()
 
 	}
+
+	endSec := time.Now().UnixNano()
+
+	// 경과시간 계산
+	elapsed := endSec - startSec
+	// microsec 단위로
+	elapsed /= 1000
+	// millisec 단위로
+	elapsed /= 1000
+	// sec 단위로
+	// 나머지 ms 계산
+	remainder := elapsed % 1000
+	elapsed /= 1000
+	fmt.Printf("Elapsed: %d.%d sec", elapsed, remainder)
 
 }
