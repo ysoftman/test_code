f64c25001b297b04100100670a8467db38f6eeaa
ysoftman <ysoftman@gmail.com>
2016-02-04 19:01:38 +0900

    fmt 적용(tab 으로 indent)

--- GoLangTest/interface/interface.go
+++ GoLangTest/interface/interface.go
@@ -21,26 +21,26 @@ type Calc interface {
 // calculate 구현
 func (f foo1) calculate(a, b int) int {
 	a += b
-    return a
+	return a
 }
 
 // foo2 타입을 받는(메소드 리시버)형 함수를 만든다.
 // calculate 구현
 func (f foo2) calculate(a, b int) int {
 	a *= b
-    return a
+	return a
 }
 
 func main() {
 	fmt.Println("go interface test.")
-    var f1 Calc
-    var f2 Calc
-    
-    f1 = foo1{}
-    f2 = foo2{}
+	var f1 Calc
+	var f2 Calc
 
-    // 각 타입에 맞는 함수(인터페이스 구현)를 수행
-    fmt.Println("foo1 calculate result:", f1.calculate(2,3))
-    fmt.Println("foo2 calculate result:", f2.calculate(2,3))
-    
+	f1 = foo1{}
+	f2 = foo2{}
+
+	// 각 타입에 맞는 함수(인터페이스 구현)를 수행
+	fmt.Println("foo1 calculate result:", f1.calculate(2, 3))
+	fmt.Println("foo2 calculate result:", f2.calculate(2, 3))
+
 }
