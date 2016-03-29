e5c017390894ae5d0ec8b97e4d4e3474e6924ce7
ysoftman <ysoftman@gmail.com>
2016-03-29 13:22:20 +0900

    array_test.cpp 수정.

--- CPPTest/array_test/array_test.cpp
+++ CPPTest/array_test/array_test.cpp
@@ -12,6 +12,14 @@ int modifyarry(int arValue[])
 	return 0;
 }
 
+// 배열은 포인터의 일종으로 * 포인터형으로 받아됨
+int modifyarry2(int *arValue)
+{
+	arValue[0] = 7;
+	arValue[1] = 99;
+	return 0;
+}
+
 void main()
 {
 	int arr[3];
@@ -29,6 +37,14 @@ void main()
 	modifyarry(arr);
 
 	printf("after modifyarry(arr)\n");
+
+	for (int i=0;i<3;i++) {
+		printf("arr[%d]: %d\n", i, arr[i]);
+	}
+
+	modifyarry2(arr);
+
+	printf("after modifyarry2(arr)\n");
 
 	for (int i=0;i<3;i++) {
 		printf("arr[%d]: %d\n", i, arr[i]);
