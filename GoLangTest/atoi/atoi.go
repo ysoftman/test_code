da1406541a0ff7d6acd637e8852ef83d4581d02d
ysoftman <ysoftman@gmail.com>
2016-04-21 17:33:59 +0900

    atoi.go 추가. strlen.go 추가.

---
+++ GoLangTest/atoi/atoi.go
@@ -0,0 +1,26 @@
+// ysoftman
+// atoi 구현해보기
+package main
+
+import "fmt"
+
+func main() {
+
+	str := "1234567"
+	n := atoi(str)
+	fmt.Printf("str=%s, n:=%d", str, n)
+}
+
+func atoi(str string) (result int) {
+	result = 0
+	fmt.Println("Atoi()")
+
+	for i := range str {
+		result *= 10
+		// result += int(str[i]) - 48
+		result += int(str[i]) - '0'
+		// fmt.Println(str[i], result)
+	}
+
+	return result
+}
---
+++ GoLangTest/strlen/strlen.go
@@ -0,0 +1,25 @@
+// ysoftman
+// strlen 구현해보기
+package main
+
+import "fmt"
+
+func main() {
+	str := "my nickname is ysoftman"
+
+	fmt.Printf("len(%s)=%d\n", str, len(str))
+	fmt.Printf("strlen(%s)=%d\n", str, strlen(str))
+}
+
+func strlen(str string) int {
+	// fmt.Println("strlen()")
+	cnt := 0
+	// for i := range str {
+	for i := 0; i < len(str); i++ {
+		// fmt.Println(str[i])
+		cnt++
+	}
+
+	return cnt
+
+}
