0ce0aabac4e6dab6759b0f4d94a5564da75e7a70
ysoftman <ysoftman@gmail.com>
2016-04-29 15:04:10 +0900

    hashlib_test.py 추가.

---
+++ Python3Test/hashlib_test.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# author: ysoftman
+# python version : 3.x
+# desc : hash 생성 테스트
+
+# MD5, SHA1, SHA224, SHA256, SHA384, SHA51
+import hashlib
+
+data = b"ysoftman"
+
+# md5 으로 해싱
+md5 = hashlib.md5()
+md5.update(data)
+print ("md5 = %s" % md5.hexdigest())
+
+# sha256 으로 해싱
+sha256 = hashlib.sha256()
+sha256.update(data)
+print ("sha256 = %s" % sha256.hexdigest())
+
+# sha512 으로 해싱
+sha512 = hashlib.sha512()
+sha512.update(data)
+print ("sha512 = %s" % sha512.hexdigest())
+
