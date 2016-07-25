2a60e252eda0878747c7d33dfe5066ca1d8e7da6
bill.2714 <bill.2714@kakaocorp.com>
2016-07-25 16:48:36 +0900

    사용자 함수 테스트 스크립트.

---
+++ sh/function_test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# ysoftman
+# 사용자 함수 테스트
+
+if [ $# != 2 ]; then
+    echo "argc != 2"
+    exit 1
+fi
+
+main_arg1=$1
+main_arg2=$2
+echo "main_arg1 =" ${main_arg1}
+echo "main_arg2 =" ${main_arg2}
+
+# 사용자 함수 파마리터도 $# $1 $2 .. 으로 받는다.
+testfunc1() {
+    if [ $# != 2 ]; then
+        echo "func_argc($#) != 2"
+        exit 1
+    fi
+    func_arg1=$1
+    func_arg2=$2
+
+    echo "func_arg1 =" ${func_arg1}
+    echo "func_arg2 =" ${func_arg2}
+}
+
+# 함수 호출
+testfunc1 ${main_arg1} ${main_arg2}
