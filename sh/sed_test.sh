302a1179279cce278b723a410b5aafafb46fd8bd
bill.2714 <bill.2714@kakaocorp.com>
2016-07-29 11:04:00 +0900

    find sed grep 테스트 스크립트 추가.

---
+++ sh/sed_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# ysoftman
+# 하위 디렉토리(파일을 제외)만 출력하기
+# ls -lR ./ ysoftman : ./ysoftman 디렉토리에서 -l : 리스트, -R : 하위 디렉토리
+# | grep ":$" : 앞의 출력 결과를 파이프로 넘겨받아 : 로 끝나는 부분만 출력
+# | sed 's/:/\//' : 앞의 출력 결과를 파이프로 넘겨받아 처음 부분을 [subdir] 로 바꿔서(추가) 출력
+ls -lR .. | grep ":$" | sed "s/^/[subdir]/"
+
+
+# stringdata.txt 내용에서 ysoftman 찾으면 출력 끝내기
+cat stringdata.txt | sed "/ysoftman/q"
+
