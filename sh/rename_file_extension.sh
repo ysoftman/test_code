7e3dec126f6a91b079aa53ee4c7ee857878b1740
ysoftman <ysoftman@gmail.com>
2016-08-08 13:29:44 +0900

    주석 수정.

--- sh/rename_file_extension.sh
+++ sh/rename_file_extension.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# rename 설치
+# rename 설치(맥의 경우)
 # sudo  brew install rename
 
-# 현재 디렉토리내의 모든.txt 확장자 파일을 .log 로 바꾸기 
+# 현재 디렉토리내의 모든.txt 확장자 파일을 .log 로 바꾸기(정규 표현식 사용하는 경우)
 rename 's/\.txt/\.log/' *.txt
-
-
