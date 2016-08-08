61de6797e82ff26c63c29dfd024fa0975afd65bd
ysoftman <ysoftman@gmail.com>
2016-08-08 11:35:49 +0900

    테스트 케이스 추가 및 주석 수정.

--- CPPTest/const_test/const_test.cpp
+++ CPPTest/const_test/const_test.cpp
@@ -12,22 +12,42 @@ int main()
 
     int num = 10;
     int *pNormal = NULL;
-    const int *pConst = NULL;
+    const int *pConst = NULL;   // pConst 값을 변경하지 못함
+    int *const pConst2 = &num;  // pConst2 주소를 변경하지 못함
+
+    printf("&num:%p\n", &num);
+    printf("num:%d\n", num);
 
     pNormal = &num;
-    printf("pNormal:%d\n", *pNormal);
-    
-    pConst = &num;
-    printf("pConst:%d\n", *pConst);
+    // pNormal 이 가르키는 num 변수 주소값
+    printf("&(pNormal->num):%p\n", pNormal);
+    // pNormal 주소 값 출력
+    printf("&(pNormal):%p\n", &pNormal);
+    // pNormal 이 가르키븐 num 변수 주소값에 저장되어 있는 값
+    printf("*(pNormal):%d\n", *pNormal);
 
-    // 일반 포인터 변수의 값은 변경가능하다.
+    // 일반 포인터 변수의 값은 변경 가능하다.
     *pNormal = 20;
-    printf("pNormal:%d\n", *pNormal);
+    printf("*(pNormal):%d\n", *pNormal);
 
-    // Const 포인터 변수의 값은 변경할 수 없다.
-    // 하지만 pNormal 이 num 의 값을 변경해서 똑같은 값이 표시된다.
-    // *pConst = 20;
-    printf("pConst:%d\n", *pConst);
     
+    printf("&(pConst):%p\n", pConst);
+    pConst = &num;
+    // pConst 포인터 변수의 값은 변경할 수 없다.
+    // 하지만 pNormal 이 num 의 값을 변경해서 20 값이 표시된다.
+    // *pConst = 30;
+    printf("&(pConst):%p\n", pConst);
+    printf("*(pConst):%d\n", *pConst);
+    
+
+    // pConst2 포인터 변수를 변경할 수 없다.
+    // pConst2 = &num;
+    // pConst2 포인터 변수의 값은 변경할 수 있다.
+    // pConst2 선언시 NULL 이 아닌값으로 초기화되어야 한다.
+    *pConst2 = 30;
+    printf("*(pConst2):%d\n", *pConst2);
+    // pConst2 가 num2 변수의 값을 변경하였다.
+    printf("num:%d\n", num);
+
     return 0;
 }
