e6cc5178bff2eb1f16760d092664498565de489e
ysoftman <ysoftman@gmail.com>
2016-08-29 11:17:18 +0900

    마스킹 로직 테스트.

---
+++ CPPTest/masking_test/masking_test.cpp
@@ -0,0 +1,35 @@
+// ysoftman
+// masking test
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    // case1
+    printf("### case1\n");
+    char szValue1[50] = "ysoftman_123+asdf";
+    printf("szValue1=%s\n", szValue1);   
+    char *p = szValue1;
+    // a-z, 0-9 인경우 X 로 변환(마스킹)하기
+    // 특수문자 전까지만 마스킹 된다.
+    while (isalnum(*p))
+    {
+        *p = 'X';
+        p++;        
+    }
+    printf("szValue1=%s\n", szValue1);
+
+    // case2
+    printf("### case2\n");
+    //char szValue2[50] = "ysoftman_123+asdf&";
+    // &로 끝나지 않은 경우
+    char szValue2[50] = "ysoftman_123+asdf";
+    p = szValue2;
+    while (*p != '\0' && *p != '&')
+    {
+        // printf("%d\n", *p);
+        *p = 'X';
+        p++;
+    }
+    printf("szValue2=%s\n", szValue2);
+}
