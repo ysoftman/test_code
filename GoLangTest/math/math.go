2f61d0e93ac477900551e5931f5af64c3dd9d69a
ysoftman <ysoftman@gmail.com>
2016-08-30 12:13:50 +0900

    math 테스트 부분 분리, zzz -> parsing 이름 변경.

---
+++ GoLangTest/math/math.go
@@ -0,0 +1,19 @@
+// ysoftman
+// math 테스트
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("math.Cos(90.0)=", math.Cos(90.0))
+	fmt.Println("math.Sqrt(4)=", math.Sqrt(4))
+	fmt.Println("math.MaxFloat64=", math.MaxFloat64)
+	fmt.Println("math.MaxFloat32=", math.MaxFloat32)
+	fmt.Println("math.Ceil(10.5)=", math.Ceil(10.5))
+	fmt.Println("math.Floor(10.5)=", math.Floor(10.5))
+	fmt.Println("15/10=", 15/10)
+	fmt.Println("float32(15)/float32(10)=", float32(15)/float32(10))
+}
