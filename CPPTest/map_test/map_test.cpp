bd7a6deca23801a55842125c21f0fefb9f6858ed
ysoftman <ysoftman@gmail.com>
2016-08-30 12:27:29 +0900

    and_or  테스트 부분 분리. zzz -> map_test 로 이름 변경.

--- CPPTest/map_test/map_test.cpp
+++ CPPTest/map_test/map_test.cpp
@@ -1,14 +1,12 @@
 /* 
 author: ysoftman
-title: C/CPP zzz
-desc: C/CPP 관련 이것저것 테스트하기 위한 코드
+title: map 관련 테스트
 */
 #include <stdio.h>
 #include <iostream>
 #include <map>
 
 using namespace std;
-
 
 struct aa {
 	int a;
@@ -33,25 +31,5 @@ int main()
 	{
 		printf("mymap[%d] = %d\n", (*iter).first, (*iter).second);
 	}
-
-
-
-	// visual c++ 에서는 and or not xor 키워드 사용 못함
-	if (a > 5 and a < 20)
-	{
-		printf("ok1\n");
-	}
-	if (a == 10 or a == 20)
-	{
-		printf("ok2\n");
-	}
-
-
-
-
-
 	return 0;
 }
-
-
-
