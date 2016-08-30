21de7993f2bcd0e4301364f1c6c58e5a74a70582
ysoftman <ysoftman@gmail.com>
2016-08-30 12:08:10 +0900

    node 테스트 부분 분리.

--- GoLangTest/zzz/zzz.go
+++ GoLangTest/zzz/zzz.go
@@ -8,8 +8,6 @@ import "strings"
 
 import "math"
 
-import "os"
-
 // InfoData : defibrillator 정보
 type InfoData *struct {
 	ID       int
@@ -76,50 +74,5 @@ func main() {
 	fmt.Println("15/10=", 15/10)
 	fmt.Println("float32(15)/float32(10)=", float32(15)/float32(10))
 
-	// 노드 및 링크 정보를 저장
-	var node map[int]nodeinfo
-	node = make(map[int]nodeinfo)
-	// 노드에 링크 정보 추가
-	node[3] = nodeinfo{
-		"node",
-		[]int{1, 2},
-	}
-	node[3] = nodeinfo{
-		"node",
-		append(node[3].node, 4),
-	}
-	node[1] = nodeinfo{
-		"node",
-		[]int{3},
-	}
-	node[2] = nodeinfo{
-		"node",
-		[]int{3},
-	}
-	node[4] = nodeinfo{
-		"node",
-		[]int{3},
-	}
-	fmt.Println(node)
-	fmt.Println(node[3])
-	fmt.Println(node[1])
-	fmt.Println(node[2])
-	fmt.Println(node[4])
-
-	// map struct 포인터타입으로 사용
-	var node2 map[int]*nodeinfo
-	node2 = make(map[int]*nodeinfo)
-	node2[3] = &nodeinfo{}
-	node2[3].name = "aaa"
-	node2[3].node = []int{1, 2}
-	fmt.Fprintln(os.Stderr, "node2", node2)
-	// fmt.Println("node2", node2)
-	fmt.Fprintln(os.Stderr, "node2[3]", node2[3])
-	// fmt.Println("node2[3]", node2[3])
-
 }
 
-type nodeinfo struct {
-	name string
-	node []int
-}
