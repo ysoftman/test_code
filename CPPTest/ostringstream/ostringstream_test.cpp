4894d8138e1e5a869d146d37e73752f7db04e200
bill.2714 <bill.2714@kakaocorp.com>
2016-09-12 14:53:07 +0900

    ostringstream 관련 테스트.

---
+++ CPPTest/ostringstream/ostringstream_test.cpp
@@ -0,0 +1,24 @@
+// ysoftman
+// ostringstream test
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+
+using namespace std;
+int main()
+{   
+    double a = 0.0000000000000123456789;
+
+    ostringstream ss;
+    ss << "a:" << a;
+    string str = ss.str();    
+    cout << str << endl;
+
+    double b = 0.12345678912345678912346;
+    ostringstream ss2;
+    ss2 << "b:" << fixed << b;
+    string str2 = ss2.str();    
+    cout << str2 << endl;
+    
+    return 0;
+}
