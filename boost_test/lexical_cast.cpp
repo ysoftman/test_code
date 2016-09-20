e29b7b90c5cb1bbd0d797a202dc6eb94e3bae03c
bill.2714 <bill.2714@kakaocorp.com>
2016-09-20 17:40:50 +0900

    lexical_cast 예시 추가. split.cpp 파일이름 변경.

---
+++ boost_test/lexical_cast.cpp
@@ -0,0 +1,26 @@
+// ysoftman
+// boost lexical_cast 사용하기
+#include <iostream>
+#include <vector>
+#include <string>
+#include <boost/lexical_cast.hpp>
+
+using namespace std;
+
+int main()
+{
+    cout << "lexical_cat test" << endl;
+    int a = 10;
+    string str = "num=";
+    // int -> string 으로 변환
+    str += boost::lexical_cast<string>(a);
+    cout << str << endl;
+
+
+    string num = "12345";
+    // string -> int 으로 변환
+    int b = boost::lexical_cast<int>(num);
+    cout << b << endl;
+
+    return 0;
+}
