745d1078dee3dde401477009e08a46902389b6d8
ysoftman <ysoftman@gmail.com>
2016-10-13 18:22:15 +0900

    구구단

---
+++ pythontest/gugudan.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# author: ysoftman
+# python version : 2.x
+# desc : gugudan 
+
+if __name__ == '__main__':
+    # 구구단 출력
+    for x in range(2,10):
+        for y in range(2,10):
+            print x, " * ", y , " = ", x*y
+        print ""
