db7728ff3a7866cc57b44c0f9732c61734d6f3fa
ysoftman <ysoftman@gmail.com>
2016-10-31 17:22:49 +0900

    slack api test

---
+++ GoLangTest/slack_api/slack_api.go
@@ -0,0 +1,25 @@
+// author : ysoftman
+// encoding : utf-8
+// title : slack api 테스트
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/nlopes/slack"
+)
+
+func main() {
+	fmt.Println("slack api test")
+	slackapi := slack.New("xoxp-3061993574-55243284818-92053298579-c89480262eead7a26906ef3eb38befda")
+	groups, err := slackapi.GetGroups(false)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+	for _, g := range groups {
+		fmt.Printf("group_id: %s group_name: %s\n", g.ID, g.Name)
+	}
+
+}
