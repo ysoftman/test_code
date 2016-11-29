41cc04e02b5cb3f9e05f4e18a8daf1c2b3bf0f88
ysoftman <ysoftman@gmail.com>
2016-11-29 15:43:40 +0900

    스트링 수정

--- sh/export_test.sh
+++ sh/export_test.sh
@@ -7,5 +7,5 @@ unset ysoftman2
 # sh export_test.sh 로 실행하면 export_test.sh 쉘에만 export 가 반영된다.
 # source export_test.sh 로 실행해야 현재 쉘에 export 내용이 반영된다.
 export ysoftmanvar1='ysoftmanvar1'
-export ysoftmanvar2='ysoftamnvar2'
+export ysoftmanvar2='ysoftmanvar2'
 
