961eca4cbfc1555d780d5c3fad84e629604750c3
ysoftman <ysoftman@gmail.com>
2016-12-02 12:29:04 +0900

    파일이름변경 및 테스트 예제 추가

--- pythontest/command_test.py
+++ pythontest/command_test.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # author: ysoftman
 # python version : 2.x
-# desc : os system test
+# desc : 쉘 명령어르 실행하고 결과 받아오기 테스트
 import os
 
-# os.system() 로 명령 실행
+# 쉘 명령 실행
 print '----- test1 -----'
 # 종료코드 리턴
 ret = os.system("ls")
@@ -17,3 +17,12 @@ print 'exitcode=' + str(ret)
 print '----- test3 -----'
 ret = os.system("echo 'ysoftman'")
 print 'exitcode=' + str(ret)
+
+# 쉘 명령 출력 결과 얻기
+print '----- test4 -----'
+output = os.popen('find . -name "*.py"').read()
+print output
+# 라인구분으로 리스트로 만들기
+linelist = output.split('\n')
+for line in linelist: 
+    print line + " --> ok."
