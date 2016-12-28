8c56cc5897393fdab5a4d89ba2ae62a9071ba55c
ysoftman <ysoftman@gmail.com>
2016-12-28 15:39:15 +0900

    echo -> printf 로 변경

--- sh/compare_version.sh
+++ sh/compare_version.sh
@@ -5,7 +5,7 @@ version2="5.0.1"
 echo 'version1='${version1}
 echo 'version2='${version2}
 # 내림 차순 정렬하여 version2 가 vesrion1 보다 높은지 파악한다.
-highest_version="$(echo "${version1}\n${version2}" | sort -r | head -n1)"
+highest_version="$(printf "${version1}\n${version2}" | sort -r | head -n1)"
 echo 'highest_version='${highest_version}
 if [ "${highest_version}"  == "${version1}" ]; then
     echo "${version1} > ${version2}"
