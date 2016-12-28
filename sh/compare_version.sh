ac9f96b924a717bbd067ef3273772e6790574818
ysoftman <ysoftman@gmail.com>
2016-12-28 15:23:41 +0900

    주석 수정

--- sh/compare_version.sh
+++ sh/compare_version.sh
@@ -4,7 +4,7 @@ version1="4.9.2"
 version2="5.0.1"
 echo 'version1='${version1}
 echo 'version2='${version2}
-# 내리 차순 정렬하여 version2 가 vesrion1 보다 높은지 파악한다.
+# 내림 차순 정렬하여 version2 가 vesrion1 보다 높은지 파악한다.
 highest_version="$(echo "${version1}\n${version2}" | sort -r | head -n1)"
 echo 'highest_version='${highest_version}
 if [ "${highest_version}"  == "${version1}" ]; then
