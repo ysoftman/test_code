b5d565368697bacc1d1f3da140fc9af3658852bb
ysoftman <ysoftman@gmail.com>
2017-03-24 20:20:46 +0900

    update

--- CPPTest/write_file_test/write_file_test.cpp
+++ CPPTest/write_file_test/write_file_test.cpp
@@ -1,5 +1,9 @@
 // ysoftman
 // write 동시에 파일 쓰기 테스트
+// 테스트 해보기
+// g++ write_file_test.cpp
+// rm yoon.txt
+// ./a.out 1 & ./a.out 2 & ./a.out 3
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -17,23 +21,27 @@ int main(int argc, char **argv)
     fprintf(stdout, "max pipe size: %d\n", PIPE_BUF);
     fprintf(stdout, "max pipe size: %d\n", F_GETPIPE_SZ);
 
-    return 1;
-    int fd = open("yoon.txt", O_WRONLY | O_CREAT | O_EXCL, 0644);
-    //fcntl(fd, F_SETPIPE_SZ, 1024*1024);
+	// 기존 함수에 추가
+    int fd = open("yoon.txt", O_WRONLY | O_CREAT | O_APPEND, 0644);
+    
+    // fcntl(fd, F_SETPIPE_SZ, 1024*1024);
 
     fprintf(stdout, "Theoretical max size: %ld\n", F_GETPIPE_SZ);
 
-    char temp[1024*1024] = {0, };
-    for (int i=0; i<1024*1024; ++i)
+    char temp[100000] = {0, };
+	int i = 0;
+    for (i=0; i<99999; ++i)
     {
         temp[i] = *argv[1];
     }
+	temp[99999] = '\n';
 
-    for (int i=0; i<10; ++i)
+    for (i=0; i<5; ++i)
     {
-        write(fd, temp, 1024*1024);
+        write(fd, temp, 100000);
     }
     close(fd);
 
     return 0;
 }
+
