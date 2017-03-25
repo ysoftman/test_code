6fccfd3287c6e328439a92f23bee08a71ee94d59
ysoftman <ysoftman@gmail.com>
2017-03-25 13:12:20 +0900

    파이프 내용 파일로 쓰기

--- CPPTest/pipe_test/pipe_test.cpp
+++ CPPTest/pipe_test/pipe_test.cpp
@@ -1,3 +1,4 @@
+
 // ysoftman
 // pipe 사용 테스트
 // pipe 는 커널에 생성되며 파일 디스크립터로 표현된다.
@@ -14,8 +15,8 @@ int main()
 {
     // pipe() 시스템 호출로 파이프 생성
     // pipe() 는 2개의 정수 배열을 파라미터로 받는다.
-    // fd[0] => read 용 파이프
-    // fd[1] => write 용 파이프
+    // fd[0] => 파이프에서서 read 용
+    // fd[1] => 파이프에 write 용
     int fd[2];
     //fd[1] = open("yoon.txt", O_WRONLY | O_CREAT | O_APPEND, 0644);
     if (pipe(fd) == -1)
@@ -24,27 +25,42 @@ int main()
         return -1;
     }
 
-
-    fprintf(stdout, "1.F_GETPIPE_SZ size: %d\n", fcntl(fd[1], F_GETPIPE_SZ));
+    // fd[0], f[1] 은 파이프 하나에 대한 read/write 용도로 같은 크기를 가진다.
+    fprintf(stdout, "fd[0] F_GETPIPE_SZ size: %d\n", fcntl(fd[0], F_GETPIPE_SZ));
+    fprintf(stdout, "fd[1] F_GETPIPE_SZ size: %d\n", fcntl(fd[1], F_GETPIPE_SZ));
 
     // fcntl 로 F_SETPIPE_SZ 의 크기를 늘려 주지 않으면 64k(65535)까지만 atomic 쓰기가 가능하다.
     // F_SETPIPE_SZ -> Linux 2.6.35 부터 사용 가능하다.
     // F_SETPIPE_SZ 크기는 설정한 값보다 크게 설정될 수 있다. 65536 의 2,4,8,16 배로 버퍼가 설정된다.
     // 최대 제한 크기 /proc/sys/fs/pipe-max-size 에 명시되어 있다.
-    fcntl(fd[1], F_SETPIPE_SZ, MAX_SIZE);
+    fcntl(fd[0], F_SETPIPE_SZ, MAX_SIZE);
 
-    char c = 'c';
-    for (int i = 1; i < MAX_SIZE; ++i)
+    fprintf(stdout, "fd[0] F_GETPIPE_SZ size: %d\n", fcntl(fd[0], F_GETPIPE_SZ));
+    fprintf(stdout, "fd[1] F_GETPIPE_SZ size: %d\n", fcntl(fd[1], F_GETPIPE_SZ));
+
+
+    int fdes = open("yoon.txt", O_WRONLY | O_CREAT | O_APPEND, 0644);
+
+    char LF = '\n';
+    char w = 'c';
+    char r[MAX_SIZE] = {0,};
+    for (int i = 0; i < MAX_SIZE-1; ++i)
     {
-        write(fd[1], &c, 1);
-        fprintf(stderr, "%d bytes write\n", i);
+        // 파이프에 쓰기
+        write(fd[1], &w, 1);
+        fprintf(stdout, "%d bytes write\n", i);
     }
+    write(fd[1], &LF, 1);
 
-    fprintf(stdout, "2.F_GETPIPE_SZ size: %d\n", fcntl(fd[1], F_GETPIPE_SZ));
+    // 파이프 내용 모두 읽어 오기
+    read(fd[0], &r, MAX_SIZE);
+    // 파일로 쓰기
+    write(fdes, &r, MAX_SIZE);
+
+    close(fdes);
 
     close(fd[0]);
     close(fd[1]);
-
 
     return 0;
 }
