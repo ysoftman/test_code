da6a8c921d25750f21d8b660a7ac8205b116c0f8
ysoftman <ysoftman@gmail.com>
2017-03-29 13:32:03 +0900

    -k 옵션으로 수정

--- sh/du_sort.sh
+++ sh/du_sort.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # ysoftman
 # 현재 디렉토리으 모든 파일에서 가장 큰 10개 출력하기
-# du -a(all) -b(bytes)
+# du -a(all) -k(kbyte) 리눅스에서는 -b(bytes) 도 사용가능
 # sort -n(number) -r(reverse)
 # head -10(lines)
-du -ab | sort -nr | head -10
+du -ak | sort -nr | head -10
 
