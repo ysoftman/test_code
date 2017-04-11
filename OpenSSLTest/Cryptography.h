8630e02a6c758ad4ca762b4068eefb04dd4fab4b
vagrant <vagrant@localhost.localdomain>
2017-04-11 15:28:08 +0000

    주석 수정, build.sh 추가

--- OpenSSLTest/Cryptography.h
+++ OpenSSLTest/Cryptography.h
@@ -1,49 +1,28 @@
-/*
-############################
-제  목 : OpenSSL 빌드
-작성자 : 윤병훈
-############################
-[윈도우 빌드]
-1. openssl-1.0.1c.tar.gz 압축 해제
-
-2. cd openssl-1.0.1c
-
-3. 콘솔창에서 VC++ 사용할 수 있도록 환경 설정
-   "c:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
-
-4. perl 로 설정파일 만들기(perl 설치파일 http://downloads.activestate.com/ActivePerl/releases/)
-   win32 경우 ==> perl Configure VC-WIN32 --prefix=C:\OpenSSL1.0.1
-   win64 경우 ==> perl Configure VC-WIN64A --prefix=C:\OpenSSL1.0.1
-
-5. 컴파일 환경 설정 배치 실행
-   win32 경우 ==> ms\do_ms.bat
-   win64 경우 ==> ms\do_win64a.bat
-
-6. 빌드
-   .lib 빌드 ==> nmake -f ms\nt.mak install
-   .dll 빌드 ==> nmake -f ms\ntdll.mak install
-
-7. 결과 확인
-   include 파일 ==> C:\OpenSSL1.0.1\include\openssl
-   .lib(.dll) 파일 ==> C:\OpenSSL1.0.1\lib
-
-[linux build]
-wget https://www.openssl.org/source/openssl-1.0.2k.tar.gz
-tar zxvf openssl-1.0.2k.tar.gz
-cd openssl-1.0.2k
-# linux
-./config --prefix=/home/ysoftman/workspace/test_code/OpenSSLTest/openssl-1.0.2 -fPIC
-# mac 
-./Configure darwin64-x86_64-cc --prefix=/Users/ysoftman/workspace/test_code/OpenSSLTest/openssl-1.0.2 -fPIC
-make && make install
-*/
-
 ////////////////////////////////////////////////////////////////////////////////////
 // ysoftman
-// OpenSSL 테스트
-// 앞서 빌드된 경로에서 include 와 lib 사용
-// 프로젝트 설정에서 추가 포함디렉토리에 현재 디렉토리(.) 추가
-// rm a.out *.o core*; g++ -g -fPIC -c Cryptography.cpp OpenSSLTest.cpp -I./openssl-1.0.2/include && g++ -shared -L./openssl-1.0.2/lib -lcrypto -lssl Cryptography.o OpenSSLTest.o
+// windows build
+// openssl-1.0.1c.tar.gz 압축 해제
+// cd openssl-1.0.1c
+// 콘솔창에서 VC++ 사용할 수 있도록 환경 설정
+// "c:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
+//
+// perl 로 설정파일 만들기(perl 설치파일 http://downloads.activestate.com/ActivePerl/releases/)
+// win32 경우 ==> perl Configure VC-WIN32 --prefix=C:\OpenSSL1.0.1
+// win64 경우 ==> perl Configure VC-WIN64A --prefix=C:\OpenSSL1.0.1
+//
+// 컴파일 환경 설정 배치 실행
+// win32 경우 ==> ms\do_ms.bat
+// win64 경우 ==> ms\do_win64a.bat
+//
+// 빌드
+// .lib 빌드 ==> nmake -f ms\nt.mak install
+// .dll 빌드 ==> nmake -f ms\ntdll.mak install
+//
+// 결과 확인
+// include 파일 ==> C:\OpenSSL1.0.1\include\openssl
+// .lib(.dll) 파일 ==> C:\OpenSSL1.0.1\lib
+// 
+// linux build -> build.sh 참고
 ////////////////////////////////////////////////////////////////////////////////////
 #include <iostream>
 #include <string>
