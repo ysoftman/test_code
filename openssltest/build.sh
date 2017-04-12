0d946bfac33fe9a8d5fd83451b1f4fd79b816a38
ysoftman <ysoftman@gmail.com>
2017-04-12 14:46:29 +0900

    update build.sh

--- openssltest/build.sh
+++ openssltest/build.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-rm -rfv openssl-1.0.2k.tar.gz openssl-1.0.2 openssl-1.0.2k
-wget https://www.openssl.org/source/openssl-1.0.2k.tar.gz
+rm -rfv openssl-1.0.2 openssl-1.0.2k
+if [ -f 'openssl-1.0.2k.tar.gz' ]; then
+    echo 'ok'
+else
+    echo 'download'
+    wget https://www.openssl.org/source/openssl-1.0.2k.tar.gz
+fi
 tar zxvf openssl-1.0.2k.tar.gz
 cd openssl-1.0.2k
 if [ $(uname) == 'Linux' ]; then
     install_path='/home/ysoftman/workspace/test_code/openssltest/openssl-1.0.2'
-    ./config --prefix=${install_path} -fPIC
+    ./config --prefix=${install_path}
 elif [ $(uname) == 'Darwin' ]; then
     install_path='/Users/ysoftman/workspace/test_code/openssltest/openssl-1.0.2'
-    ./configure darwin64-x86_64-cc --prefix=${install_path}
+    ./Configure darwin64-x86_64-cc --prefix=${install_path}
 fi
 make clean
 make -j4 && make install
