09cb274b04cf9f16efe036a01ce7ae109db5a742
ysoftman <ysoftman@gmail.com>
2017-04-12 10:39:43 +0900

    파일명 변경에 따른 수정

--- openssltest/build.sh
+++ openssltest/build.sh
@@ -2,10 +2,13 @@
 # wget https://www.openssl.org/source/openssl-1.0.2k.tar.gz
 # tar zxvf openssl-1.0.2k.tar.gz
 # cd openssl-1.0.2k
-# ./config --prefix=/home/ysoftman/workspace/test_code/OpenSSLTest/openssl-1.0.2
+# linux
+# ./config --prefix=/Users/ysoftman/workspace/test_code/openssltest/openssl-1.0.2
+# mac
+# ./configure darwin64-x86_64-cc --prefix=/Users/ysoftman/workspace/test_code/openssltest/openssl-1.0.2
+# make clean
 # make && make install
 
 rm a.out *.o core*
-g++ -g -fPIC -c Cryptography.cpp OpenSSLTest.cpp -I./openssl-1.0.2/include
-g++ -shared -L./openssl-1.0.2/lib -lcrypto -lssl Cryptography.o OpenSSLTest.o
-
+g++ -g -fPIC -c cryptography.cpp test_cryptography.cpp -I./openssl-1.0.2/include
+g++ -shared -L./openssl-1.0.2/lib -lcrypto -lssl cryptography.o test_cryptography.o
