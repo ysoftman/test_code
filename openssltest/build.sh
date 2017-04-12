02bb7c0122cff0dc41b43bc1139eb461cc079eb6
ysoftman <ysoftman@gmail.com>
2017-04-12 13:59:55 +0900

    build.sh 스크립트 linux / osx 에 맞게 수정

--- openssltest/build.sh
+++ openssltest/build.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-# wget https://www.openssl.org/source/openssl-1.0.2k.tar.gz
-# tar zxvf openssl-1.0.2k.tar.gz
-# cd openssl-1.0.2k
-# linux
-# ./config --prefix=/Users/ysoftman/workspace/test_code/openssltest/openssl-1.0.2
-# mac
-# ./configure darwin64-x86_64-cc --prefix=/Users/ysoftman/workspace/test_code/openssltest/openssl-1.0.2
-# make clean
-# make && make install
+rm -rfv openssl-1.0.2k.tar.gz openssl-1.0.2 openssl-1.0.2k
+wget https://www.openssl.org/source/openssl-1.0.2k.tar.gz
+tar zxvf openssl-1.0.2k.tar.gz
+cd openssl-1.0.2k
+if [ $(uname) == 'Linux' ]; then
+    install_path='/home/ysoftman/workspace/test_code/openssltest/openssl-1.0.2'
+    ./config --prefix=${install_path} -fPIC
+elif [ $(uname) == 'Darwin' ]; then
+    install_path='/Users/ysoftman/workspace/test_code/openssltest/openssl-1.0.2'
+    ./configure darwin64-x86_64-cc --prefix=${install_path}
+fi
+make clean
+make -j4 && make install
+cd ..
 
 rm a.out *.o core*
-g++ -g -fPIC -c cryptography.cpp test_cryptography.cpp -I./openssl-1.0.2/include
-g++ -L./openssl-1.0.2/lib -lcrypto -lssl cryptography.o test_cryptography.o
+g++ -g -fPIC -c cryptography.cpp test_cryptography.cpp -I${install_path}/include
+g++ -L${install_path}/lib -lcrypto -lssl cryptography.o test_cryptography.o
