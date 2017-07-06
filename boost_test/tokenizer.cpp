f5ad75c08bd736937af25d21553c0d5f4c54623a
ysoftman <ysoftman@gmail.com>
2017-07-06 17:29:35 +0900

    토큰 생성 케이스 추가

--- boost_test/tokenizer.cpp
+++ boost_test/tokenizer.cpp
@@ -11,18 +11,24 @@ int main()
 {
     cout << "boost tokenizer test" << endl;
 
-    string str = "Yoon,Byoung,Hoon";
+    string str = "Yoon,Byoung_Hoon Boost";
     cout << "str = " << str << endl;
 
     typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
-    boost::char_separator<char> sep(",");
-    tokenizer tok(str, sep);
-
-    cout << "separator = ," << endl;
-    for(tokenizer::iterator iter=tok.begin(); iter!=tok.end(); ++iter)
-    {
+    // 공백으로만 구분하여 토큰 생성
+    tokenizer tok(str, boost::char_separator<char>(" "));
+    for (tokenizer::iterator iter = tok.begin(); iter != tok.end(); ++iter)
         cout << *iter << endl;
-    }
+
+    // 공백,_ 3개 문자로 구분하여 토큰 생성
+    tokenizer tok2(str, boost::char_separator<char>(" ,_"));
+    for (tokenizer::iterator iter = tok2.begin(); iter != tok2.end(); ++iter)
+        cout << *iter << endl;
+
+    // 공백,_ 3개 문자로 구분하고 B 와 H 글자를 토큰으로 만든다.
+    tokenizer tok3(str, boost::char_separator<char>(" ,_", "BH"));
+    for (tokenizer::iterator iter = tok3.begin(); iter != tok3.end(); ++iter)
+        cout << *iter << endl;
 
     return 0;
 }
