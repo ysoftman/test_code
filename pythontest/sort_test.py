18acd969369f978d356f7447704b5cb3efffadef
ysoftman <ysoftman@gmail.com>
2017-07-13 01:10:33 +0900

    sorted key 사용

--- pythontest/sort_test.py
+++ pythontest/sort_test.py
@@ -5,7 +5,7 @@
 
 
 def do_sort():
-    data = [5,4,3,2,1]
+    data = [5, 4, 3, 2, 1]
     print 'orgin = ', data
 
     # 정렬하기
@@ -13,13 +13,21 @@ def do_sort():
     print 'sort = ', data
 
     # 리스트 생성시 정렬하기
-    data2 = sorted([10,9,8,7,6,5,4,3,2,1,0])
+    data2 = sorted([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
     print 'sorted = ', data2
 
     # 내림차순으로 정렬
     # data3 = sorted(data2, reverse=True)
     data2.sort(reverse=True)
-    print 'reversed sort = ',data2
+    print 'reversed sort = ', data2
+
+    # 2차원 배열
+    # iterate 리스트 마다 원소 합을 람다함수로 키 로직 설정
+    data3 = [[2, 2], [4, 1], [6, 1], [3, 7]]
+    print '2d list sorted = ', sorted(data3)
+    data3_sorted = sorted(data3, key=lambda x: x[0] + x[1])
+    print '2d list sorted by custom key = ', data3_sorted
+
 
 if __name__ == '__main__':
     do_sort()
