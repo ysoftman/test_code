fe22788882e6a32b2825ddd9af87efa0410cab8b
ysoftman <ysoftman@gmail.com>
2017-08-17 15:32:17 +0900

    소인수 분해 구하기

---
+++ cpp/prime_factorization_test.cpp
@@ -0,0 +1,53 @@
+////////////////////////////////////////////////////////////////////////////////////
+// ysoftman
+// prime factorization (소인수 분해)
+////////////////////////////////////////////////////////////////////////////////////
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 소인수 분해하기
+void prime_factorization(int num, vector<int> &result)
+{
+	if (num < 2)
+	{
+		printf("Wrong Number...\n");
+		exit(1);
+	}
+
+	int divisor = 2;
+	// int remainder = 0;
+	int quotient = num;
+	while (quotient != 1)
+	{
+		if (quotient % divisor == 0)
+		{
+			quotient = quotient / divisor;
+			result.push_back(divisor);
+		}
+		else
+		{
+			++divisor;
+		}
+	}
+}
+
+int main()
+{
+	int num = 0;
+
+	printf("Input Number : ");
+	scanf("%d", &num);
+
+	vector<int> result;
+	vector<int>::iterator iter;
+	prime_factorization(num, result);
+	for (iter = result.begin(); iter != result.end(); ++iter)
+	{
+		cout << *iter << endl;
+	}
+
+	return 0;
+}
