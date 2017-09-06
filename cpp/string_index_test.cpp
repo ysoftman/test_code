64b13ca741d46fec3a404dcc1dfd242a7b279d29
ysoftman <ysoftman@gmail.com>
2017-09-06 14:56:44 +0900

    string index test

---
+++ cpp/string_index_test.cpp
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////////////////////////////////////////
+// ysoftman
+// string 값을 인덱싱 해보기
+////////////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str = "ysoftman";
+    for (int i = 0; i < 10; i++)
+    {
+        // string::operator[] 는 index 범위를 넘어선 경우 null character(\0) 취급
+        if (str[i] == '\0')
+        {
+            cout << i << " " << "empty" << endl;
+            continue;
+        }
+        cout << i << " " << str[i] << endl;
+    }
+    return 0;
+}
