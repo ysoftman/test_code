239e3d5024e9f238b5914b7de1e73705e2fc3968
ysoftman <ysoftman@gmail.com>
2017-10-11 11:06:17 +0900

    go-cacahe 예제 코드

---
+++ golang/go-cache/go-cache.go
@@ -0,0 +1,45 @@
+// ysoftman
+// go-cache test
+// go get -u "github.com/patrickmn/go-cache"
+package main
+
+import "time"
+import cache "github.com/patrickmn/go-cache"
+import "fmt"
+
+var gocache *cache.Cache
+
+func main() {
+	// 캐시 데이터 유지 기간
+	defaultExpiration := time.Second * 3
+	// 캐시 데이터 전체 클린업 체크 간격
+	cleanupInterval := time.Minute * 10
+	gocache = cache.New(defaultExpiration, cleanupInterval)
+
+	key := "key1"
+	value := "ysoftman_data"
+	setCache(key, value, defaultExpiration)
+	getCache(key)
+	getCache(key)
+
+	fmt.Println("wait for ", defaultExpiration, " sec")
+	time.Sleep(defaultExpiration)
+	// 3 초후면 캐시 데이터 만료
+	getCache(key)
+
+}
+
+func setCache(key, value string, expiration time.Duration) {
+	gocache.Set(key, value, expiration)
+	fmt.Println("go-cache set (", key, ",", value, ")")
+}
+
+func getCache(key string) string {
+	out, found := gocache.Get(key)
+	if found {
+		fmt.Println("go-cache get (", key, ") = ", out)
+		return out.(string)
+	}
+	fmt.Println("not found (", key, ") data")
+	return ""
+}
