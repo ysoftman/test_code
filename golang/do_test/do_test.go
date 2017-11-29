aee80f6effb476b7008388ec2b67dcebca6f771e
ysoftman <ysoftman@gmail.com>
2017-11-29 19:10:27 +0900

    설명 수정

--- golang/do_test/do_test.go
+++ golang/do_test/do_test.go
@@ -2,7 +2,7 @@
 // encoding : utf-8
 // title : testing 테스트
 // desc :
-// 테스트 대상 파일명은 _test.go 로 끝나야하고, 함수명은 Test 로 시작해야 한다.
+// 테스트 대상 파일명은 _test.go 로 끝나야하고, 함수명은 TestX(Test대문자)로 시작해야 한다.
 // go test -v 파일명.go 로 전체 TestXXX 함수들 테스트
 // go test -v -run 함수명 으로 특정 함수(들)만 테스트
 
