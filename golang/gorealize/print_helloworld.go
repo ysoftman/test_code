17da9a9b28b2db5703762fb5551b734ee06941d3
bill.2714 <bill.2714@kakaocorp.com>
2017-12-15 17:52:48 +0900

    설명 추가

--- golang/gorealize/print_helloworld.go
+++ golang/gorealize/print_helloworld.go
@@ -10,8 +10,17 @@ func main() {
 		fmt.Println("there is no argurment")
 		os.Exit(0)
 	}
-
-	msg := "just print  "
+	msg := "just print --> "
 	msg += os.Args[1]
 	fmt.Println(msg)
+	aaa()
+	bbb()
+}
+
+func aaa() {
+	fmt.Println("aaa")
+}
+
+func bbb() {
+	fmt.Println("bbb")
 }
