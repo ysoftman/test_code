600544a22fcaeeb7cba57ac3a7758525a1dffaf6
ysoftman <ysoftman@gmail.com>
2017-12-28 12:09:17 +0900

    node.js test

---
+++ node.js/helloworld_tcp.js
@@ -0,0 +1,15 @@
+// helloworld 응답주는 tcp 서버
+// tcp 테스트
+// curl -v localhost:8080
+const tcp = require("net")
+const server = tcp.createServer(function (conn) {
+    conn.write("hello world~\n")
+    conn.end()
+});
+
+const ip = "127.0.0.1"
+const port = 8080
+
+server.listen(port, ip, () => {
+    console.log(`server ${ip}:${port}`)
+})
