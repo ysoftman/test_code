528f92227c606978ab44e375161ab091fbaadd5c
bill.2714 <bill.2714@kakaocorp.com>
2018-01-03 15:41:03 +0900

    alias test

--- sh/alias_test.sh
+++ sh/alias_test.sh
@@ -1,15 +1,18 @@
-# 다음과 같이 alias \ 가 들어간 경우
-alias echoysoftman='echo ysoftman && \
-echo ysoftman'
+# alias 설정시 다음과 같이 명령어가 계속 이어지는중을 표시하기 위해 \ 를 사용한 경우
+alias echoysoftman='echo ysoftman1 &&
+echo ysoftman2 &&
+echo \
+-e ysoftman3'
 
 # zsh 에선 다음과 같이 멀티라인 처리되어 다음과 같이 출력
-ysoftman
-ysoftman
+ysoftman1
+ysoftman2
+ysoftman3
 
-# bash 에선 \ 를 커맨드로 인식해서 에러 발생
-ysoftman
+# bash 에선 \ 를 다르게 인식해서 에러 발생
+ysoftman1
+ysoftman2
 bash: echoysoftman: command not found
 
-# \ 문자 빼면 zsh, bash 둘 잘 작동한다.
-alias echoysoftman='echo ysoftman &&
-echo ysoftman'
+# 한줄로 처리하면 zsh, bash 둘 잘 작동한다.
+alias echoysoftman='echo ysoftman1 && echo ysoftman2 && echo -e ysoftman3'
