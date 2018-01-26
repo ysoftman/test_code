388b98366d7d407ce4a01a00ad3b0f943560c6a1
ysoftman <ysoftman@gmail.com>
2018-01-26 18:00:39 +0900

    배열 테스트

--- golang/array_slice/array_slice.go
+++ golang/array_slice/array_slice.go
@@ -10,6 +10,11 @@ func main() {
 	arTest0 := [5]int{11, 22, 33, 44, 55}
 	fmt.Println("arTest0 len = ", len(arTest0), "arTest1 cap = ", cap(arTest0), "arTest1 = ", arTest0)
 
+	// 배열 생성시 2번째는 7값, 5번째는 99값을 넣는다.
+	// 이때 설정안된 곳은 0값으로 채워지고 마지막으로 설정된 n번째가 배열의 크기가 된다.
+	arTest00 := []int{2: 7, 5: 99}
+	fmt.Println("arTest0 len = ", len(arTest00), "arTest1 cap = ", cap(arTest00), "arTest1 = ", arTest00)
+
 	// ... 파라미터는 variadic 으로 부고, 0 이상의 파라미터가 올수 있다는 의미
 	// https://golang.org/ref/spec#Function_types
 	// [...] 로 구체적 원소개수를 명시하지 않아도 된다.
