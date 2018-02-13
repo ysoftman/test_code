6b9e4a3b1b3569a8570c8d194224d08f2ccf9e84
ysoftman <ysoftman@gmail.com>
2018-02-13 14:23:35 +0900

    package 설치 스크립트 추가

--- golang/custom_package/custom_package.go
+++ golang/custom_package/custom_package.go
@@ -3,23 +3,25 @@
 // title : 패키지 테스트
 package main
 
-import "fmt"
-import "my_math"
+import (
+	"fmt"
+	"my_math"
+)
 
 func main() {
 	// 같은 패키지 않의 함수 호출 가능
 	innerFunc1(1)
-	
+
 	// 다른 패키지의 소문자로 시작하는 함수 호출 불가능
 	// 컴파일 에러발생 --> cannot refer to unexported name my_math.add
 	//result := my_math.add(1,2)
-	
+
 	// 다른 패키지의 대문자로 시작하는 함수 호출 가능
 	fmt.Println("call my_math.Add(1,2)")
-	result := my_math.Add(1,2)
+	result := my_math.Add(1, 2)
 	fmt.Println("result = ", result)
 }
 
 func innerFunc1(x int) {
-	fmt.Println("innerFunc1(" , x, ")")
+	fmt.Println("innerFunc1(", x, ")")
 }
