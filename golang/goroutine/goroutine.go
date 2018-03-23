d1b35847983db31c33588e0b95062204e3bc67d2
ysoftman <ysoftman@gmail.com>
2018-03-23 17:59:25 +0900

    이름 변경

--- golang/goroutine/goroutine.go
+++ golang/goroutine/goroutine.go
@@ -12,9 +12,9 @@ const Maxcnt = 10
 
 func main() {
 	// go routine 간의 통신 및 동기화를 위한 channel(고루틴간의 데이터 공유를 위한 통로) 생성
-	// 버퍼 명시를 안하면 상대가 준비될때까지 블록되어 lock 없이 동기화 가능
+	// 버퍼 명시를 안하면(unbuffered channel) 상대가 준비될때까지 블록되어 lock 없이 동기화 가능
 	//channel1 := make(chan int)
-	// 버퍼를 명시하면 비동기로 블록되지 않고 바로 수행된다.
+	// 버퍼를 명시하면(buffered channel) 비동기로 블록되지 않고 바로 수행된다.
 	channel1 := make(chan int, 10)
 	channel2 := make(chan int, 10)
 
@@ -23,58 +23,58 @@ func main() {
 	wg.Add(3)
 	// go 로 go routine(경량 쓰레드) 돌리기
 	// go routine 생성 비용은 크지 않아 수천개를 사용해도 된다.
-	go send_value1(channel1, &wg)
-	go send_value2(channel2, &wg)
-	go recv_value(channel1, channel2, &wg)
+	go sendValue1(channel1, &wg)
+	go sendValue2(channel2, &wg)
+	go recvValue(channel1, channel2, &wg)
 	//  WaitGroup 의 고루틴들이 모두 끝날(Done)때까지 대기
 	wg.Wait()
 }
 
-func send_value1(ch chan int, wg *sync.WaitGroup) int {
+func sendValue1(ch chan int, wg *sync.WaitGroup) int {
 	defer wg.Done()
 	value := 0
 	for {
 		time.Sleep(100 * time.Millisecond)
-		fmt.Printf("[%s] send_value1 [%d]\n", time.Now().Format("2006-01-02 15:04:05"), value)
+		fmt.Printf("[%s] sendValue1 [%d]\n", time.Now().Format("2006-01-02 15:04:05"), value)
 
 		// 채널에 value 값 전달
 		ch <- value
 
 		// 채널 송신측에서는 채널을 close 할 수 있다.
 		if value >= Maxcnt {
-			//fmt.Printf("[%s] send_value1 채널닫음\n", time.Now().Format("2006-01-02 15:04:05"))
+			//fmt.Printf("[%s] sendValue1 채널닫음\n", time.Now().Format("2006-01-02 15:04:05"))
 			//close(ch)
 			break
 		}
 		value++
 	}
-	fmt.Printf("[%s] send_value1 end\n", time.Now().Format("2006-01-02 15:04:05"))
+	fmt.Printf("[%s] sendValue1 end\n", time.Now().Format("2006-01-02 15:04:05"))
 	return 0
 }
 
-func send_value2(ch chan int, wg *sync.WaitGroup) int {
+func sendValue2(ch chan int, wg *sync.WaitGroup) int {
 	defer wg.Done()
 	value := 0
 	for {
 		time.Sleep(200 * time.Millisecond)
-		fmt.Printf("[%s] send_value2 \t[%d]\n", time.Now().Format("2006-01-02 15:04:05"), value)
+		fmt.Printf("[%s] sendValue2 \t[%d]\n", time.Now().Format("2006-01-02 15:04:05"), value)
 
 		// 채널에 value 값 전달
 		ch <- value
 
 		// 채널 송신측에서는 채널을 close 할 수 있다.
 		if value >= Maxcnt {
-			//fmt.Printf("[%s] send_value2 채널닫음\n", time.Now().Format("2006-01-02 15:04:05"))
+			//fmt.Printf("[%s] sendValue2 채널닫음\n", time.Now().Format("2006-01-02 15:04:05"))
 			//close(ch)
 			break
 		}
 		value++
 	}
-	fmt.Printf("[%s] send_value2 \tend\n", time.Now().Format("2006-01-02 15:04:05"))
+	fmt.Printf("[%s] sendValue2 \tend\n", time.Now().Format("2006-01-02 15:04:05"))
 	return 0
 }
 
-func recv_value(ch1, ch2 chan int, wg *sync.WaitGroup) {
+func recvValue(ch1, ch2 chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
 	cnt1, cnt2 := 0, 0
 	for {
