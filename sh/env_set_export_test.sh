09a144e6e72b97acd0e49bcfeb1f20416e0feb10
ysoftman <ysoftman@gmail.com>
2018-04-03 14:03:32 +0900

    환경변수 테스트 파일 이름 변경

---
+++ sh/env_set_export_test.sh
@@ -0,0 +1,33 @@
+#/bin/sh
+# ysoftman
+
+# set(builtin command) 현재 쉘에만 국한된 환경변수 조회
+set
+
+echo '-----'
+
+# ysoftmanvar1=1234 로 저장된 상태
+export ysoftmanvar1="1234"
+echo $ysoftmanvar1
+
+
+# env 만 사용하면 zzz 값으로 되지 않는다.
+env ysoftmanvar1="zzz" echo $ysoftmanvar1
+# ysoftmanvar1=zzz 로 명령 수행하기
+ysoftmanvar1="zzz" bash -c 'echo $ysoftmanvar1'
+
+
+# unexport 
+unset ysoftmanvar1
+unset ysoftmanvar2
+
+# export 는 현재 쉘과 현재 쉘에서 파생된 쉘에 환경변수를 적용한다.
+# sh ysoftman.sh 로 실행하면 ysoftman.sh 쉘과 그 자식쉘에만 export 가 반영된다.
+# source ysoftman.sh 로 실행해야 현재 쉘에 export 내용이 반영된다.
+export ysoftmanvar1='ysoftmanvar1'
+export ysoftmanvar2='ysoftmanvar2'
+
+echo '-----'
+
+# export 또는 env 로 export 된 환경변수 조회
+env
