a64e04fe9c6bde348c8058fb49b692e8d0b42046
ysoftman <ysoftman@gmail.com>
2018-04-06 15:52:12 +0900

    file stream 테스트

---
+++ cpp/file_stream_test.cpp
@@ -0,0 +1,36 @@
+////////////////////////////////////////////////////////////////////////////////////
+// ysoftman
+// ifstrea(파일 입력), ofstraem (파일 출력) 테스트
+// http://www.cplusplus.com/reference/fstream/ofstream/open/
+////////////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    char filename[100] = "file_stream_test.txt";
+
+    cout << "filename: " << filename << endl;
+
+    // 파일 쓰기
+    ofstream outFile;
+    outFile.open(filename, std::ofstream::app);
+    outFile.write("abcdefghijklmnopqrstuvwxyz\n", 27);
+    outFile.close();
+
+    // 파일 읽기
+    ifstream inFile(filename);
+    while (not inFile.eof())
+    {
+        char szBuffer[27] = {
+            0,
+        };
+        inFile.read(szBuffer, 27);
+        cout << szBuffer << endl;
+    }
+    inFile.close();
+
+    return 0;
+}
