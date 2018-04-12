37a9168b730d1c627f78bd868c96fcdb114382bb
ysoftman <ysoftman@gmail.com>
2018-04-12 09:43:54 +0900

    주석 추가

--- cpp/infinite_file_read_test.cpp
+++ cpp/infinite_file_read_test.cpp
@@ -1,6 +1,8 @@
 // ysoftman
 // 파일 무한 읽기 테스트
 // 이 프로그램이 파일 하나를 (아주 천천히)읽고 있는 와중에, 파일을 삭제하거나 수정하였을때 현상을 보기 위함
+// 파일이 오픈되어 있는지 확인
+// lsof -u ysoftman | grep infinite_file.txt
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h> // for sleep function
