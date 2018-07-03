790b2fff261f7effae922aee66b4aac5639f0402
ysoftman <ysoftman@gmail.com>
2018-07-03 19:08:00 +0900

    주석 추가

--- golang/list/list.go
+++ golang/list/list.go
@@ -10,10 +10,13 @@ import (
 
 func main() {
 	mylist := list.New()
+	// 리스트 끝에 추가
 	mylist.PushBack("aaa")
 	mylist.PushBack(111)
-	el := mylist.PushFront(123)
-	mylist.InsertBefore(222, el)
+	// 리스트 처음에 추가
+	el := mylist.PushFront(222)
+	// el 앞에 추가
+	mylist.InsertBefore(333, el)
 	// fmt.Println(mylist)
 	for iter := mylist.Front(); iter != nil; iter = iter.Next() {
 		fmt.Println(iter.Value)
