3c044484acae387683da6463d3dfd354de4388c8
ysoftman <ysoftman@gmail.com>
2018-07-06 17:37:01 +0900

    variable 길이 체크 테스트

--- sh/variable.sh
+++ sh/variable.sh
@@ -84,3 +84,13 @@ for v in `cat variable.txt`
 do
     echo ${v}
 done
+
+
+var1="aaa"
+var2="bb"
+var3=${var1}${var2}
+echo ${var3}
+echo ${#var3}
+if [[ ${#var3} == 5 ]]; then
+    echo "len(${var3}) == 5"
+fi
