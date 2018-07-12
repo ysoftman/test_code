d35af3e9e7ce0c43c5483a64301e0d0785f628ad
ysoftman <ysoftman@gmail.com>
2018-07-12 14:52:46 +0900

    du_sort.sh diskusage_test.sh 로 합치기

--- sh/diskusge_test.sh
+++ sh/diskusge_test.sh
@@ -8,3 +8,9 @@ du -sh
 
 # 현재 디렉토리에서 파일 및 하위 디렉토리의 디스크 사용량 보기
 du -sh *
+
+# 현재 디렉토리으 모든 파일에서 가장 큰 10개 출력하기
+# du -a(all) -k(kbyte) 리눅스에서는 -b(bytes) 도 사용가능
+# sort -n(number) -r(reverse)
+# head -10(lines)
+du -ak | sort -nr | head -10
