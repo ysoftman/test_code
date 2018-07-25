e0898313a373b11dae02d6de49db8a08414c98bb
ysoftman <ysoftman@gmail.com>
2018-07-25 19:33:24 +0900

    주석추가, 코드 포맷팅

--- python/mysql_test.py
+++ python/mysql_test.py
@@ -9,7 +9,7 @@
 # mysql-python 설치 (3.x 버전)
 # pip install pymysql
 
-# mysql-python 설치 확인 
+# mysql-python 설치 확인
 # pip list
 
 import pymysql
@@ -22,38 +22,40 @@ db_passwd = "ysoftman"
 db_dbname = "test"
 db_charset = "utf8"
 
+
 def db_test():
-	# db 연결
-	conn = pymysql.connect(host=db_host, port=db_port, user=db_user, passwd=db_passwd, database=db_dbname, charset=db_charset)
+    # db 연결
+    conn = pymysql.connect(host=db_host, port=db_port, user=db_user,
+                           passwd=db_passwd, database=db_dbname, charset=db_charset)
 
-	# 커서 준비
-	cursor = conn.cursor()
+    # 커서 준비
+    cursor = conn.cursor()
 
-	# 쿼리 설정
-	query = "select curdate();"
+    # 쿼리 설정
+    query = "select curdate();"
 
-	# 쿼리 실행하기
-	cursor.execute(query)
+    # 쿼리 실행하기
+    cursor.execute(query)
 
-	# 쿼리 결과 가져오기
-	result = cursor.fetchone()
+    # insert, update 는 execute() 후 commit() 해야 반영 된다.
+    # conn.commit()
 
-	# 결과 출력
-	print("query = %s , result = %s" % (query, result))
+    # 쿼리 결과 가져오기
+    result = cursor.fetchone()
 
+    # 결과 출력
+    print("query = %s , result = %s" % (query, result))
 
+    # 테이블 상태 파악 해보기
+    query = "show table status;"
+    cursor.execute(query)
+    print("query = %s" % query)
+    for out in cursor:
+        print("name = %s , engine = %s , version = %s , row_foramt = %s , rows = %s " % (
+            out[0], out[1], out[2], out[3], out[4]))
 
-	# 테이블 상태 파악 해보기
-	query = "show table status;"
-	cursor.execute(query)
-	print("query = %s" % query)
-	for out in cursor:
-		print("name = %s , engine = %s , version = %s , row_foramt = %s , rows = %s " % (out[0], out[1], out[2], out[3], out[4]))
-
-
-	# 연결 종료
-	conn.close()
+    # 연결 종료
+    conn.close()
 
 # mysql db test
 db_test()
-
