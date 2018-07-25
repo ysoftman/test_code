143ccd8397a5b1e330b4a88cbfb1f08d676948fc
ysoftman <ysoftman@gmail.com>
2018-07-25 20:08:51 +0900

    딕션러니 값 조회해서 추가하기 테스트

--- python/dictionary_test.py
+++ python/dictionary_test.py
@@ -10,16 +10,29 @@ import operator
 
 # dictionary 테스트
 val1 = {}
-print("va1 = ", val1)
+print("val1 = ", val1)
 
 # key1 = 문자열 추가
 val1["key1"] = "string value"
 # key2 = 리스트 추가
 val1['key2'] = [50, 60, 70]
 
-print("va1 = ", val1)
-print("va1 = key1 -> ", val1["key1"])
-print("va1 = key2 -> ", val1["key2"])
+print("val1 = ", val1)
+# print("val1 = key1 -> ", val1["key1"])
+# print("val1 = key2 -> ", val1["key2"])
+
+# key3 값이 없으면 추가
+if not 'key3' in val1:
+    val1['key3'] = "aaaa"
+    print("add {key3:aaa}")
+# key3 값이 있으면 값 변경
+if 'key3' in val1:
+    val1['key3'] = "bbb"
+    print("add {key3:bbb}")
+
+
+print("print all in val1", val1)
+
 
 # key2 의 리스트 값중 0번째 출력
 print(val1["key2"][0])
@@ -33,7 +46,8 @@ for i in val2:
     print("val2[%d] = %d" % (i, val2[i]))
 
 # 내림 차순 정렬
-val3 = collections.OrderedDict(sorted(val2.items(), key=operator.itemgetter(1), reverse=True))
+val3 = collections.OrderedDict(
+    sorted(val2.items(), key=operator.itemgetter(1), reverse=True))
 print(val3)
 for i in val3:
     print("val3[%d] = %d" % (i, val3[i]))
