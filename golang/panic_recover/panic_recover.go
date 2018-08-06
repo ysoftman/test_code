7b71943c28d7c8c3bed31495ce4bf4a53b339cb9
ysoftman <ysoftman@gmail.com>
2018-08-06 22:33:51 +0900

    패닉 내용 파일로 쓰기

--- golang/panic_recover/panic_recover.go
+++ golang/panic_recover/panic_recover.go
@@ -3,7 +3,11 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("panic and recover...")
@@ -20,9 +24,19 @@ func makePanicAndRecover(a int, b int) {
 	// defer 로 처리되는 함수에 recover 를 사용해서 패닉 메시지를 출력하고 프로그램을 계속 실행할 수 있다.
 	// recover() defer 내에서 처리되이어야 한다.
 	defer func() {
-		err := recover()
-		if err != nil {
-			fmt.Println(err)
+		r := recover()
+		if r != nil {
+
+			// 패닉 내용 파일로 쓰기
+			fp, err := os.OpenFile("panic.log", os.O_RDWR|os.O_APPEND|os.O_CREATE|os.O_SYNC, 0644)
+			if err != nil {
+				fmt.Println("open file error...", err)
+			}
+			defer fp.Close()
+			log.SetOutput(fp)
+			log.Println(r)
+
+			fmt.Println(r)
 		}
 		fmt.Println("defer end")
 	}()
