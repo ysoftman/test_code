cdf2030bcd37af00ba039920966651b7489b20bd
ysoftman <ysoftman@gmail.com>
2018-08-10 02:30:52 +0900

    mockup http server 함수

--- golang/http_test/httptest_server_client.go
+++ golang/http_test/httptest_server_client.go
@@ -11,22 +11,25 @@ import (
 	"net/http/httptest"
 )
 
-func main() {
-
+func makeMockup() *httptest.Server {
 	// 테스트를 위한 http 서버 생성
 	httpserver := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "ysoftman response")
 	}))
-	defer httpserver.Close()
 
-	fmt.Println("[httptest server] running...")
+	fmt.Printf("[httptest server] %s running...\n", httpserver.URL)
+	return httpserver
+}
+
+func main() {
+	hs := makeMockup()
+	defer hs.Close()
 
 	// 테스트 http 서버에 get 요청
-	resp, err := http.Get(httpserver.URL)
+	resp, err := http.Get(hs.URL)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	// httpserver 응답 확인
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
