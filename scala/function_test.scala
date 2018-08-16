9f0017af9a652adece00b76fae1c2538613303c5
ysoftman <ysoftman@gmail.com>
2018-08-17 00:01:32 +0900

    컴파일, 빌드 설명

--- scala/function_test.scala
+++ scala/function_test.scala
@@ -1,34 +1,55 @@
-// scala 함수 테스트
-val string="scala function test"
-println(string)
-val num1 = 1
-val num2 = 2
-println(num1,num2)
-println(num1+num2)
+// scala 설치
+// sudo apt-get install scala
 
-// 함수 사용
-def addInt(num: Int): Int = {
-    num + 1
+// scala console 실행
+// scala
+
+// scala console 종료 방법
+// :q
+// :quit
+// sys.exit
+
+
+// 컴파일(.class 파일 생성)
+// scalac ./function_test.scala
+
+// 실행
+// scala ysoftman
+
+object ysoftman {
+    def main(args: Array[String]) {
+        val string="scala function test"
+        println(string)
+        val num1 = 1
+        val num2 = 2
+        println(num1,num2)
+        println(num1+num2)
+
+        // 함수 사용
+        def addInt(num: Int): Int = {
+            num + 1
+        }
+        // 재할당 할 수 없다.
+        // num1 = addInt(1)
+        val num3 = addInt(1)
+        // s"" 리터럴 출력
+        println(s"addInt=$num3")
+        println("addInt="+addInt(1))
+
+        def addInt2(num1: Int, num2: Int) = {
+            num1 + num2 + 1
+        }
+        println("addInt="+addInt2(2, 2))
+
+        def addInt2_1(num1: Int)(num2: Int) = {
+            num1 + num2 + 1
+        }
+        println("addInt="+addInt2(2, 2))
+
+        // 함수 인자 미리 설정해 놓기
+        val addInt3 = addInt2(5, _:Int)
+        val addInt4 = addInt2(_:Int, 7)
+        println("addInt="+addInt3(5))
+        println("addInt="+addInt4(5))
+    }
 }
-// 재할당 할 수 없다.
-// num1 = addInt(1)
-val num3 = addInt(1)
-// s"" 리터럴 출력
-println(s"addInt=$num3")
-println("addInt="+addInt(1))
-
-def addInt2(num1: Int, num2: Int) = {
-    num1 + num2 + 1
-}
-println("addInt="+addInt2(2, 2))
-
-def addInt2_1(num1: Int)(num2: Int) = {
-    num1 + num2 + 1
-}
-println("addInt="+addInt2(2, 2))
-
-// 함수 인자 미리 설정해 놓기
-val addInt3 = addInt2(5, _:Int)
-val addInt4 = addInt2(_:Int, 7)
-println("addInt="+addInt3(5))
-println("addInt="+addInt4(5))
