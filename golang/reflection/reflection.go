ab0aae2823bbc34690817ef631c243248c8cef2c
ysoftman <ysoftman@gmail.com>
2018-08-16 12:05:27 +0900

    reflectValue 테스트

--- golang/reflection/reflection.go
+++ golang/reflection/reflection.go
@@ -24,24 +24,31 @@ func main() {
 	fmt.Printf("myBox ==> %+#v\n", myBox)
 
 	// reflection 은 런타임시 대상 구조체(클래스),함수 등의 메모리는 알고 있지만 형식(타입)을 모를 경우
-	// 분석하여 클래스 또는 변수의 이름, 타입등을 파악할 수 있도록 해준다.
+	// 분석하여 클래스 또는 변수의 이름, 타입, 값등을 파악할 수 있도록 해준다.
 	// reflect.TypeOf 로 대상의 타입을 파악한다.
 	myBoxReflectType := reflect.TypeOf(myBox)
+	// reflect.TypeOf 로 대상의 저장된 값을 파악한다.
+	myBoxReflectValue := reflect.ValueOf(myBox)
 
 	// struct 이름
 	fmt.Println("myBoxReflectType.Name():", myBoxReflectType.Name())
 	// 필드 파악
 	fmt.Println("myBoxReflectType.Field(0).Name:", myBoxReflectType.Field(0).Name)
+	fmt.Println("myBoxReflectType.Field(0).Tag:", myBoxReflectType.Field(0).Tag)
 	fmt.Println("myBoxReflectType.Field(0).Type:", myBoxReflectType.Field(0).Type)
 	fmt.Println("myBoxReflectType.Field(0).Type.Kind():", myBoxReflectType.Field(0).Type.Kind())
 	if myBoxReflectType.Field(0).Type.Kind() == reflect.String {
 		fmt.Println("myBoxReflectType.Field(0).Type.Kind() == reflect.String")
+		fmt.Println("myBoxReflectValue.Field(0).String():", myBoxReflectValue.Field(0).String())
 	}
-	fmt.Println("myBoxReflectType.Field(0).Tag:", myBoxReflectType.Field(0).Tag)
 	fmt.Println("myBoxReflectType.Field(1).Name:", myBoxReflectType.Field(1).Name)
+	fmt.Println("myBoxReflectType.Field(1).Tag:", myBoxReflectType.Field(1).Tag)
 	fmt.Println("myBoxReflectType.Field(1).Type:", myBoxReflectType.Field(1).Type)
 	fmt.Println("myBoxReflectType.Field(1).Type.Kind():", myBoxReflectType.Field(1).Type.Kind())
-	fmt.Println("myBoxReflectType.Field(1).Tag:", myBoxReflectType.Field(1).Tag)
+	if myBoxReflectType.Field(1).Type.Kind() == reflect.Int {
+		fmt.Println("myBoxReflectType.Field(1).Type.Kind() == reflect.Int")
+		fmt.Println("myBoxReflectValue.Field(1).Int():", myBoxReflectValue.Field(1).Int())
+	}
 
 	// 타입 내부의 변수명으로 필드를 가져올 수 도 있다.
 	myBoxFieldByName, ret := myBoxReflectType.FieldByName("user")
