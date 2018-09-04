3b5739a3e3546efb452e7687a9a16e8ce3ec23cc
ysoftman <ysoftman@gmail.com>
2018-09-04 12:55:08 +0900

    json 파일 한글 저장하기

--- python/json_test.py
+++ python/json_test.py
@@ -2,8 +2,16 @@
 # author: ysoftman
 # python version : 2.x
 # desc : json 파싱해가
-
+import os
 import json
+
+
+import sys
+
+# 한글 파일 저장을 위해 필요
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 
 strJson = '''
 {
@@ -12,7 +20,7 @@ strJson = '''
         "key2": 123
     },
     "obj2" : {
-        "key1": "bbb",
+        "key1": "bbb-한글이 들어있습니다.",
         "key2": 9999
     }
 }
@@ -39,8 +47,14 @@ def parse_json():
             print key, value
 
     # file 로 쓰기
-    fp = file('data.json', 'wb')
-    json.dump(data, fp, indent=2)
+    outfile = os.path.basename(__file__)
+    outfile = outfile.split('.')[0]
+    outfile = outfile + "_out.json"
+    print "outfile :", outfile
+    fp = file(outfile, 'wb')
+    # 참고 https://docs.python.org/2/library/json.html
+    # json.dump(data, fp, indent=2) # json 파일에 한글 \uXXX 로 escape 되어 기록된다.
+    json.dump(data, fp, indent=2, ensure_ascii=False)
     fp.close()
 
 
