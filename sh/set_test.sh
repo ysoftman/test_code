a0e6f29eb696fcd5063191d491a6a063245f9760
ysoftman <ysoftman@gmail.com>
2018-10-08 15:41:42 +0900

    오타 수정

--- sh/set_test.sh
+++ sh/set_test.sh
@@ -3,5 +3,5 @@
 
 # set -x 옵션을 사용하면 현재 스크립트 명령 자체도 stderr 로 확인 할 수 있다.
 set -x
-fruite="apple banana lemon"
-echo "${fruite}" | awk '{print $2}'
+fruits="apple banana lemon"
+echo "${fruits}" | awk '{print $2}'
