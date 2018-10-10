a3d1a8d22357ea440eb8f37c1349a3fac38a3ea0
ysoftman <ysoftman@gmail.com>
2018-10-10 12:12:56 +0900

    산술연산 테스트

--- sh/variable.sh
+++ sh/variable.sh
@@ -120,3 +120,26 @@ fi
 if [[ ${var1} == "5" ]] || [[ ${var2} == "10" ]]; then
     echo 'condition 4'
 fi
+
+
+num1=1
+num2=2
+if [[ $num1 < $num2 ]]; then
+    echo "$num1 < $num2"
+fi
+num1=1
+num2=1
+# 이중소괄호는 C형태의 산술연산이 가능하다.
+if (( $num1 <= $num2 )); then
+    echo "$num1 <= $num2"
+fi
+# $를 생략해도 된다.
+(( ++num1 ))
+(( num1++ ))
+echo $num1
+(( --num1 ))
+(( num1-- ))
+echo $num1
+# 삼항 연산도 가능
+(( out = num1==1?99:100 ))
+echo $out
