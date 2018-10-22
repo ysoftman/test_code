20437891ae9a074e83d91c862ccde5b1a411da81
ysoftman <ysoftman@gmail.com>
2018-10-22 14:31:10 +0900

    주석 추가

--- python/fibonacci_test.py
+++ python/fibonacci_test.py
@@ -2,13 +2,15 @@
 # author: ysoftman
 # fibonacci test
 
-
+# 0 1 1 2 3 5 8 13 21 34 55
+# 현재피보나치값 = (현재피보나치값-2) + (현재피보나치값-1) 가 반복된다.
 def fibonnaci(n):
     if n == 1 or n == 0:
         return n
-    return fibonnaci(n - 1) + fibonnaci(n - 2)
+    return fibonnaci(n - 2) + fibonnaci(n - 1)
 
 if __name__ == '__main__':
+    # x번째 값의 피보나치 값을 구한다.
     print 'fibonnaci(0) = ', fibonnaci(0)
     print 'fibonnaci(1) = ', fibonnaci(1)
     print 'fibonnaci(2) = ', fibonnaci(2)
