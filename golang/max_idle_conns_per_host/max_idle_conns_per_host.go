d621c278dabd62f5ded7de5226fe726f8eec9a1c
ysoftman <ysoftman@gmail.com>
2018-12-24 19:54:46 +0900

    테스트 대상 서버 인자 입력 옵션 추가

--- golang/max_idle_conns_per_host/max_idle_conns_per_host.go
+++ golang/max_idle_conns_per_host/max_idle_conns_per_host.go
@@ -8,14 +8,14 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
+var targetServer = "http://localhost:55555"
 var myClient *http.Client
 
-func main() {
-	go startWebServer()
-
+func init() {
 	// http transport.go 에서는
 	// MaxIdleConns = 100
 	// MaxIdleConnsPerHost = 2
@@ -32,7 +32,15 @@ func main() {
 	defaultTransport := *defaultTransportPointer
 	defaultTransport.MaxIdleConns = 100
 	defaultTransport.MaxIdleConnsPerHost = 100
-	myClient = &http.Client{Transport: &defaultTransport}
+	myClient = &http.Client{Transport: &defaultTransport, Timeout: 1000 * time.Millisecond}
+}
+
+func main() {
+	if len(os.Args) > 1 {
+		fmt.Println("os.Args[1]:", os.Args[1])
+		targetServer = os.Args[1]
+	}
+	go startWebServer()
 
 	// 웹서버에 부하를 줘서 time_wait(서버의 접속 끊김 ack를 기다리는 상태) 를 만든다.
 	// Linux 환경에서 모니터링
@@ -44,7 +52,7 @@ func main() {
 		// go startClientReq(i)
 
 		// myClient(커스텀 클라이언트)를 사용하면 time_wait 재사용되어 사라진다.
-		// 프로그램이 끝나면 10개는 time_wait 로 되어 있는건 프로그램이 끝났으니 사용되지 않아 time_wait 상태로가 유지되고, 60초(커널에 하드코딩된 time_wait 만료시간) 후 사라진다.
+		// 프로그램이 끝나면 10개는 time_wait 로 되어 있는건 프로그램이 끝나서 서버 응답을 못받아 time_wait 상태로가 유지되고, 60초(커널에 하드코딩된 time_wait 만료시간) 후 사라진다.
 		go startCustomClientReq(i)
 	}
 
@@ -56,9 +64,13 @@ var receiveCnt int
 
 func startWebServer() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(1 * time.Second)
-		receiveCnt++
-		// fmt.Println("Server (", receiveCnt, ") RequestURI : ", r.RequestURI)
+		time.Sleep(100 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte("hahaha~"))
+		if err == nil {
+			receiveCnt++
+			fmt.Println("Server (", receiveCnt, ") RequestURI : ", r.RequestURI)
+		}
 	})
 	http.ListenAndServe(":55555", nil)
 }
@@ -66,12 +78,17 @@ func startWebServer() {
 func startClientReq(jobNum int) {
 	reqCnt := 0
 	for {
-		resp, err := http.Get("http://localhost:55555")
+		resp, err := http.Get(targetServer)
 		if err != nil {
 			log.Println(err.Error())
+			panic(err.Error())
 		}
 		reqCnt++
-		fmt.Println("Client (", jobNum, "-", reqCnt, ")", resp)
+		var bodybtyes []byte
+		bodybtyes, err = ioutil.ReadAll(resp.Body)
+		if err == nil {
+			fmt.Println("Client (", jobNum, "-", reqCnt, ")", string(bodybtyes))
+		}
 		// resp.Body 는 다음 http 연결(소켓)에 재사용되기 때문에 devNull 로 만들어야 한다.
 		io.Copy(ioutil.Discard, resp.Body)
 		resp.Body.Close()
@@ -82,12 +99,17 @@ func startCustomClientReq(jobNum int) {
 	reqCnt := 0
 
 	for {
-		resp, err := myClient.Get("http://localhost:55555")
+		resp, err := myClient.Get(targetServer)
 		if err != nil {
 			log.Println(err.Error())
+			panic(err.Error())
 		}
 		reqCnt++
-		fmt.Println("myClient (", jobNum, "-", reqCnt, ")", resp)
+		var bodybtyes []byte
+		bodybtyes, err = ioutil.ReadAll(resp.Body)
+		if err == nil {
+			fmt.Println("Client (", jobNum, "-", reqCnt, ")", string(bodybtyes))
+		}
 		// resp.Body 는 다음 http 연결(소켓)에 재사용되기 때문에 devNull 로 만들어야 한다.
 		io.Copy(ioutil.Discard, resp.Body)
 		resp.Body.Close()
