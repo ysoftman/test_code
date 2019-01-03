573180f1a3cb8203f73912429bb20568539152eb
ysoftman <ysoftman@gmail.com>
2019-01-03 15:48:23 +0900

    add test case

--- golang/url/url.go
+++ golang/url/url.go
@@ -15,14 +15,19 @@ func main() {
 		log.Fatal(err)
 	}
 	// url 파싱후 각 요소 출력
+	fmt.Println(u)
 	fmt.Println(u.Scheme)
 	fmt.Println(u.Host)
 	fmt.Println(u.Path)
 	fmt.Println(u.RawQuery)
 	fmt.Println(u.Query())
+	fmt.Println(u.Query()["key1"])
+	fmt.Println(u.Query()["key2"])
 
 	// 공백의 경우 %20 대신 + 가 된다.
 	v := url.Values{}
-	v.Set("key", "1 2")
+	v.Set("key", "aaa bbb")
+	fmt.Println(v)
+	fmt.Println(v.Get("key"))
 	fmt.Println(v.Encode())
 }
