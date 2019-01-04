a257ad10447883894378ad6a154865ce2eb2fe0b
ysoftman <ysoftman@gmail.com>
2019-01-04 18:49:54 +0900

    변수명 underscore 제거, log print stderr 확인

--- golang/log/log.go
+++ golang/log/log.go
@@ -14,30 +14,32 @@ func main() {
 	fmt.Fprintln(os.Stdout, "Fprintln()")
 
 	// log 를 이용한 출력
+	// var std = New(os.Stderr, "", LstdFlags)
+	// stderr 만 확인해보자
+	// go run log.go > /dev/null
+	log.Println("로그 디폴트는 stderr 로 출력된다.")
 	// 표준 출력 로거 생성
-	stdout_writer := os.Stdout
-	mylogger1 := log.New(stdout_writer, "[mylogger1]", log.Lshortfile|log.Ldate|log.Ltime)
-
+	stdoutWriter := os.Stdout
+	mylogger1 := log.New(stdoutWriter, "[mylogger1]", log.Lshortfile|log.Ldate|log.Ltime)
 	mylogger1.Println("표준출력 로그 테스트1", 123, "기록")
 	mylogger1.Println("표준출력 로그 테스트2", 123, "기록")
 	mylogger1.Println("표준출력 로그 테스트3", 123, "기록")
 
 	// 버퍼 로거 생성
-	var buffer_writer bytes.Buffer
-	mylogger2 := log.New(&buffer_writer, "[mylogger2]", log.Ldate|log.Ltime|log.Lshortfile)
-
+	var bufferWriter bytes.Buffer
+	mylogger2 := log.New(&bufferWriter, "[mylogger2]", log.Ldate|log.Ltime|log.Lshortfile)
 	// 버퍼에 로그 출력
 	mylogger2.Printf("버퍼 로그 테스트1 %d", 1234)
 	// 버퍼내용 화면에 출력
-	fmt.Printf("%s", buffer_writer.String())
+	fmt.Printf("%s", bufferWriter.String())
 
 	// 버퍼 리셋
-	buffer_writer.Reset()
+	bufferWriter.Reset()
 
 	// 버퍼에 로그 출력
 	mylogger2.Printf("버퍼 로그 테스트2 %d", 1234)
 	// 버퍼내용 화면에 출력
-	fmt.Printf("%s", buffer_writer.Bytes())
+	fmt.Printf("%s", bufferWriter.Bytes())
 
 	// 파일에 로그 출력하기
 	// 대상 파일 열기, 없으면 생성
