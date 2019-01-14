0b8569e2b027cb9807694d75223d0f2ab87bcbae
ysoftman <ysoftman@gmail.com>
2019-01-14 15:14:34 +0900

    dirs, tilde 사용 테스트

---
+++ sh/dirs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# directory stack
+# https://www.gnu.org/software/bash/manual/html_node/Directory-Stack-Builtins.html
+
+# 디렉토리 이동 히스토리 지우기
+dirs -c 
+
+# 디렉토리 이동 히스토리(스택, 왼쪽이 top으로 현재 최신인 현재디렉토리이다.), 기본 현재디렉토리(~)
+dirs 
+
+# 디렉토리 추가, 추가한 디렉토리로 자동 이동한다.
+# pushd, popd 를 할때마다 disrs 가 자동 호출왼다.
+echo 'pushd /'
+pushd /
+echo 'pushd $HOME'
+pushd $HOME
+
+# 최신 디렉토리 제거, 이전 디렉토리로 자동 이동된다.
+echo 'popd'
+popd
+
+
+# 참고로 -는 바로 이전 디렉토리($OLDPWD)로 이동한다.
+# cd -
---
+++ sh/tilde_expansion.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# tild expansion 테스트
+# 참고 http://www.gnu.org/software/bash/manual/html_node/Tilde-Expansion.html
+
+# ~ 는 $HOME 과 같다.
+echo $HOME
+echo ~
+
+# ~+  는 $PWD 와 같다.
+echo $PWD
+echo ~+
+
+
+
+testfile="~/aaa.txt"
+echo ${testfile}
+# testfile 은 ~ 는 실행될때 $HOME 을 찾게 된다.
+# 따라서 단순 ~/aaa.txt 스트링은 실행되지 않으면 $HOME 경로를 가지고 않은 단순 스트링일 뿐이다.
+# No such file or directory 에러가 발생한다.
+# touch ${testfile}
+# eval 사용하면 ~/aaa.txt 자체를 실행하기 때문에 $HOME 를 알 수 있다.
+eval touch ${testfile}
+eval ls -ahl ${testfile}
+eval rm -f ${testfile}
+
+# 위와 같이 ~ 있는 구분을 매번 eval 하지 않으려면 그냥 $HOME 을 사용하는것이 좋다.
+testfile="$HOME/aaa.txt"
+touch ${testfile}
+ls -ahl ${testfile}
+rm -f ${testfile}
