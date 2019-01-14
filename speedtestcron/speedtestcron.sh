8c0059b284b0de26929005a24918124c743de438
ysoftman <ysoftman@gmail.com>
2019-01-14 17:24:53 +0900

    에러로그 기록 수정

--- speedtestcron/speedtestcron.sh
+++ speedtestcron/speedtestcron.sh
@@ -7,7 +7,8 @@
 # crontab -e 으로 등록
 # macOS 의 경우 "operation not permitted" 발생시
 # system preferences -> security & privacy -> privacy 에서 iterm2 를 추가해준다.
-# 0 * * * * bash /Users/ysoftman/workspace/test_code/speedtestcron/speedtestcron.sh 2>&1 $HOME/speedtestcron.log
+# zsh 에서 noclobber 설정때문에 강제(!) 추가
+# 0 * * * * bash /Users/ysoftman/workspace/test_code/speedtestcron/speedtestcron.sh 2>>! $HOME/speedtestcron.log
 
 
 # cron 환경설정
@@ -17,13 +18,10 @@ PATH=$PATH:/usr/local/bin
 speedtestpath=$(which speedtest)
 if [[ ${speedtestpath##*/} != 'speedtest' ]]; then
     pip install speedtest-cli
-else
-    echo 'speedtest-cli installed'
 fi
 
 outfile="$HOME/speedtestcron.out"
 # 다운로드 속도만 주기적으로 기록해보자
-# zsh 에서 noclobber 설정때문에 강제(!) 추가
 date | tr '\n' ' ' >> ${outfile}
 # speedtest output(stdout) 을 tee 로 stderr 로 출력하고 Download 라인만 grep 해서 파일로 기록 한다.
 speedtest --no-upload | tee /dev/stderr | grep "Download" >> ${outfile}
