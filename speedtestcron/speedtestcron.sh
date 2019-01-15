8a7f10f8b815d48bfd06cf271595d680287dc4a9
ysoftman <ysoftman@gmail.com>
2019-01-15 12:35:40 +0900

    macos cron 설정 설명 추가

--- speedtestcron/speedtestcron.sh
+++ speedtestcron/speedtestcron.sh
@@ -7,6 +7,8 @@
 # crontab -e 으로 등록
 # macOS 의 경우 "operation not permitted" 발생시
 # system preferences -> security & privacy -> privacy 에서 iterm2 를 추가해준다.
+# 그리고 macOS 에는 sleep 모드에선 cron 이 작동하지 않는다.
+# system preferences -> energy saver 에서 디스플레이가 꺼져도 잠자기 않도록 해야 한다.
 # 0 * * * * bash /Users/ysoftman/workspace/test_code/speedtestcron/speedtestcron.sh 2>> $HOME/speedtestcron.log
 
 
