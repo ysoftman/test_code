8f95a2f23f947ae153716ba193c47f05753952e7
ysoftman <ysoftman@gmail.com>
2019-01-15 16:50:02 +0900

    size 출력

--- boost/regex.cpp
+++ boost/regex.cpp
@@ -107,11 +107,13 @@ void regex_url()
 	{
 		cout << "(regex_match) url: " << match_result[0] << endl;
 	}
+	cout << "match_result.size(): " << match_result.size() << endl;
 	// 부분 매칭인 경우
 	if (boost::regex_search(url, match_result, host_pattern))
 	{
 		cout << "(regex_search) host: " << match_result[0] << endl;
 	}
+	cout << "match_result.size(): " << match_result.size() << endl;
 }
 
 int main()
