6d0ae35e94b2adf632c89f64fd1d9af7a6946bdb
ysoftman <ysoftman@gmail.com>
2019-01-28 14:50:18 +0900

    주석 추가

--- sh/exist_file.sh
+++ sh/exist_file.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 # ysoftman
 # 디렉토리 존재여부
+# -d file, True if file exists and is a directory.
 if [ -d '/Users/ysoftman/workspace' ]; then
     echo "exist directory"
 else
     echo "not exist directory"
 fi
 
-
 # 파일 존재여부
-if [ -f './exist_file.sh' ]; then
+# e file, True if file exists.
+if [ -e './exist_file.sh' ]; then
     echo "exist file"
 else
+    echo "not exist file"
+fi
+
+# 레귤러 파일 존재 하지 않는 경우
+# -f file, True if file exists and is a regular file.
+if [ ! -f './not_exist_file.sh' ]; then
     echo "not exist file"
 fi
