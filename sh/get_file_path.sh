a56d7b0cfb672800234136cf3d29d870a14a92be
ysoftman <ysoftman@gmail.com>
2019-01-29 11:38:10 +0900

    file command test

--- sh/get_file_path.sh
+++ sh/get_file_path.sh
@@ -9,5 +9,25 @@ elif [ $(uname) == 'Linux' ]; then
 fi
 echo fullpath=${fullpath}
 
+# get dir path
+# method1 - using dirname command
 onlydir=`dirname ${fullpath}`
 echo onlydir=${onlydir}
+
+# method2 - using realpath command
+onlydir2=$(realpath ../)
+echo onlydir2=${onlydir2}
+
+
+# if exist symbloic link file, delete
+if [ -h $0.link ]; then
+	echo "exist link file $0.link"
+	unlink $0.link
+fi
+ln -s $0 $0.link
+
+# show link file own contents
+readlink $0.link
+
+# show link contents
+cat $0.link
