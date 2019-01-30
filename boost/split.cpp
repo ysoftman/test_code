9316d2bbb1f9f5a51cb139a8d0ee9741783053e7
ysoftman <ysoftman@gmail.com>
2019-01-30 15:21:48 +0900

    split 예시 추가

--- boost/split.cpp
+++ boost/split.cpp
@@ -1,5 +1,7 @@
 // ysoftman
 // boost split 사용하기
+// 빌드 및 실행
+// g++ -std=c++11 split.cpp && ./a.out
 #include <iostream>
 #include <vector>
 #include <string>
@@ -14,10 +16,21 @@ int main()
 
     string str = "Yoon,Byoung,Hoon";
     cout << "str = " << str << endl;
-    
+
     vector<string> vecResult;
     boost::split(vecResult, str, boost::is_any_of(","));
-    
+
+    string str2 = "ysoftman/bill";
+    vector<string> vecTemp;
+    boost::split(vecTemp, str2, boost::is_any_of("/"));
+    // auto 는 c++11 버전이상
+    for (auto v : vecTemp)
+    {
+        // vecResult.insert(vecResult.begin(), v);
+        // vecResult.insert(vecResult.end(), v);
+        vecResult.push_back(v);
+    }
+
     cout << "separator = ," << endl;
     cout << "count = " << vecResult.size() << endl;
     vector<string>::iterator iter;
