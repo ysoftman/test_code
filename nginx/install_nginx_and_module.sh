0c397ea2e130fda6a17517534c9ac14fe3e890f8
ysoftman <ysoftman@gmail.com>
2019-03-13 17:09:26 +0900

    mac nginx 설치 방법 수정

--- nginx/install_nginx_and_module.sh
+++ nginx/install_nginx_and_module.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+echo `uname`
 # nginx 에 필요한 모듈 설치
 if [ $(uname) == 'Darwin' ]; then
-    echo `uname`
+    which openssl
+    if [ $? != 0 ]; then
+        brew install openssl
+    fi
+    # openssl 을 gcc(g++) 에서 사용는 경로에도 추가해준다.
+    echo 'y' | ln -s /usr/local/opt/openssl/include/openssl /usr/local/include
+    echo 'y' | ln -s /usr/local/opt/openssl/lib/libssl.1.0.0.dylib /usr/local/lib
+    # --with-openssl=openssl_source 옵션을 주게 되더라도
+    # openssl 내에서 Configure darwin64-x86_64-cc 옵션으로 설정해야 하는데
+    # nginx configure 로 설정할 수 없다.
+    # 그냥 brew 로 nginx 설치는 할 수 있지만 echo 모듈은 사용할 수 없다.
+    brew install nginx
+    # 설정은 vi /usr/local/etc/nginx/nginx.conf
+    exit 0
+
 elif [ $(uname) == 'Linux' ]; then
-    echo `uname`
     yum --version
     if [ $? == 0 ]; then
         sudo yum install -y pcre-devel
@@ -14,22 +28,29 @@ elif [ $(uname) == 'Linux' ]; then
         sudo apt-get install -y libssl-dev
         sudo apt-get install -y libz-dev
     fi
+else
+    echo 'not support system'
+    exit 1
 fi
-
 
 # echo 모듈
 git clone https://github.com/openresty/echo-nginx-module.git
-export ECHO_PWD=$(pwd)/echo-nginx-module
+echo_module_path=$(pwd)/echo-nginx-module
 
-# 다운로드
-wget https://nginx.org/download/nginx-1.14.0.tar.gz
-# 압축 해제
-tar zxvf nginx-1.14.0.tar.gz
+# nginx
+if [ ! -e nginx-1.14.0.tar.gz ]; then
+    # 다운로드
+    wget https://nginx.org/download/nginx-1.14.0.tar.gz
+fi
+if [ ! -d nginx-1.14.0 ]; then
+    # 압축 해제
+    tar zxvf nginx-1.14.0.tar.gz
+fi
 cd nginx-1.14.0
 
 # configure 수행 및 빌드 및 설치
-./configure --with-http_ssl_module --with-http_v2_module --with-http_stub_status_module --add-module=$ECHO_PWD
-make
+./configure --with-http_ssl_module --with-http_v2_module --with-http_stub_status_module --add-module=$echo_module_path
+make -j8
 sudo make install
 
 # nginx 시작
@@ -40,6 +61,8 @@ sudo make install
 #sudo /usr/local/nginx/sbin/nginx -s stop
 # nginx 설명
 #sudo /usr/local/nginx/sbin/nginx -h
+
+cd ..
 
 # 설정 변경하면서 테스트할때
 sudo cp -v ./nginx.conf /usr/local/nginx/conf/nginx.conf && sudo /usr/local/nginx/sbin/nginx -s stop; sudo /usr/local/nginx/sbin/nginx && curl -Lv http://localhost:8080/echo
