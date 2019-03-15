7ca92b2a36104e4a7e238fa55e7d5768a2056a7a
ysoftman <ysoftman@gmail.com>
2019-03-15 10:52:05 +0900

    string npos test

--- cpp/substr_string.cpp
+++ cpp/substr_string.cpp
@@ -31,5 +31,11 @@ int main()
     // 마지막까지 substr 할 경우 시작 위치만 정해주면 된다.
     cout << str.substr(pos) << endl;
 
+    // string::size_type 는 unsignet int 로 음수면 오버플로우가 발생해 양수 큰값으로 설정된다.
+    string::size_type p = -1;
+    cout << p << endl;
+    // static const size_type npos = -1; 로 설정되어 있다.
+    cout << string::npos << endl;
+
     return 0;
 }
