9add7bae4d1b4fe69f1a7d119196a41c290a57fb
ysoftman <ysoftman@gmail.com>
2019-04-19 18:19:36 +0900

    urlencoding test

--- python/url_encoding_test.py
+++ python/url_encoding_test.py
@@ -3,8 +3,27 @@
 # python version : 2.x
 # desc : urlencoding test
 import urllib
+import urlparse
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 
 if __name__ == "__main__":
-    url = {"key1":"1 2",  "key2":"value2"}
+    url = "ysoftman 한글 123"
+    # 공백을 %20 로 표기한다
+    urlenc = urllib.quote(url)
+    print urlenc
+    # url 디코딩
+    urldec = urllib.unquote(url)
+    print urldec
+
+    # dictionary 데이터는 urlencode 을 사용
+    url = {"key1": "1 2",  "key2": "value2한글"}
     # 공백을 %20 대신 + 로 표기한다
-    print urllib.urlencode(url)
+    urlenc = urllib.urlencode(url)
+    print urlenc
+    urldec = urlparse.parse_qsl(urlenc)
+    urldec = dict(urldec)
+    print urldec
