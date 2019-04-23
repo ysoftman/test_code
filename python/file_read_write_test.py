21312d7f706b9a26a717f16f30c0cf1c3d4fdfda
ysoftman <ysoftman@gmail.com>
2019-04-23 13:21:59 +0900

    add file read test

--- python/file_read_write_test.py
+++ python/file_read_write_test.py
@@ -3,12 +3,16 @@
 # python version : 2.x
 # desc : 파이썬 기본 테스트
 
-# import os
+import os
 # import sys
+
+filename = os.path.basename(__file__)
+filename += ".text"
 
 
 def test_file_write(val):
-    myfile = open('test.txt', "w")
+
+    myfile = open(filename, "w")
     for idx in range(1, 5):
         # 숫자를 문자열로 변경하여 문자열 합치기
         strMsg = str(idx) + " " + val + "\n"
@@ -16,21 +20,31 @@ def test_file_write(val):
         myfile.write(strMsg)
         # print 함수를 통한 파일 쓰기
         # 3.x
-        #print(strMsg, end='', file=myfile)
+        # print(strMsg, end='', file=myfile)
         # 2.x
         myfile.write(strMsg)
     myfile.close()
     return None
 
 
-def test_file_read():
+def test_file_read1():
+    myfile = open(filename, "r")
+    line = myfile.readline()
+    while line:
+        print line
+        # 라인별 읽기
+        line = myfile.readline()
+
+
+def test_file_read2():
     myfile = None
     try:
-        myfile = open("test.txt", "r")
+        myfile = open(filename, "r")
+        # 버퍼로 한번에 읽어 놓기
         linebuffer = myfile.readlines()
         for temp in linebuffer:
             # 3.x
-            #print(temp, end='')
+            # print(temp, end='')
             # 2.x
             print(temp)
     except IOError as err:
@@ -42,4 +56,8 @@ def test_file_read():
 
 
 test_file_write("파일에 쓰기 테스트")
-test_file_read()
+print '-----'
+test_file_read1()
+print '-----'
+test_file_read2()
+os.remove(filename)
