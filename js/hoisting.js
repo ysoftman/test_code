842463714fed4459ab23ad93ab873368e1912947
ysoftman <ysoftman@gmail.com>
2019-05-07 18:19:04 +0900

    설명 추가

--- js/hoisting.js
+++ js/hoisting.js
@@ -1,8 +1,13 @@
 // javascript 는 함수,변수 선언부분을 hoisting(끌어올린다.)한다.
 
 // 함수 선언으로 함수 작성
-// 함수 선언언 이전에 함수를 호출해도 수행된다.
-// 함수 선언부가 맨위로 끌여올려지기 때문이다.
+// 함수 선언부가 맨위로 끌여올려지기 때문에 실행시 다음과 같이 처리된다.
+// function aaa() {
+//     document.getElementById('resultbox1').innerHTML = "aaa";
+//     console.log("aaa")
+// }
+// aaa()
+// 따라서 함수 선언 이전에 함수를 호출해도 수행된다.
 aaa()
 function aaa() {
     document.getElementById('resultbox1').innerHTML = "aaa";
