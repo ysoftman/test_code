c4db78301dd8b2367d5e3c7e804af465dcea5c23
ysoftman <ysoftman@gmail.com>
2019-05-09 18:29:44 +0900

    string find test

---
+++ cpp/string_find_test.cpp
@@ -0,0 +1,36 @@
+// ysoftman
+// string find test
+// g++ string_find_test.cpp -std=c++11
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> vecStr;
+    vecStr.push_back("lemon");
+    vecStr.push_back("apple");
+    vecStr.push_back("banana");
+
+    for (auto i : vecStr)
+    {
+        // "na" 문자열이 포함된 경우만 찾는다.
+        if (i.find("na") != string::npos)
+        {
+            cout << i << endl;
+        }
+    }
+
+    for (auto i : vecStr)
+    {
+        // m 또는 l 이 포함된 포함된 경우만 찾는다.
+        // 스트링에서 m 또는 l 중 처음 매치되는 위치를 리턴한다.
+        size_t pos = i.find_first_of("ml");
+        if (pos != string::npos)
+        {
+            cout << i << "   pos:" << pos << endl;
+        }
+    }
+}
