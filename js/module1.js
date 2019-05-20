01f908bc24e8673f06db2f98614393ff78bb7480
ysoftman <ysoftman@gmail.com>
2019-05-20 18:01:25 +0900

    IIFE, module, 등 테스트

---
+++ js/module1.js
@@ -0,0 +1,6 @@
+// javascript 모듈로 외부로 노출하기 위해 export 를 사용한다.
+export let fruits = ["lemon", "apple"];
+export let myname = "ysoftman";
+export let myfunc = () => {
+    console.log("this is myfunc");
+}
---
+++ js/module2.js
@@ -0,0 +1,5 @@
+import { fruits, myname, myfunc } from "./module1.js"
+
+console.log(fruits);
+console.log(myname);
+myfunc();
