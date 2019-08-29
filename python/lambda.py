9287eb98920c67d6db91cf94e17b3328f14ef3f2
ysoftman <ysoftman@gmail.com>
2019-08-29 18:27:19 +0900

    python3 에서도 실행되도록 수정

--- python/lambda.py
+++ python/lambda.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # author: ysoftman
-# python version : 2.x
+# python version : 2.x 3.x
 # desc : lambda test
 
 
@@ -8,12 +8,12 @@ def add(a):
     return a+1
 
 
-print add(9)
+print(add(9))
 
 # 람다 문법
 # (lambda 파라미터 : 표현)(파라미터값)
 # 위를 람다식으로 바꾸면
-print (lambda a: a+1)(9)
+print((lambda a: a+1)(9))
 
 
 def a(a):
@@ -23,8 +23,8 @@ def a(a):
         return "no"
 
 
-print a("aaa")
+print(a("aaa"))
 
 # 위를 if 를 포함하는 람다식으로 바꾸면
 # 참값 if 표현식 else 참이아닌값
-print (lambda a: "ok" if a == "aaa" else "no")("aaa")
+print((lambda a: "ok" if a == "aaa" else "no")("aaa"))
