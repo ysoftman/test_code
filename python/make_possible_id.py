aa761b083856ebe9285f6d6285ba5b299facfb6f
ysoftman <ysoftman@gmail.com>
2019-08-29 19:10:52 +0900

    add comment

--- python/make_possible_id.py
+++ python/make_possible_id.py
@@ -11,5 +11,7 @@ digits = '0123456789'
 #         for d1 in digits:
 #             for d2 in digits:
 #                 print(l1+l2+d1+d2)
+
+# list comprehensions(파이썬에서는 함축 이라는 의미로 사용한다.) 방법
 answer = [l1+l2+d1+d2 for l1 in lowercase for l2 in lowercase for d1 in digits for d2 in digits]
 print(answer)
