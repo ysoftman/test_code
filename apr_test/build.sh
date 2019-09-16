aad9c7f537fd1c83d3ce7596a3ff0831cf586010
ysoftman <ysoftman@gmail.com>
2019-09-16 23:07:12 +0900

    update library env

--- apr_test/build.sh
+++ apr_test/build.sh
@@ -2,7 +2,7 @@
 # build
 aprver="1.6.5"
 libapreq2ver="2.13"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${HOME}/workspace/apr-${aprver}/lib:${HOME}/workspace/libapreq2-${libapreq2ver}/lib:
+export LD_LIBRARY_PATH=${HOME}/workspace/apr-${aprver}/lib:${HOME}/workspace/libapreq2-${libapreq2ver}/lib:
 g++ -O2 -g -fPIC apr_test.cpp -o aprtest -std=c++11 \
 -I${HOME}/workspace/apr-${aprver}/include/apr-1 \
 -I${HOME}/workspace/libapreq2-${libapreq2ver}/include/apreq2 \
@@ -17,4 +17,4 @@ g++ -O2 -g -fPIC apr_test.cpp -o aprtest -std=c++11 \
 -ldl
 
 # aprtest 실행 환경 설정필요
-echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${HOME}/workspace/apr-${aprver}/lib:${HOME}/workspace/libapreq2-${libapreq2ver}/lib:"
+echo "export LD_LIBRARY_PATH=${HOME}/workspace/apr-${aprver}/lib:${HOME}/workspace/libapreq2-${libapreq2ver}/lib:"
