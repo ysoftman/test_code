12506b5ff72c1075f663c78089fbd96660a879dd
ysoftman <ysoftman@gmail.com>
2019-09-17 02:16:59 +0900

    create ld config file

--- apr_test/build.sh
+++ apr_test/build.sh
@@ -11,10 +11,16 @@ g++ -O2 -g -fPIC apr_test.cpp -o aprtest -std=c++11 \
 -L${HOME}/workspace/libapreq2-${libapreq2ver}/lib \
 -L/usr/local/ssl/lib \
 -lapr-1 \
+-laprutil-1 \
 -lapreq2 \
 -lssl \
 -lcrypto \
 -ldl
 
-# aprtest 실행 환경 설정필요
-echo "export LD_LIBRARY_PATH=${HOME}/workspace/apr-${aprver}/lib:${HOME}/workspace/libapreq2-${libapreq2ver}/lib:"
+
+# ld.so.conf.d shared library 경로 파일 생성
+echo "${HOME}/workspace/apr-${aprver}/lib
+${HOME}/workspace/libapreq2-${libapreq2ver}/lib
+" > libapr.conf
+sudo mv -v libapr.conf /etc/ld.so.conf.d/libapr.conf
+sudo /sbin/ldconfig
