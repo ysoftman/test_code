b1e3d61865a720306e2fd4ac02c83e035a7fff40
ysoftman <ysoftman@gmail.com>
2019-09-17 11:17:17 +0900

    skip shared library env in macOS

--- apr_test/build.sh
+++ apr_test/build.sh
@@ -18,6 +18,11 @@ g++ -O2 -g -fPIC apr_test.cpp -o aprtest -std=c++11 \
 -ldl
 
 
+# mac 에서는 ldconfig 사용하지 않는다.
+# otool -L ./aprtest 로 확인하면 라이브러리 경로가 잘 잡혀 있다.
+if [[ $(uname) != 'Linux' ]]; then
+    exit 0
+fi
 # ld.so.conf.d shared library 경로 파일 생성
 echo "${HOME}/workspace/apr-${aprver}/lib
 ${HOME}/workspace/libapreq2-${libapreq2ver}/lib
