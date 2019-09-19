15aab327a2c19a1b6414c9df1252cc7b23f6e1af
ysoftman <ysoftman@gmail.com>
2019-09-19 13:50:44 +0900

    update numpy list test

--- python/numpy_array_calc.py
+++ python/numpy_array_calc.py
@@ -9,67 +9,80 @@ import numpy as np
 
 def calculate_array():
     # 2x3 배열(행렬))
-    ar1 = np.array([[1, 2, 3], [4, 5, 6]])
-    print('ar1 =', ar1)
+    ar = np.array([[1, 2, 3], [4, 5, 6]])
+    print('ar =', ar)
     # 타입
-    print('type(ar1) = ', type(ar1))
+    print('type(ar) = ', type(ar))
     # 차원을 나타내는 튜플
-    print('ar1.shape = ', ar1.shape)
+    print('ar.shape = ', ar.shape)
     # 원소 개수
-    print('ar1.size = ', ar1.size)
+    print('ar.size = ', ar.size)
     # 특정 원소 인덱싱
     # 0번째 행의 2번째 열 위치 원소
-    print('ar1[[0],[2]] = ', ar1[[0], [2]])
-    print('')
+    print('ar[[0],[2]] = ', ar[[0], [2]])
+    print()
+
+    # 큰 배열 생성
+    ar = np.zeros(1000000, dtype=int)
+    print("ar np.zeros(1000000, dtype=int) =", ar)
+    print()
+    ar = np.empty(1000000, dtype=int)
+    print("ar = np.empty(1000000, dtype=int) =", ar)
+    print()
 
     # 3x3 크기의 모든 원소 값이 0
-    ar2 = np.zeros((3, 3))
-    print("ar2 np.zeros((3, 3) =", ar2)
-    print('')
+    ar = np.zeros((3, 3))
+    print("ar np.zeros((3, 3) =", ar)
+    print()
     # 3x3 크기의 모든 원소 값이 9
-    ar3 = np.full((3, 3), 9.0)
-    print("ar3 np.full((3, 3), 9.0) = ", ar3)
-    print('')
+    ar1 = np.full((3, 3), 9.0)
+    print("ar1 np.full((3, 3), 9.0) = ", ar1)
+    print()
+    # 3x3 크기의 모든 원소 값이 3
+    ar2 = np.full((3, 3), 3.0)
+    print("ar2 np.full((3, 3), 3.0) = ", ar2)
+    print()
 
     # 행렬에 값하나(스칼라) 연산
-    print("ar3 + 2 = ", ar3 + 2)
-    print("ar3 - 2 = ", ar3 - 2)
-    print("ar3 / 3 = ", ar3 / 3)
-    print("ar3 * 3 = ", ar3 * 3)
+    print("ar1 + 2 = ", ar1 + 2)
+    print("ar1 - 2 = ", ar1 - 2)
+    print("ar1 / 3 = ", ar1 / 3)
+    print("ar1 * 3 = ", ar1 * 3)
+    print()
 
-    ar4 = np.full((3, 3), 3.0)
-    print("ar3 + ar4 =", ar3 + ar4)
-    print("ar3 - ar4 =", ar3 - ar4)
-    print("ar3 / ar4 =", ar3 / ar4)
-    # 각 원소에 대한 단순 곱
-    print("ar3 * ar4 =", ar3 * ar4)
+    # 행렬 끼리 연산
+    print("ar1 + ar2 =", ar1 + ar2)
+    print("ar1 - ar2 =", ar1 - ar2)
+    print("ar1 / ar2 =", ar1 / ar2)
+    print("ar1 * ar2 =", ar1 * ar2)
+    print()
 
     # 행렬의 곱은 dot 연산 사용
-    print("ar3 dot ar4 =", np.dot(ar3, ar4))
-
+    print("ar1 dot ar2 =", np.dot(ar1, ar2))
+    print()
     # 램덤 출력
-    print('randint(100) test')
+    print('np.random.randint(100) test')
     for _ in range(0, 10):
         r = np.random.randint(100)
         print(r, end=' ')
+    print()
+    print()
 
-    print('')
-
-    ar5 = [i for i in range(1, 11)]
-    print(ar5)
-    print(np.mean(ar5))
+    ar = [i for i in range(1, 11)]
+    print('ar =', ar)
+    print('np.mean(ar) =', np.mean(ar))
     # min, max -> amin(), amax() 의 별명
-    print(np.min(ar5))
-    print(np.amin(ar5))
-    print(np.max(ar5))
-    print(np.amax(ar5))
+    print('np.min(ar) =', np.min(ar))
+    print('np.amin(ar) =', np.amin(ar))
+    print('np.max(ar) =', np.max(ar))
+    print('np.amax(ar) =', np.amax(ar))
     print()
 
     # -10 ~ 10 상의 숫자 10개 랜덤 생성
-    rar = np.random.randint(-10, 10, 10)
-    print(rar)
+    ar = np.random.randint(-10, 10, 10)
+    print('ar np.random.randint(-10, 10, 10) =', ar)
     # 이전 원소값 + 현재 원소값 => 현재원소값
-    print(rar.cumsum())
+    print('ar.cumsum()', ar.cumsum())
 
 
 if __name__ == '__main__':
