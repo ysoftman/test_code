cd51ee2c05d1ec8d9df4f60c84011db59df620b0
ysoftman <ysoftman@gmail.com>
2019-10-07 19:23:52 +0900

    update mutext test

--- golang/mutex_atomic/mutex_atomic.go
+++ golang/mutex_atomic/mutex_atomic.go
@@ -14,61 +14,75 @@ var g_value int = 0
 var g_atomic_value int32 = 0
 var g_mutex = &sync.Mutex{}
 
-func thread1() {
+func thread1(wg *sync.WaitGroup, enableMutex bool) {
+	defer wg.Done()
 	for {
 		// 뮤텍스 락으로 동기화
-		g_mutex.Lock()
+		if enableMutex {
+			g_mutex.Lock()
+		}
 
 		g_value++
-		fmt.Println("[", time.Now(), "] thread1, g_value = ", g_value)
+		fmt.Println("[", time.Now().Format(time.StampMilli), "] thread1, g_value = ", g_value)
+		if enableMutex {
+			g_mutex.Unlock()
+		}
 
 		if g_value > 20 {
 			return
 		}
 
-		g_mutex.Unlock()
-
-		time.Sleep(1 * time.Second)
+		time.Sleep(10 * time.Millisecond)
 
 		// 원자성을 보장하여 2씩 증가시키
 		atomic.AddInt32(&g_atomic_value, 2)
 	}
 }
 
-func thread2() {
+func thread2(wg *sync.WaitGroup, enableMutex bool) {
+	defer wg.Done()
 	for {
 		// 뮤텍스 락으로 동기화
-		g_mutex.Lock()
+		if enableMutex {
+			g_mutex.Lock()
+		}
 
 		g_value++
-		fmt.Println("[", time.Now(), "] thread2, g_value = ", g_value)
+		fmt.Println("[", time.Now().Format(time.StampMilli), "] thread2, g_value = ", g_value)
+		if enableMutex {
+			g_mutex.Unlock()
+		}
+
 		if g_value > 20 {
 			return
 		}
 
-		g_mutex.Unlock()
-
-		time.Sleep(1 * time.Second)
+		time.Sleep(10 * time.Millisecond)
 
 		// 원자성을 보장하여 2씩 증가시키
 		atomic.AddInt32(&g_atomic_value, 2)
 	}
 }
 
-func thread3() {
+func thread3(wg *sync.WaitGroup, enableMutex bool) {
+	defer wg.Done()
 	for {
 		// 뮤텍스 락으로 동기화
-		g_mutex.Lock()
+		if enableMutex {
+			g_mutex.Lock()
+		}
 
 		g_value++
-		fmt.Println("[", time.Now(), "] thread3, g_value = ", g_value)
+		fmt.Println("[", time.Now().Format(time.StampMilli), "] thread3, g_value = ", g_value)
+		if enableMutex {
+			g_mutex.Unlock()
+		}
+
 		if g_value > 20 {
 			return
 		}
 
-		g_mutex.Unlock()
-
-		time.Sleep(1 * time.Second)
+		time.Sleep(10 * time.Millisecond)
 
 		// 원자성을 보장하여 2씩 증가시키
 		atomic.AddInt32(&g_atomic_value, 2)
@@ -76,15 +90,23 @@ func thread3() {
 }
 
 func main() {
-	fmt.Println("[", time.Now(), "] Mutext test...")
+	fmt.Println("[", time.Now().Format(time.StampMilli), "] Mutext test...")
+	var wg sync.WaitGroup
 
-	go thread1()
-	go thread2()
-	go thread3()
+	wg.Add(3)
+	go thread1(&wg, false)
+	go thread2(&wg, false)
+	go thread3(&wg, false)
+	wg.Wait()
+	fmt.Println("g_atomic_value = ", g_atomic_value)
 
-	// 고루틴 쓰레드가 처리될때까지 기다려주기 위해
-	input := ""
-	fmt.Scanln(&input)
-
+	g_value = 0
+	g_atomic_value = 0
+	wg.Add(3)
+	// mutext 사용한 경우 g_value 순서 보장
+	go thread1(&wg, true)
+	go thread2(&wg, true)
+	go thread3(&wg, true)
+	wg.Wait()
 	fmt.Println("g_atomic_value = ", g_atomic_value)
 }
