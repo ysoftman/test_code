659b77eb6961c9b5f9727c37c0b6c041d00b8adb
ysoftman <ysoftman@gmail.com>
2019-10-10 18:53:17 +0900

    std sort test

---
+++ cpp/std_sort.cpp
@@ -0,0 +1,26 @@
+// ysoftman
+// stl sort test
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[] = {3, 5, 10, 1, 7, 2, 20, 17};
+    // 배열 길이 파악
+    int length = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < length; ++i)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    std::sort(arr, arr + length);
+
+    for (int i = 0; i < length; ++i)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
