bdc0b8189d04a28139f2ea43c62f64e017e5261d
ysoftman <ysoftman@gmail.com>
2019-10-27 21:06:28 +0900

    fix typo

--- python/numpy_linspace.py
+++ python/numpy_linspace.py
@@ -7,8 +7,8 @@
 import numpy as np
 
 # start=1 부터 stop=10 까지 num=20개 값을 균일하게 생성
-linlsit = np.linspace(1, 10, 20)
-print(linlsit)
+l = np.linspace(1, 10, 20)
+print(l)
 # start=1 부터 stop=10 까지 num=20개 값을 균일하게 생성, int 값으로
-linlsit = np.linspace(1, 10, 20, dtype=int)
-print(linlsit)
+l = np.linspace(1, 10, 20, dtype=int)
+print(l)
