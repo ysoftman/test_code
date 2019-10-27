139ed64ad473c64c582a204e85aecb070c6268f6
ysoftman <ysoftman@gmail.com>
2019-10-27 20:06:14 +0900

    add enumerate function

---
+++ python/enumerate_test.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# author: ysoftman
+# python version : 3.x
+# desc : enumerate 테스트
+
+l = ["apple", "lemon", "orange"]
+i = 0
+for v in l:
+    print("l[{}] = {}".format(i, v))
+    i = i + 1
+
+
+print("using enumerate() => index and value")
+# enumerate함수를 사용하면 카운터(index)도 리턴 받아 사용 할 수 있다.
+for index, value in enumerate(l):
+    print("l[{}] = {}".format(index, value))
+
+print("using enumerate() => into a list of tuple")
+print(list(enumerate(l)))
