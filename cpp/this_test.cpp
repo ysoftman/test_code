92a7ff63901427affd8cca102b2e8bb59d776f8f
ysoftman <ysoftman@gmail.com>
2019-10-29 01:31:20 +0900

    update

--- cpp/this_test.cpp
+++ cpp/this_test.cpp
@@ -9,13 +9,31 @@ using namespace std;
 class foobar
 {
 public:
-    int cnt;
-    string str;
-
-    void print_variables()
+    int num;
+    foobar &set_num(int num)
     {
-        cout << cnt << "   " << str << endl;
+        // this 사용이유 1
+        // 파리미터와 클래스 멤버변수가 이름이 같을때 구분하기 위해
+        this->num = num;
+        cout << "this->num: " << this->num << endl;
+
+        // this 사용이유 2
+        // 클래스 인스턴스 자체를 리턴하기 위해
+        return *this;
     }
+    foobar *set_num2(int num)
+    {
+        // this 사용이유 1
+        // 파리미터와 클래스 멤버변수가 이름이 같을때 구분하기 위해
+        this->num = num;
+        cout << "this->num: " << this->num << endl;
+
+        // this 사용이유 2
+        // 클래스 인스턴스 자체를 리턴하기 위해
+        return this;
+    }
+
+    int cnt;
     // this 포인터를 리턴
     foobar *this_pointer()
     {
@@ -35,29 +53,25 @@ public:
 int main()
 {
     foobar fb;
-    printf("%p  fb\n", &fb);
-    fb.cnt = 0;
+    fb.set_num(1).set_num(2).set_num(3);
+    cout << "fb.num: " << fb.num << endl;
+    fb.set_num2(7)->set_num2(8)->set_num2(9);
+    cout << "fb.num: " << fb.num << endl;
 
-    printf("%p  ", fb.this_pointer());
+    printf("fb %p\n", &fb);
+
+    printf("fb.this_pointer() %p\n", fb.this_pointer());
     fb.this_pointer()->cnt++;
-    fb.this_pointer()->str = "a";
-    fb.print_variables();
 
-    printf("%p  ", &fb.this_reference());
+    printf("&fb.this_reference() %p\n", &fb.this_reference());
     fb.this_reference().cnt++;
-    fb.this_reference().str = "b";
-    fb.print_variables();
 
     // this_dereference 는 설정할 수 없다.
     // fb.this_dereference().cnt++;
     // this 역참조는 값복사해서 써야 한다.
     foobar fb2 = fb.this_dereference();
-    printf("%p  ", &fb2);
+    printf("fb.this_dereference() --> &fb2 %p\n", &fb2);
     fb2.cnt++;
-    fb2.str = "c";
-    fb2.print_variables();
 
-    // fb 는 변하지 않는다.
-    printf("%p  ", &fb.this_reference());
-    fb.print_variables();
+    return 0;
 }
