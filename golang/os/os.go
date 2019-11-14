84f9eb66b832a87aa8c291d0d2c5057b85f72484
ysoftman <ysoftman@gmail.com>
2019-11-14 12:34:48 +0900

    update

--- golang/os/os.go
+++ golang/os/os.go
@@ -11,6 +11,8 @@ import (
 )
 
 func main() {
+	runExternalComamnd()
+
 	// 현재 워킹 디렉토리
 	cur_dir, _ := os.Getwd()
 	fmt.Println("current directory =", cur_dir)
@@ -30,8 +32,6 @@ func main() {
 	}
 
 	fmt.Println()
-
-	runExternalComamnd()
 	// 0 > 경우 비정상 종료로 처리
 	//	os.Exit(-1)
 	// 정상종료로 처리
@@ -41,13 +41,14 @@ func main() {
 func runExternalComamnd() {
 	fmt.Println("runExternalComamnd - start")
 	cmd := exec.Command("sleep", "2")
-
+	fmt.Printf("cmd: %v\n", cmd)
 	// 명령이 끝날때까지 기다린다.
 	if err := cmd.Run(); err != nil {
 		fmt.Println("[error run sleep]", err)
 	}
 
 	cmd = exec.Command("echo", "orange", "lemon")
+	fmt.Printf("cmd: %v\n", cmd)
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		fmt.Println("[error echo StdoutPipe]", err)
@@ -64,7 +65,8 @@ func runExternalComamnd() {
 	cmd.Wait()
 	fmt.Println(string(stdbytes))
 
-	cmd = exec.Command("bash", "-c", "echo $PWD")
+	cmd = exec.Command("bash", "-c", "echo pwd:${PWD}; echo home:${HOME};")
+	fmt.Printf("cmd: %v\n", cmd)
 	stdout, err = cmd.StdoutPipe()
 	if err != nil {
 		fmt.Println("[error bash StdoutPipe]", err)
