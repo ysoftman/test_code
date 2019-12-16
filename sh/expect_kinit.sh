0078e5613ba23367a2b7ff2673dc01c8a61f0254
ysoftman <ysoftman@gmail.com>
2019-12-16 13:32:02 +0900

    expect test  변경 사항에 대한 커밋 메시지를 입력하십시오. '#' 문자로 시작하는

---
+++ sh/expect_kinit.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/expect 
+# ysoftman
+# bash 가 아닌  expect 로 실행해야 한다.
+
+# kint 암호 자동 입력
+# 참고로 linux 에선 echo  파이프로 처리할 수 있다.
+# echo "aaa\!23" | kinit ysoftman
+
+# expect 환경에서 프로세스 실행
+spawn kinit ysoftman
+
+# password 비슷한 스트링이 나오면
+expect "*password*"
+
+# mac 에선 newline \r (carriage return)
+# linux 에선 newline \n (line feed)
+# windows 에선 newline \r\n (carriage return + line feed)
+send "aaa\!23\r"
+
+# spawn 프로세스 닫기
+expect eof
--- sh/expect_ssh.sh
+++ sh/expect_ssh.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/expect 
 # ysoftman
+# bash 가 아닌  expect 로 실행해야 한다.
 
-# 프로세스 실행
+# expect 환경에서 프로세스 실행
 spawn ssh -l ysoftman localhost
 
 # 1초 대기
@@ -11,9 +12,8 @@ sleep 1
 expect "*assword:*"
 
 # 암호 입력, 엔터
+# -- 옵션의 끝(더이상의 옵션은 없음을 의미)
 send -- "qwer1234\r"
 
+# spawn 프로세스 닫기
 expect eof
-
-
-
