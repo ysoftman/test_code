598b7be7f8727d848a1ac60573ad4a399fbe72fa
ysoftman <ysoftman@gmail.com>
2019-12-17 20:11:50 +0900

    update

--- vue/ysoftman-project/src/components/ToolBar.vue
+++ vue/ysoftman-project/src/components/ToolBar.vue
@@ -69,11 +69,11 @@ export default {
     toggleDark() {
       if (this.$vuetify.theme.dark === true) {
         this.$vuetify.theme.dark = false;
-        // value 는 스트링으로 저장된다.
-        localStorage.setItem("dark_theme", false);
+        // localStorage value 는 스트링으로 저장된다.
+        localStorage.setItem("dark_theme", "false");
       } else {
         this.$vuetify.theme.dark = true;
-        localStorage.setItem("dark_theme", true);
+        localStorage.setItem("dark_theme", "true");
       }
     }
   }
