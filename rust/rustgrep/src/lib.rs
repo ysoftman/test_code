6c7ce309b8e56f84ff5205e7a724a1b98d93e408
ysoftman <ysoftman@gmail.com>
2020-01-08 13:29:26 +0900

    update

--- rust/rustgrep/src/lib.rs
+++ rust/rustgrep/src/lib.rs
@@ -1,5 +1,6 @@
-// 테스트 및 모듈화를 위해 lib.rs(라이브러리) 에 로직을 구현하고 main.rs(바이너리) 에서는 최소한의 코드만 유지한다.
-
+// 테스트수행 및 모듈화를 위해 lib.rs(라이브러리) 에 로직을 구현하고 main.rs(바이너리) 에서는 최소한의 코드만 유지한다. rust 라이브러리만 테스트한다.
+// cargo test 로 테스트해보자
+use std::env;
 use std::error::Error; // Error trait
 use std::fs::File;
 use std::io::prelude::*; // read_to_string()
@@ -7,6 +8,7 @@ use std::io::prelude::*; // read_to_string()
 pub struct Config {
     query: String,
     filename: String,
+    case_insensitive: bool, // 대소문자 구별 여부
 }
 impl Config {
     // 에러 처리에 친화적인 Resuslt<성공일때, 에럴일때> 타입으로 리런
@@ -20,7 +22,19 @@ impl Config {
         // 비용은 들지만 직관적인 클론으로 데이터를 복사한다.
         let query = args[1].clone();
         let filename = args[2].clone();
-        Ok(Config { query, filename })
+
+        // 대소문자 옵션은 환경변수로 부터 받는다.
+        // var 는 Result 타입을 리턴하고 Result의 is_err() 메소드로 에러 판단
+        // CASE_INSENSITIVE 환경변수가 설정되어 있으면 is_err() 는 false 리턴,
+        // let case_insensitive = env::var("CASE_INSENSITIVE").is_err();
+        // CASE_INSENSITIVE 환경변수가 설정되어 있으면 is_ok() 는 true 리턴,
+        let case_insensitive = env::var("CASE_INSENSITIVE").is_ok();
+        print!("case_insensitive:{}\n", case_insensitive);
+        Ok(Config {
+            query,
+            filename,
+            case_insensitive,
+        })
     }
 }
 
@@ -37,6 +51,79 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // ? 로 패닉없이 Result Err 이면 리턴, Ok이면 계속 진행
     f.read_to_string(&mut contents)?;
     println!("[파일내용]\n{}", contents);
+
+    // 대소문자 구별 옵션에 따라 분기
+    let results = if config.case_insensitive {
+        // search()로 contents 에서 query 가 포함된 라인들(Vector)을 출력, 대소문자 구별 안함
+        // results 불변 변수에 할당 라인이 끝이 아니기 때문에 ; 쓰면 안된다.(3항연사자와 비슷)
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        // search()로 contents 에서 query 가 포함된 라인들(Vector)을 출력, 대소문자 구별
+        // results 불변 변수에 할당 라인이 끝이 아니기 때문에 ; 쓰면 안된다.(3항연사자와 비슷)
+        search(&config.query, &contents)
+    };
+    for line in results {
+        println!("[query:{} 가 있는 라인]\n{}", config.query, line);
+    }
     // 이상 없으면 OK unit(nil, ()) 리턴
     Ok(())
+}
+
+// contents 을 리턴과 lifetime 문법으로 연결시킨다.
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    // query 가 포함된 라인을 벡터로 저장
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    // 소문자로 변경
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        // 라인들 소문자로 변경해서 비교
+        // 위에서 query 슬라이스가 아닌 to_lowercase() 는 스트링을 리턴해
+        // contain()의 &str 슬라이스 인자를 &query(&String타입) 로 써야 한다.
+        // &String -> &str 로 쓸 수 있다.
+        if line.to_lowercase().contains(&query) {
+            results.push(line)
+        }
+    }
+    return results;
+}
+
+#[cfg(test)]
+mod rustgreptest {
+    // 같은 파일에 있지만 rustgreptest 테스트 모듈 밖에 있는 search() 등의 함수들을 사용하기 위해
+    // rustgreptest 모듈내에서 사용해야 한다.
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick  three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+    }
+
+    #[test]
+    fn test2_case_insensitive() {
+        let query = "FAsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick  three.";
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search_case_insensitive(query, contents)
+        )
+    }
 }
--- rust/rustgrep/src/main.rs
+++ rust/rustgrep/src/main.rs
@@ -1,7 +1,10 @@
 // ysoftman
 // rust grep 구현
-// 실행
+// 실행 - poem.txt 텍스트 파일에서 frog,body 가 포함된 라인을 출력한다.
 // cargo run frog poem.txt
+// cargo run body poem.txt
+// 실행 - 대소문자 무시하기 환경변수 사용
+// CASE_INSENSITIVE=1 cargo run DAY poem.txt
 
 // rustgrep 모듈(패키지)를 불러오고
 extern crate rustgrep;
@@ -19,14 +22,15 @@ fn main() {
     // config new 로 생성시 cli arguments 파싱
     // unwrap_or_else() 는 Err 발생시 Err 값을 err 인자로 전달하고 클로저(익명함수) 수행
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("파싱 에러:{}", err);
+        // stderr 로 출력
+        eprintln!("파싱 에러:{}", err);
         // panic! 의 불필요한 메시지를 출력하지 않기 위해 exit()사용
         process::exit(1);
     });
 
     // run 이 Err 를 리턴할때 처리, 성공하면 () 를 리턴
     if let Err(e) = rustgrep::run(config) {
-        println!("run 에러 {}", e);
+        eprintln!("run 에러 {}", e);
         process::exit(1);
     }
 }
