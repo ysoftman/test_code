f26a594036340378539e8df4f97c20c8c10ad02e
ysoftman <ysoftman@gmail.com>
2020-01-09 11:57:48 +0900

    update

--- rust/closure.rs
+++ rust/closure.rs
@@ -56,36 +56,38 @@ fn do_something(val: u32) {
     // 함수 호출을 변수에 담아 사용할 수 있다.
     // let cs = calc_something(val);
 
+    // 클로저는 fn , 타입명시등의 어노테이션이 없이 간략하다.
     // 클로저를 정의하고 변수에 저장, 클로저 정의형식은 다음과 같다.
     // |클로저파라미터1, 클로저파라미터2 ... | { 클로저 내용 }
-    // let cs_closure = |num| {
+    // 타입추론이 가능하면 파라티머 타입을 명시하지 않아도 된다.
+    // 클로저 내용이 하나의 표현식이면 {}가 없어도 된다.
+    let cs_closure = |num| num;
+    println!("{}", cs_closure(123));
 
     // 또는 명확성을 높이기 위해 클로저 파라미터외 리턴 타입을 명시할 수도 있다.
-    // let cs_closure = |num: u32| -> u32 {
+    let cs_closure = |num: u32| -> u32 { num };
+    println!("{}", cs_closure(123));
 
-    // 클로저 내용이 하나의 표현식이면 {}가 없어도 된다.
-    // let cs_closure = |num| num + 1;
-
-    // 클로저는 fn , 타입명시등의 어노테이션이 없이 간략하다.
-    // let cs_closure = |num| {
-    //     println!("calculating something slowly...");
-    //     thread::sleep(time::Duration::from_secs(1));
-    //     num
-    // }; // let 문을 완성하기 위해 ; 필요
-    // if val < 25 {
-    //     // 클로저를 사용
-    //     println!("val < 25 {}", cs_closure(val));
-    //     println!("val < 25 {}", cs_closure(val));
-    //     println!("val < 25 {}", cs_closure(val));
-    //     println!("val < 25 {}", cs_closure(val));
-    //     println!("val < 25 {}", cs_closure(val));
-    //     // 최초 클로저 사용시점에 파라미터, 리턴 타입이 추론되서
-    //     // 이후부터는 같은 타입의 파라미터로만 클로저를 사용해야 한다.
-    //     // 이미 위에서 u32 값으로 클로저를 사용해야 아래 코드는 컴파일시 에러가 난다.
-    //     // println!("{}", cs_closure(String::from("ysoftman")));
-    // } else {
-    //     println!("val >= 25 {}", cs_closure(val));
-    // }
+    // 1초있다가 받은 파라미터 리턴하는  클로저
+    let cs_closure = |num| {
+        println!("calculating something slowly...");
+        thread::sleep(time::Duration::from_secs(1));
+        num
+    }; // let 문을 완성하기 위해 ; 필요
+    if val < 25 {
+        // 클로저를 사용
+        println!("val < 25 {}", cs_closure(val));
+        println!("val < 25 {}", cs_closure(val));
+        println!("val < 25 {}", cs_closure(val));
+        println!("val < 25 {}", cs_closure(val));
+        println!("val < 25 {}", cs_closure(val));
+    // 최초 클로저 사용시점에 파라미터, 리턴 타입이 추론되서
+    // 이후부터는 같은 타입의 파라미터로만 클로저를 사용해야 한다.
+    // 이미 위에서 u32 값으로 클로저를 사용해야 아래 코드는 컴파일시 에러가 난다.
+    // println!("{}", cs_closure(String::from("ysoftman")));
+    } else {
+        println!("val >= 25 {}", cs_closure(val));
+    }
 
     let x = vec![1, 2, 3];
     // 일반 내부 함수에서는 함수 밖의 변수에 접근하지 못한다.
@@ -93,7 +95,6 @@ fn do_something(val: u32) {
     //     println!("{:?}", x);
     // }
     // inner_func();
-
     // 클로저와 클로저수행후 값을 저장하고 있는 struct 사용을 위해 new 로 생성
     // move 를 사용하면 클로저가 정의 될때 외부 변수 x 의 소유권이 이동된다.
     // let mut cs_closure = Cacher::new(move |num| {
@@ -102,11 +103,9 @@ fn do_something(val: u32) {
         println!("calculating something slowly...{:?}", x);
         thread::sleep(time::Duration::from_secs(1));
         num
-    }); // let 문을 완성하기 위해 ; 필요
-
+    });
     // 위 클로저가 move 를 사용했다면 소유권이 클로저 안으로 이동되어 여기서 x 를 사용할 수 없다.
     println!("x {:?}", x);
-
     if val < 25 {
         // 최초 한번만 클로저 실행
         println!("val < 25 {}", cs_closure.value(val));
