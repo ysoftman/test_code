19988607fdabf30f81f47a85af9544e9cde77ef6
ysoftman <ysoftman@gmail.com>
2020-01-10 17:42:58 +0900

    box deref test

---
+++ rust/box.rs
@@ -0,0 +1,28 @@
+// ysoftman
+// box test
+// box 로 heap(힙)에 데이터를 저장할 수 있는 스마트포인터다.
+
+// 매번 List:: 를 명시하지 않기 위해 사용
+use List::{Cons, Nil};
+
+fn main() {
+    // 5 값을 힙에 데이터를 저장, 힙도 스코프를 벗어나면 메모리 해제한다.
+    let b = Box::new(5);
+    println!("{}", b);
+    // let list = Cons(1, Cons(2, Cons(3, Nil)));
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    println!("{:?}", list);
+}
+
+#[derive(Debug)]
+enum List {
+    // Cons i32, List 의 쌍을 갖는데, List 자신을 포함하는 재귀적으로 구성
+    // rust List 열거형이 얼마큼의 공가이 필요하지 컴파일시 파악하게 되는데
+    // List -> List -> ... 무한 반복 돼서 에러가 발생한다.
+    // Cons(i32, List),
+    // Box , Rc , & 등의 사용해 포인터크기만 필요하다는것 알게 할 수 있게 한다.
+    Cons(i32, Box<List>),
+    // null, nil 의미가 이닌 재귀의 기본 케이스로
+    // 재귀의 끝, 아이템이 없음을 나타낸다.
+    Nil,
+}
--- rust/hello_world.rs
+++ rust/hello_world.rs
@@ -1,13 +1,19 @@
 // ysoftman
 // https://rinthel.github.io/rust-lang-book-ko/ch00-00-introduction.html
- 
-// hello_world 처럼 단어 사이는 _ 사용
+
+// rust naming conventions
+// https://doc.rust-lang.org/1.0.0/style/style/naming/README.html
+// value 레벨에서는 snake_case (단어 사이 _ 사용) : crates, mod, fn, method, local_variable
+// type 레벨에서는 CamelCase : type, traits, enum
+
 // 들여쓰기는 space 사용
+
 // rustfmt(${HOME}/.cargo/bin/rustfmt) 으로 코드 포맷팅할 수 있다.
 // rustfmt 가 없다면 cargo 로 설치 할 수 있다.
 // cargo install rustfmt
 // 코드 포맷팅
 // rustfmt ./hello_world.rs
+
 // 빌드 및 실행
 // rustc ./hello_world.rs && ./hello_world
 fn main() {
