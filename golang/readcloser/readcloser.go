2e43a9181327195f594c95f33448f9632a1d766f
ysoftman <ysoftman@gmail.com>
2020-01-17 14:41:52 +0900

    update readcloser test

--- golang/readcloser/readcloser.go
+++ golang/readcloser/readcloser.go
@@ -5,7 +5,6 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"io"
 	"io/ioutil"
 )
 
@@ -47,33 +46,34 @@ func main() {
 
 	//////////////////////////
 	// readcloser 생성 및 사용
-	var rc io.ReadCloser
-	rc = ioutil.NopCloser(bytes.NewBuffer(bb1.Bytes()))
-	numOfBytes, err0 := rc.Read(bb1.Bytes())
-	if err0 == nil {
-		fmt.Println("bb1:", bb1.String(), " rc.read() numofBytes", numOfBytes)
-	}
-	rc.Close()
+	lemon := new(bytes.Buffer)
+	lemon.WriteString("lemon")
 
-	//////////////////////////
-	// io.ReadCloser 생성 (NopCloser 는 close() 처리시 nil 리턴하게 된다.)
-	aa1 := ioutil.NopCloser(bytes.NewBuffer(bb1.Bytes()))
+	fmt.Println("lemon:", lemon.String())
+	// io.ReadCloser 생성
+	// NopCloser() 는 Close() 실제 버퍼 데이터를 변경하지 않고
+	// nil만 리턴해서 rc readall 로 offset 이동되어도
+	// lemon 과 공유되는 버퍼에는 영향을 주지 않는다.
+	rc := ioutil.NopCloser(bytes.NewBuffer(lemon.Bytes()))
+	fmt.Println("lemon:", lemon.String())
 
-	// readadll 하면 read 시 offset 이 EOF 로 이동되어 aa 의 내용을 읽을 수 없게 된다.
-	bytes1, err1 := ioutil.ReadAll(aa1)
+	// readadll 하면 read 시 버퍼에 대한 offset 이 EOF 로 이동돼
+	// rc 로는 버퍼 내용을 읽을 수 없게 된다.
+	bytes1, err1 := ioutil.ReadAll(rc)
 	if err1 == nil {
-		fmt.Println("aa1:", string(bytes1))
+		fmt.Println("ReadAll(rc):", string(bytes1))
 	}
-	// 위에서 aa 는 close() 되서 내용이 삭제된 상태다.
-	bytes2, err2 := ioutil.ReadAll(aa1)
+	bytes2, err2 := ioutil.ReadAll(rc)
 	if err2 == nil {
-		fmt.Println("aa1:", string(bytes2))
+		fmt.Println("ReadAll(rc):", string(bytes2))
 	}
-	// bytes 로 새로 새로 만들어야 한다.
-	aa2 := ioutil.NopCloser(bytes.NewBuffer(bytes1))
-	bytes3, err3 := ioutil.ReadAll(aa2)
-	if err3 == nil {
-		fmt.Println("aa2:", string(bytes3))
-	}
+	// rc EOF 만 이동된것이라 lemon 값은 유지 된다.
+	fmt.Println("lemon:", lemon.String())
 
+	// bytes 로 새로 새로 만들어야 한다.
+	rc2 := ioutil.NopCloser(bytes.NewBuffer(bytes1))
+	bytes3, err3 := ioutil.ReadAll(rc2)
+	if err3 == nil {
+		fmt.Println("ReadAll(rc2):", string(bytes3))
+	}
 }
