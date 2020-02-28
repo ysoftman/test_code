52aa31482f297e9f11e7699907a9e52804c42921
ysoftman <ysoftman@gmail.com>
2020-02-28 11:23:14 +0900

    update comment

--- vue/ysoftman-project/src/router.js
+++ vue/ysoftman-project/src/router.js
@@ -18,7 +18,7 @@ Vue.use(Router)
 export default new Router({
   // 디폴트 hash mode ;  http://localhost:8080/# 와 같이 hash 가 붙는다.
   // HTML5 history mode : http://localhost:8080/ 와 같이 hash 가 붙지 않는다.
-  // 히스토리 모드에서는 전체 경로를 명시해야 하기 때문에 hash 모드 처럼 중간 경로만 사용하는 경우 404 에러가 발생한다.
+  // 히스토리 모드에서는 # 없는 path 를 사용하기 때문에 브라우저 refresh 시 서버에 해당 path 로 요청해 서버에서도 각 path 마다 처리를 해주지 않으면 404 에러가 발생한다.
   // vue 의 컴포넌트 path, 백엔드 서버의 path 가 다를 경우 hash 모드를 사용하자.
   // mode: 'history',
   // 라우팅 정보
