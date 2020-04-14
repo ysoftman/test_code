796e77222e82c6b1bd38ddc31ee05537b7629d2f
ysoftman <ysoftman@gmail.com>
2020-04-14 14:52:28 +0900

    tee test

--- sh/tee_test.sh
+++ sh/tee_test.sh
@@ -2,7 +2,14 @@
 rm -f tee_test_file
 
 # tee 명령으로 화면과 파일에 동시 출력하기
-echo "ysoftman" | /usr/bin/tee tee_test_file
+# -a append
+echo "ysoftman1" | /usr/bin/tee -a tee_test_file
+echo "ysoftman2" | /usr/bin/tee -a tee_test_file
+echo "ysoftman3" | /usr/bin/tee -a tee_test_file
+
+
 
 # 화면에 출력후 파일에도 기록된다.
-# cat tee_test_file
+echo '[tee_test_file]'
+cat tee_test_file
+
