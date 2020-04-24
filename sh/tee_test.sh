86c0eb1a34335ebe030c4d77ef0711fe8ff9ec36
ysoftman <ysoftman@gmail.com>
2020-04-24 17:58:54 +0900

    stderr tee 출력

--- sh/tee_test.sh
+++ sh/tee_test.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-rm -f tee_test_file
+rm -f tee_test.log
 
 # tee 명령으로 화면과 파일에 동시 출력하기
 # -a append
-echo "ysoftman1" | /usr/bin/tee -a tee_test_file
-echo "ysoftman2" | /usr/bin/tee -a tee_test_file
-echo "ysoftman3" | /usr/bin/tee -a tee_test_file
+echo "ysoftman1" | tee -a tee_test.log
+echo "ysoftman2" | tee -a tee_test.log
+echo "ysoftman3" | tee -a tee_test.log
 
 
 
 # 화면에 출력후 파일에도 기록된다.
-echo '[tee_test_file]'
-cat tee_test_file
+echo '[tee_test.log]'
+cat tee_test.log
+
+
+# stderr 만 골라 출력
+# stderr
+asdf 2> >(tee -a tee_test.log)
 
