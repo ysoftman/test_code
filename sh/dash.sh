762fb4f30e1aab07e20038f43cc559cbff154908
ysoftman <ysoftman@gmail.com>
2020-05-13 09:47:10 +0900

    update

--- sh/dash.sh
+++ sh/dash.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -x
 
-# - (dash) 로 끝나면 - 이후에 오는 값들은 파일로 취급한다.
+# - (dash) 로 끝나면 - 이후에 옵션은 모두 무시되며
+# 파일명이나 아큐먼트로 취급한다.
 # echo 출력(/dev/stdout) 후 file 에서 - 는 /dev/stdin 으로 받는다.
 echo ysoftman | file -
 
