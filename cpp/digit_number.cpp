039ddd7b83ff81f86f2cf08d5416b3d4ab684929
ysoftman <ysoftman@gmail.com>
2020-08-24 01:08:42 +0900

    update

--- cpp/digit_number.cpp
+++ cpp/digit_number.cpp
@@ -1,23 +1,32 @@
+
 // ysoftman
 // 자리수 숫자 파싱
 #include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+void parseDigitNumber(int n)
+{
+    printf("n = %d\n", n);
+    vector<int> vec;
+    while (n >= 1)
+    {
+        vec.push_back(n % 10);
+        n /= 10;
+    }
+    vector<int>::reverse_iterator iter;
+    for (iter = vec.rbegin(); iter != vec.rend(); ++iter)
+    {
+        printf("%d ", *iter);
+    }
+    printf("  digit cnt(%ld)\n", vec.size());
+}
 
 int main()
 {
-    int n = 1230560;
-    int temp = n;
-    int digit_cnt = 1;
-    while (temp >= 10)
-    {
-        temp /= 10;
-        ++digit_cnt;
-    }
-    printf("n = %d, digit_cnt(%d)\n", n, digit_cnt);
-    for (int i = 0; i < digit_cnt; ++i)
-    {
-        printf("%d ", n % 10);
-        n /= 10;
-    }
-    printf("\n");
+    parseDigitNumber(228);
+    parseDigitNumber(1230560);
+    parseDigitNumber(78984568);
     return 0;
 }
