78a6a4245d9ed9505c50a926f487819a19dedd68
ysoftman <ysoftman@gmail.com>
2020-08-26 16:34:52 +0900

    update

--- golang/defer/defer.go
+++ golang/defer/defer.go
@@ -24,4 +24,17 @@ func main() {
 
 	fmt.Println("--------------")
 
+	fmt.Println(aaa())
+}
+
+func aaa() (result string) {
+	// 여러개의 defer 를 사용 할 수 있다.(하지만 트레이싱이 어려워 지양해야 한다.)
+	// defer 는 LIFO 로 b -> a 순으로 출력된다.
+	defer fmt.Println("a")
+	defer fmt.Println("b")
+
+	// defer 에서 변수 할당을 위해서 함수로 구현해 호출해야 한다.
+	// 최종적으로 lemon 이 리턴된다.
+	defer func() { result = "lemon" }()
+	return "appple"
 }
