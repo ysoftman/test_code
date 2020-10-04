4b5abc2827c5f8cd5fb050a68f9d8c7cf999da27
ysoftman <ysoftman@gmail.com>
2020-10-04 23:52:34 +0900

    if test

--- sh/if_test.sh
+++ sh/if_test.sh
@@ -2,7 +2,7 @@
 
 # var="a"
 var="b"
-# var="c"
+echo $var
 if [[ $var == 'a' ]]; then
     echo 'first'
 # elif 를 써야 한다. else if 는 syntax error: unexpected end of file
@@ -10,4 +10,14 @@ elif [[ $var == 'b' ]]; then
     echo 'second'
 else
     echo 'last'
+fi
+
+
+var="ysoftman lemon 123"
+echo $var
+if [[ $var == *"man"* ]]; then
+    echo "contain"
+fi
+if [[ $var == *"lemon 1"* ]]; then
+    echo "contain"
 fi
