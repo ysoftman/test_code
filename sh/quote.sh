b6541ddc9bd4422fa118992b5a29982e41a96a1a
ysoftman <ysoftman@gmail.com>
2020-10-11 02:42:39 +0900

    test quote

---
+++ sh/quote.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "ysoftman" # ysoftman
+echo 'ysoftman' # ysoftman
+echo "'ysoftman'"   # 'ysoftman'
+echo '"ysoftman"'   # "ysoftman"
+echo "''ysoftman''" # ''ysoftman''
+
+# single quote in single quote
+# 방법1
+# 이전 블럭은 '' 로 닫고 "'" 로 ' 를 출력하는 방법
+echo 'a' "'"'ysoftman'"'" 'b'   # a 'ysoftman' b
+
+# 방법2
+# bash 2.4 이후 $로 시작하면 escape 로 처리할 수 있다.
+echo $'a \'ysoftman\' b'    # a 'ysoftman' b
