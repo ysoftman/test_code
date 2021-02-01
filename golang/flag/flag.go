897ea7d9ac9af86902d727c680d2193cbf5d09e6
ysoftman <ysoftman@gmail.com>
2021-02-01 11:28:11 +0900

    flag test

--- golang/flag/flag.go
+++ golang/flag/flag.go
@@ -9,20 +9,29 @@ import (
 
 func main() {
 	fmt.Println("flag test...")
-
+	// 플래스(옵션)명이 틀리거나하면 usage 문구와
+	// 각 옵션별 사용예시문구(디폴트값)가 출력된다.
 	// 문자열을 옵션 -a 설정, 디폴트값, 사용예시
 	a := flag.String("a", "abc", "-a abc")
 	// 정수형 옵션 -b 설정, 디폴트값, 사용예시
 	b := flag.Int("b", 999, "-b 999")
 	// time.Duration(int64, nanosecond) 옵션 -c 설정
 	c := flag.Duration("c", 100, "-c 100")
+	d := flag.Bool("d", false, "false|true")
 	// flag 에 설정된 값들 파싱하기
 	flag.Parse()
 
 	fmt.Printf("a=%s\n", *a)
 	fmt.Printf("b=%d\n", *b)
 	fmt.Println("c=", *c)
+	fmt.Println("d=", *d)
 
-	// 플래스(옵션)명이 틀리거나하면 usage 문구와
-	// 각 옵션별 사용예시문구(디폴트값)가 출력된다.
+	getfalg()
+}
+
+func getfalg() {
+	fmt.Println("getlfag a:", flag.Lookup("a").Value.String())
+	fmt.Println("getlfag b:", flag.Lookup("b").Value.String())
+	fmt.Println("getlfag c:", flag.Lookup("c").Value.String())
+	fmt.Println("getlfag d:", flag.Lookup("d").Value.String())
 }
