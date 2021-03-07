fd9d989f2d5de4eee843bf75e05e2dcba8bb6032
ysoftman <ysoftman@gmail.com>
2021-03-07 11:20:34 +0900

    add fibonacci2

--- rust/fibonacci/fibonacci.rs
+++ rust/fibonacci/fibonacci.rs
@@ -17,4 +17,23 @@ fn main() {
         // i 는 immutable 변수라 할당 할 수 없다.
         // i += 1;
     }
+
+    println!("fibonacci2:10 ----> {} ", fibonacci2(10));
+}
+
+fn fibonacci2(n: i32) -> i32 {
+    let mut ppre = 0;
+    let mut pre = 1;
+    let mut result = 0;
+    for i in 0..=n {
+        if i <= 1 {
+            result = i;
+        } else {
+            result = ppre + pre;
+        }
+        println!("{}:{}", i, result);
+        ppre = pre;
+        pre = result;
+    }
+    result
 }
