5d128cfdddcd26383c7eda23e240c83b74e47412
ysoftman <ysoftman@gmail.com>
2021-03-11 09:25:19 +0900

    mod test

--- rust/my_mod/src/main.rs
+++ rust/my_mod/src/main.rs
@@ -1,5 +1,6 @@
-// my_mod(library crate)사용을 위한 binary crate
+// 이 파일은 my_mod(library crate, lib.rs)사용을 위한 binary crate(main.rs)
 // 이 파일은 my_mod crate 와는 별개의 binary crate 이다.
+
 // 외부의 my_mod 사용
 extern crate my_mod;
 
@@ -18,6 +19,9 @@ mod lemon {
 use lemon::apple::orange;
 use lemon::apple::orange::juice;
 
+// 별도 파일로 된 모듈 사용, 모듈명과 파일명이 같아야 한다.
+mod banana;
+
 fn main() {
     // my_mod unsed function 경고를 없애기 위해 사용
     my_mod::fruit::list();
@@ -30,4 +34,7 @@ fn main() {
 
     // use lemon::apple::orange::juice; 를  사용한 경우
     juice();
+
+    // banana.rs 모듈 함수 실행
+    banana::print_banana();
 }
