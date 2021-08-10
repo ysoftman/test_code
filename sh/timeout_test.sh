acf1a6766407e4adae9bad13fd64b58f2fc20911
ysoftman <ysoftman@gmail.com>
2021-08-10 13:43:51 +0900

    add timeout test

---
+++ sh/timeout_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# timeout duration command command-args
+
+# duration 이후 kill command kill
+# 3초(기본 초단위) 후 sleep kill
+# timeout 3 sleep 10
+
+# 3초후 sleep 에 3(QUIT) 시그널 보내기
+# timeout -s QUIT 3s sleep 10
+timeout 3 bash -c 'echo "timeout 3 sleep 10"; for ((i=1;i<10;i++)) do echo $i; sleep 1; done;'
