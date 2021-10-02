b7813c4bb92cbffbbc485ba524a46ad579b074ee
ysoftman <ysoftman@gmail.com>
2021-10-03 00:26:11 +0900

    choose OpenSSL installation in build.sh

--- openssl/build.sh
+++ openssl/build.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
-rm a.out *.o core*
-if [ $(uname) == 'Linux' ]; then
-    install_path='/home/ysoftman/workspace/test_code/openssltest/openssl-1.0.2'
-elif [ $(uname) == 'Darwin' ]; then
-    install_path='/Users/ysoftman/workspace/test_code/openssltest/openssl-1.0.2'
+
+openssl_version="1.0.2k"
+openssl_install_path="openssl-${openssl_version}"
+install_openssl()
+{
+    rm -rfv openssl-${openssl_version}
+    if [ -f 'openssl-${openssl_version}.tar.gz' ]; then
+        echo 'ok'
+    else
+        echo 'download'
+        wget https://www.openssl.org/source/openssl-${openssl_version}.tar.gz
+    fi
+    tar zxvf openssl-${openssl_version}.tar.gz
+    cd openssl-${openssl_version}
+
+    if [ $(uname) == 'Linux' ]; then
+        ./config --prefix=${openssl_install_path}
+    elif [ $(uname) == 'Darwin' ]; then
+        ./Configure darwin64-x86_64-cc --prefix=${openssl_install_path}
+    fi
+    make clean
+    make -j4 && make install
+    cd ..
+}
+
+
+echo "install openssl library? [y/n]"
+read yesno
+yesno=$(echo $yesno | tr '[:upper:]' '[:lower:]')
+if [ ${yesno} == 'y' ]; then
+    install_openssl
 fi
-g++ -g -fPIC -c cryptography.cpp test_cryptography.cpp -I${install_path}/include
-g++ cryptography.o test_cryptography.o -L${install_path}/lib -lcrypto -lssl -ldl
+
+rm a.out *.o core*
+g++ -g -fPIC -c cryptography.cpp test_cryptography.cpp -I${openssl_install_path}/include
+g++ cryptography.o test_cryptography.o -L${openssl_install_path}/lib -lcrypto -lssl -ldl
