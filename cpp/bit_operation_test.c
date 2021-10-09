f5729e61112524ef20ab2b54c326c6da092dee40
ysoftman <ysoftman@gmail.com>
2021-10-09 11:23:24 +0900

    bit operation test

---
+++ cpp/bit_operation_test.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+
+void print_binary_digit(int num)
+{
+    const int max_length = 10;
+    int temp = num;
+    int d[max_length], idx = 0;
+    memset(d, 0, sizeof(int) * max_length);
+
+    while (temp > 0)
+    {
+        d[idx++] = temp % 2;
+        temp /= 2;
+    }
+    d[idx++] = temp;
+
+    for (int i = (max_length - 1); i >= 0; i--)
+    {
+        printf("%d", d[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int a = 100, b = 128;
+    printf("a: %d, b: %d\n", a, b);
+    int temp = a & b;
+    printf("a&b: %d\n", temp);
+    temp = a | b;
+    printf("a|b: %d\n", temp);
+
+    printf("a=>: ");
+    print_binary_digit(a);
+    printf("b=>: ");
+    print_binary_digit(b);
+
+    printf("b>>1:");
+    print_binary_digit(b >> 1);
+    printf("b>>2:");
+    print_binary_digit(b >> 2);
+    printf("b<<1:");
+    print_binary_digit(b << 1);
+
+    // max_length overflow
+    // printf("b<<2:");
+    // print_binary_digit(b << 2);
+
+    return 0;
+}
