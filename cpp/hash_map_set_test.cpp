3002fce8ef671f918350fe5d5d5989fda841aaf1
ysoftman <ysoftman@gmail.com>
2021-10-10 10:48:03 +0900

    update test

--- cpp/hash_map_set_test.cpp
+++ cpp/hash_map_set_test.cpp
@@ -23,65 +23,80 @@ int main()
     map[0] = 0;
     for (auto i : map)
     {
-        cout << i.first << endl;
+        cout << i.first << ":" << i.second << endl;
     }
-
     cout << "-----" << endl;
 
-    // unordered_map 정렬되지 않지만 O(1) 로 빠르다.
+    // Unordered map is an associative container that contains key-value pairs with unique keys. Search, insertion, and removal of elements have average constant-time complexity.
+    // 정렬되지 않지만 O(1) 로 빠르다.
     unordered_map<int, int> hashmap;
     hashmap[3] = 3;
     hashmap[2] = 2;
     hashmap[5] = 5;
     hashmap[4] = 4;
     hashmap[0] = 0;
-    cout << "hashmap" << endl;
     // 정렬되지 않기 때문에 무작위로 순서로 출력된다.
     for (auto i : hashmap)
     {
-        cout << i.first << endl;
+        cout << i.first << ":" << i.second << endl;
     }
+    cout << "hashmap.size():" << hashmap.size() << endl;
+    cout << "hashmap.count(5):" << hashmap.count(5) << endl;
     if (hashmap.find(3) != hashmap.end())
     {
-        cout << "hashmap found: 3->" << hashmap[3] << endl;
+        cout << "hashmap[3] ->" << hashmap[3] << endl;
     }
-
     cout << "-----" << endl;
 
-    // unordered_set
+    // Unordered_set are containers that store unique elements in no particular order, and which allow for fast retrieval of individual elements based on their value.
     unordered_set<string> hashset;
-    hashset.insert("1.lemon");
-    hashset.insert("2.apple");
-    hashset.insert("3.orange");
-    hashset.insert("3.orange"); // 중복값은 하나로 취급한다.
-    cout << "hashset" << endl;
+    hashset.insert("lemon");
+    hashset.insert("orange");
+    hashset.insert("fineapple");
+    hashset.insert("apple");
+    hashset.insert("BANANA");
+    hashset.insert("orange"); // 중복값은 하나로 취급한다.
     // 정렬되지 않기 때문에 무작위로 순서로 출력된다.
     for (auto i : hashset)
     {
         cout << i << endl;
     }
-    if (hashset.find("2.apple") != hashset.end())
+    cout << "hashset.size():" << hashset.size() << endl;
+    cout << "hashset.count(\"orange\"):" << hashset.count("orange") << endl;
+    if (hashset.find("apple") != hashset.end())
     {
-        cout << "hashset found: "
-             << "2.apple" << endl;
+        cout << "hashset apple is found" << endl;
     }
-
-    // unordered_multiset
+    if (hashset.count("apple") > 0)
+    {
+        cout << "hashset apple exists!" << endl;
+    }
     cout << "-----" << endl;
+
+    // Unordered multisets are containers that store elements in no particular order, allowing fast retrieval of individual elements based on their value, much like unordered_set containers, but allowing different elements to have equivalent values.
     unordered_multiset<string> hashmset;
     hashmset.insert("lemon");
     hashmset.insert("orange");
     hashmset.insert("apple");
-    hashmset.insert("lemon"); // 중복값된 값을 추가할 수 있다.
+    hashmset.insert("lemon");  // 중복값된 값을 추가할 수 있다.
+    hashmset.insert("orange"); // 중복값된 값을 추가할 수 있다.
     hashmset.insert("apple");
     hashmset.insert("lemon");
     for (auto i : hashmset)
     {
         cout << i << endl;
     }
-    if (hashmset.find("lemon") != hashmset.end())
+    cout << "hashmset.size():" << hashmset.size() << endl;
+    cout << "hashmset.count(\"orange\"):" << hashmset.count("orange") << endl;
+    if (hashmset.count("apple") > 0)
     {
-        cout << "hashmset found: lemon" << endl;
+        cout << "hashmset apple exists!" << endl;
     }
+    if (hashmset.find("apple") != hashmset.end())
+    {
+        cout << "hashmset apple is found" << endl;
+    }
+
+    cout << "-----" << endl;
     return 0;
 }
