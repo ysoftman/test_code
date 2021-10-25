e33d7f4793b4fcb3eb7cd837af8a3c5be3a9c089
ysoftman <ysoftman@gmail.com>
2021-10-25 13:48:20 +0900

    update quote test

--- sh/quote.sh
+++ sh/quote.sh
@@ -13,3 +13,19 @@ echo 'a' "'ysoftman'" 'b'   # a 'ysoftman' b
 # 방법2
 # bash 2.4 이후 $로 시작하면 escape 로 처리할 수 있다.
 echo $'a \'ysoftman\' b'    # a 'ysoftman' b
+
+
+
+####
+
+set -x
+
+var="__ysoftman\n
+__apple__\n
+__lemon__"
+
+# '' 안에 변수 치환 예시
+message='{"aaa":"A1", "bbb":"'"${var}"'", "ccc": "C3"}'
+echo -e $message
+
+set -
