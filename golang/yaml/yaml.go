0a401f6a08f17684eb3d19d563464fbbae9e63ef
ysoftman <ysoftman@gmail.com>
2021-11-15 21:58:24 +0900

    marshal/unmarshal yaml test

---
+++ golang/yaml/yaml.go
@@ -0,0 +1,42 @@
+// ysoftman
+// yaml test
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
+func main() {
+
+	yamlData := struct {
+		AA string `yaml:"aa"`
+		BB []int  `yaml:"bb"`
+	}{
+		AA: "aaaa",
+		BB: []int{1, 2, 3, 4, 6},
+	}
+	mYaml, err := yaml.Marshal(&yamlData)
+	if err != nil {
+		log.Fatal("err=> ", err)
+	}
+	fmt.Println(string(mYaml))
+
+	// indent 는 space 로 사용해야 한다.
+	// found a tab character that violates indentation
+	yamlStr := `
+name: ysoftman
+aa:
+   favorites: ["lemon", "apple", "orange"]
+`
+
+	fmt.Println(yamlStr)
+
+	data := make(map[interface{}]interface{})
+	if err := yaml.Unmarshal([]byte(yamlStr), &data); err != nil {
+		log.Fatal("err => ", err)
+	}
+
+}
