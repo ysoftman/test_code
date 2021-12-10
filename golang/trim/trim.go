c5b574b0cfdc277fec9d2621bf4e41e5db48f0b1
ysoftman <ysoftman@gmail.com>
2021-12-10 13:03:08 +0900

    update

--- golang/trim/trim.go
+++ golang/trim/trim.go
@@ -7,17 +7,21 @@ import (
 
 func main() {
 	s := "menu-menu-apple-menu-juice-ordered-ordered"
-	// 앞에서 부터 첫번째 menu- 전체 일치 되는 부분만 삭제
+	// 앞 부터 첫번째 menu- 일치 되는 부분만 한번 삭제
 	fmt.Println(strings.TrimPrefix(s, "menu-"))
 
-	// 앞에서 부터 menu- 문자들중 일치되는 부분까지 삭제
+	// 앞 부터 menu- 일치되는 부분들 (일치 되지 않을때까지) 모두 삭제한 곳 이후의 슬라이스 리턴 s[i:]
 	fmt.Println(strings.TrimLeft(s, "menu-"))
 
-	// 뒤에서 부터 첫번째 -ordered 일치 되는 부분만 삭제
+	// 뒤 부터 첫번째 -ordered 일치 되는 부분만 한번 삭제
 	fmt.Println(strings.TrimSuffix(s, "-ordered"))
 
-	// 뒤에서부터 -ordered 문자들중 일치되는 부분까지 삭제
+	// 뒤 부터 -ordered 일치되는 부분들 (일치 되지 않을때까지) 모두 삭제한 곳 까지의 슬라이스 리턴 s[:i]
+	// s[:i] 로 i번째 문자가 포함되지 않으니 주의
 	fmt.Println(strings.TrimRight(s, "-ordered"))
+
+	// -or 뒤쪽은 모두 삭제
+	fmt.Println(strings.Split(s, "-or")[0])
 
 	// index 를 찾아 apple-muenu-juice 내용만 가져오기
 	first := strings.Index(s, "menu-menu-")
