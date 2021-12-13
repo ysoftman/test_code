7a5025a657205ff561920aee1807fa5997e2f9fc
ysoftman <ysoftman@gmail.com>
2021-12-13 11:00:02 +0900

    update comment

--- sh/ssh_stdin_null.sh
+++ sh/ssh_stdin_null.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 # ysoftman
 
-# 테스트를 위한 임시 파일 생성
-echo '1
-2
-3' > temp.txt
+# 테스트를 위한 임시 서버 리스트 파일 생성
+echo 'server-lemon
+server-apple
+server-orange' > temp_server_list.txt
 
-while read a
+# 여러 호스트에 ssh 명령 실행
+# stdin(< temp_server_list.txt) 파이프라인을 사용중 ssh 로 원격 명령실
+while read server
 do
-    echo ${a}
-    # stdin(< temp.txt) 파이프라인을 사용중 ssh 로 원격 명령실행으로
-    # stdin("hostname") 을 사용하면 기존 stdin(< temp.txt)이 끝나게 되어 루프가 끝나게 된다.
-    # out=`ssh ysoftman@server "hostname"`
+    echo ${server}
+    # stdin("hostname" 명령) 을 사용하면 기존 stdin(< temp_server_list.txt)이 끝나게 되어 루프가 끝나게 된다.
     # < /dev/null 로 stdin 을 사용하지 않도록 한다.
-    # -o "StrictHostKeyChecking no" 호스트 체크 안하기
+    # -o "StrictHostKeyChecking no" 접속시 호스트 체크 안하기
     out=`ssh -o "StrictHostKeyChecking no" ysoftman@server "hostname" < /dev/null`
     echo ${out}
-done < temp.txt
+done < temp_server_list.txt
 
 # 테스트를 위한 임시 파일 삭제
-rm temp.txt
+rm temp_server_list.txt
