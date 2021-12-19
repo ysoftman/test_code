daa73219e6e35181b42d9d00297d7dd98c23d06f
ysoftman <ysoftman@gmail.com>
2021-12-20 00:02:57 +0900

    update

--- golang/rune/rune.go
+++ golang/rune/rune.go
@@ -9,6 +9,18 @@ import (
 )
 
 func main() {
+	// byte
+	a := byte('a')
+	fmt.Printf("type %T %v\n", a, a)
+	// string len 은 string, slice, map, array 등은 되지만 byte, int32, rune 은 안된다.
+	aa := "a"
+	fmt.Printf("type %T %v %v\n", aa, aa, len(aa))
+	// int32 == rune
+	aaa := 'a'
+	fmt.Printf("type %T %v\n", aaa, aaa)
+	aaaa := rune('a')
+	fmt.Printf("type %T %v\n", aaaa, aaaa)
+
 	printRune()
 	cutRuneByMaxByte(7)
 	cutRuneByMaxByte(13)
@@ -23,10 +35,11 @@ func printRune() {
 	// rune 기준 글자 개수
 	fmt.Println("utf8.RuneCountInString(str):", utf8.RuneCountInString(str))
 
-	// golang 에서 str 은 utf8 이고, rune 은 4byte utf-8(한글 3byte)을 담는데 사용한다.
+	// golang 에서 str 은 utf8 이고, str 의 각 요소는 rune 타입이다.
+	// rune 은 int32(4bytes) alias 로, utf-8(영어 1byte, 한글 3bytes 1~4바이트 차지)을 담는데 사용한다.
 	// range 로 스트링에서 개별 룬문자와 개별 룬문자의 시작 위치(index) 알 수 있다.
 	for index, runeValue := range str {
-		fmt.Printf("index(%d) %v %U %#U len(string(%d))\n", index, runeValue, runeValue, runeValue, len(string(runeValue)))
+		fmt.Printf("index(%d) %T %v %U %#U len(string(%d))\n", index, runeValue, runeValue, runeValue, runeValue, len(string(runeValue)))
 	}
 }
 
