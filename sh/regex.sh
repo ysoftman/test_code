99e8dff8f0112c416305040234749e6bd65086c3
ysoftman <ysoftman@gmail.com>
2022-01-05 13:04:04 +0900

    update

--- sh/regex.sh
+++ sh/regex.sh
@@ -8,13 +8,14 @@ aaa-bbb-123
 # bash 에서는 변수를 "" 안에 둬야 newline 이 출력한된다.
 # echo "$sample"
 
-regx1="^[0-9]{3}-[0-9]{3}-[0-9]{3}$"
+# regx1="^[0-9]{3}-[0-9]{3}-[0-9]{3}$"
 regx2="^\([0-9]{3}\) [0-9]{3}-[0-9]{3}$"
 while read line
 do
     echo -n "${line} ==> "
     # =~ 정규식 매칭
-    if [[ ! ${line} =~ $regx1  ]] && [[ ! ${line} =~ $regx2  ]]; then
+    # 주의 정규표현식을 "" 로 묶으면 스트링으로 취급되니 주의!
+    if [[ ! ${line} =~ ^[0-9]{3}-[0-9]{3}-[0-9]{3}$ ]] && [[ ! ${line} =~ $regx2  ]]; then
         echo "invalid"
     else
         echo "valid"
