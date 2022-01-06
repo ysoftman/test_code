00746f1abd6a3eaf28d7215a28821d274ff11437
ysoftman <ysoftman@gmail.com>
2022-01-06 16:55:29 +0900

    sed -i options test

--- sh/sed_edit_file2.sh
+++ sh/sed_edit_file2.sh
@@ -39,9 +39,16 @@ echo 'delete pattern ccc ~ ggg and print'
 cat ${TEMP_FILE} | sed "/^ccc/,/^ggg/d;"
 
 # ${TEMP_FILE} 에서 ysoftman -> bill 로 변경해서 저장
+# -i 확장자 명시하면 수정결과를 파일로 저장하고 수정전의 원본파일.확장자 파일로 저장한다.
+# -i '' (mac)
+# 또는 -i 로 옵션값이 없으면 (linux)
+# 원본 백업 파일을 생성하지 않고 대상 파일을 변경한다.
 echo 'replace ysoftman to bill in '${TEMP_FILE}
-# -i '' 또는 -i 로 옵션값이 없으면 원본 백업 파일을 생성하지 않고 대상 파일을 변경한다.
-sed -i '' -e 's/ysoftman/bill/g' ${TEMP_FILE}
+if [[ $(uname -s) =~ ^Darwin.* ]]; then
+    sed -i '' -e 's/ysoftman/bill/g' ${TEMP_FILE}
+elif [[ $(uname -s) =~ ^Linux.* ]]; then
+    sed -i -e 's/ysoftman/bill/g' ${TEMP_FILE}
+fi
 cat ${TEMP_FILE}
 
 # 임시 파일 삭제
