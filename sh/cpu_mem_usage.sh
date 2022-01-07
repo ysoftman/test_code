d8a43594aa3ede20fdf8f2373b48afe47d09357b
ysoftman <ysoftman@gmail.com>
2022-01-07 12:17:54 +0900

    update comment

--- sh/cpu_mem_usage.sh
+++ sh/cpu_mem_usage.sh
@@ -13,6 +13,11 @@ cpu_usage=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {pr
 mem_usage=$(free | grep -i mem | awk '{print (($2-$7)/$2)*100}')%;
 
 # buff/cache 합쳐진 컬럼이 아닌 경우
+#              total       used       free     shared    buffers     cached
+# Mem:           15G       2.9G        12G       399M       156M       2.1G
+# -/+ buffers/cache:       746M        14G
+# Swap:         1.0G         0B       1.0G
+
 # free memory  = (total - used + buffer + cache)
 # memory utilized = ((total-free)/total*100)
 if [[ $(free | grep -i 'buff/cache') == "" ]]; then
