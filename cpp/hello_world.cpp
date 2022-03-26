0ca56d5d04509df593aaf93075efb803e2157818
ysoftman <ysoftman@gmail.com>
2022-03-26 19:41:42 +0900

    hello world test

---
+++ cpp/hello_world.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+// using namespace std;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+    string a = "1";
+    if (a == "1")
+    {
+        cout << "hello world" << endl;
+    }
+    return 0;
+}
