dd1f5224d2dd85e636cde2f46ce0c86708a04752
ysoftman <ysoftman@gmail.com>
2022-04-18 17:55:28 +0900

    update

--- python/string_test.py
+++ python/string_test.py
@@ -5,12 +5,12 @@
 
 
 def modify_data(str):
-    str = 'kkk'
-    print('modified')
+    str = "kkk"
+    print("modified")
     return str
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     # 큰 따옴표 "  를 포함 시키고 싶을때
     str = '"ysoftman"'
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     print(str)
 
     # 여러줄로 표현하고 싶은 경우
-    str = 'yoon\nbyoung\nhoon'
+    str = "yoon\nbyoung\nhoon"
     print(str)
 
     # 이스케이프 문자 없이 여러줄료 표현하고 싶은 경우
@@ -30,31 +30,31 @@ byoung
 hoon"""
     print(str)
 
-    str = '''yoon
+    str = """yoon
 byoung
-hoon'''
+hoon"""
     print(str)
 
     # 스트링 포맷팅
-    data = {'first': 'bill', 'second': 'yoon'}
-    print('{first} {second}'.format(**data))
+    data = {"first": "bill", "second": "yoon"}
+    print("{first} {second}".format(**data))
     print("{first} {second}".format(**data))
 
     # 스트링 복사
-    str = 'abc'
+    str = "abc"
     str2 = str
-    print('str =', str)
-    print('str2 =', str2)
+    print("str =", str)
+    print("str2 =", str2)
 
-    str = 'zzz'
-    print('str =', str)
-    print('str2 =', str2)
+    str = "zzz"
+    print("str =", str)
+    print("str2 =", str2)
 
     str = modify_data(str)
-    print('str =', str)
-    print('str2 =', str2)
+    print("str =", str)
+    print("str2 =", str2)
 
-    str = 'yOOn Byoung hooN'
+    str = "yOOn Byoung hooN"
     print(str)
     # 모두 소문자로
     str = str.lower()
@@ -68,23 +68,33 @@ hoon'''
     # 시작문자 존재 체크
     # false
     print("str.startswith", str.startswith("aaaaaaaaaaaaaaaaaaa"))
-    # 0번째 위치에서 찾기
+    # 0번째 위치 이후에서 체크
     print("str.startswith", str.startswith("Yoon b"))
-    # 5번째 위치에서 찾기
+    # 5번째 위치 이후에서 체크
     print("str.startswith", str.startswith("byoung", 5))
 
+    # 끝문자 존재 체크
+    print(str)
+    print("str.endswith", str.lower().endswith("hoon"))
+    # 20번째 이후에서 체크
+    print("str.endswith", str.lower().endswith("hoon", 20))
+    # apple,lemon,hoon 중 하나로 끝나는지 체크
+    print("str.endswith", str.endswith(("apple", "lemon", "hoon")))
+
     str = "윤\n병\n훈"
-    print('str = {}'.format(str))
+    print("str = {}".format(str))
     # raw 문자: escape 문자의 기능이 적용되지 않고 문자 그대로 출력
     str = r"윤\n병\n훈"
-    print('str = {}'.format(str))
+    print("str = {}".format(str))
 
     # 멀티라인에서 포맷팅
     str = """
         "number:{},
         "name":{}
-    """.format(123, "ysoftman")
-    print('str =', str)
+    """.format(
+        123, "ysoftman"
+    )
+    print("str =", str)
 
     # 멀티라인에서 {} 자체를 출력하기 위해선 {{ }} 사용
     # .3f 소수점3째자리로 표현
@@ -97,10 +107,12 @@ hoon'''
         "number:{0:,.3f},
         "name":{1}
     }}
-    """.format(123456789, "ysoftman")
-    print('str =', str)
+    """.format(
+        123456789, "ysoftman"
+    )
+    print("str =", str)
 
     # 문자열 앞,뒤 공백 제거
     str = "   ysoftman     "
-    print('str =', str, "okay")
-    print('str =', str.strip(), "okay")
+    print("str =", str, "okay")
+    print("str =", str.strip(), "okay")
