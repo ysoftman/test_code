151934de8b6528a29d2816975ff1a3ae7d90c263
ysoftman <ysoftman@gmail.com>
2022-04-21 21:20:20 +0900

    fix typo

--- python/unit_test.sh
+++ python/unit_test.sh
@@ -6,7 +6,7 @@ python unit_test.py
 # 방법2 - unittest 모듈 이용
 python -m unittest -v  unit_test
 
-# 방법3 - green 패키키 사용
+# 방법3 - green 패키지 사용
 # pip install green
 # python 실행시 다음과 같이 green 으로 수행하면 된다.
 # -v 옵션에 v 가 많을 수록 좀더 자세한 결과가 보여진다.
