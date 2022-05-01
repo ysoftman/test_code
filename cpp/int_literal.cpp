6204fce4f1b4f4f3e407dd6eec30935e62e43c32
ysoftman <ysoftman@gmail.com>
2022-05-01 11:57:42 +0900

    int literal test

---
+++ cpp/int_literal.cpp
@@ -0,0 +1,14 @@
+// ysoftman
+// digit literal test
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+    cout << 10000 << endl;
+    // https://en.cppreference.com/w/cpp/language/integer_literal
+    cout << 10'000 << endl; // c++14 부터 지원
+}
