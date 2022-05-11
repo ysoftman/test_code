bcd69ba6ddede5e8fefccc4a6bab32f530eb2252
ysoftman <ysoftman@gmail.com>
2022-05-11 14:18:18 +0900

    k8s python client test

---
+++ python/k8s_python_client.py
@@ -0,0 +1,29 @@
+# pip install kubernetes
+import os
+
+from kubernetes import client, config
+
+
+class color:
+    default = "\033[0m"
+    black = "\033[0;30m"
+    red = "\033[0;31m"
+    green = "\033[0;32m"
+    yellow = "\033[0;33m"
+    blue = "\033[0;34m"
+    purple = "\033[0;35m"
+    cyan = "\033[0;36m"
+    white = "\033[0;37m"
+
+
+# KUBECONFIG 환경 변수 사용 없으면, default: ~/.kube/config
+cfgfile = os.environ.get("KUBECONFIG", "~/.kube/config")
+print(cfgfile)
+config.load_kube_config(cfgfile)
+
+v1 = client.CoreV1Api()
+ns = v1.list_pod_for_all_namespaces(watch=False)
+for item in ns.items:
+    print(
+        f"{color.green}{item.status.pod_ip}, {color.yellow}{item.metadata.namespace}, {color.blue}{item.metadata.name}{color.default}"
+    )
