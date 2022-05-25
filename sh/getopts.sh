eeb601f734e9b4265fa4ee0d81c7d85323a446f1
ysoftman <ysoftman@gmail.com>
2022-05-25 20:54:07 +0900

    update

--- sh/getopts.sh
+++ sh/getopts.sh
@@ -3,19 +3,19 @@
 # 실행 예
 # short(한글자) 옵션명 -a -b
 # long(여러자) 옵션명 --ccc
-# bash ./getopts.sh -a lemon -b apple --ccc ysoftman
+# bash ./getopts.sh -a lemon -b apple -c orange --ccc ysoftman
 
 # 다음 처리되어야 옵션 index
 # echo ${OPTIND}
-# 옵션 값
+# 옵션 값
 # echo ${OPTARG}
 
 # : 로 끝나면 arg값이 있는 경우 옵션이다.
 # getopts 는 short 옵션만 처리가능하다.
-while getopts "a:b:" opt; do
+while getopts "a:b:c" opt; do
     case ${opt} in
-        'a')
-            echo "a ${OPTIND} ${OPTARG}"
+        a|c)
+            echo "a|c ${OPTIND} ${OPTARG}"
             ;;
         'b')
             echo "b ${OPTIND} ${OPTARG}"
