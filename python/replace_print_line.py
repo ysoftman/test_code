895bb47c1762af67da5b14a34dff129b7a532954
ysoftman <ysoftman@gmail.com>
2022-05-29 15:38:54 +0900

    update

--- python/replace_print_line.py
+++ python/replace_print_line.py
@@ -5,20 +5,21 @@ import signal
 # https://docs.python.org/3/howto/curses.html
 import curses
 
-# for i in range(2):
-#     # print(f"\rtesting...{i}")
-#     # newline(\n) 대신 carriage return(\r) 를 사용해 다음 출력도 같은 라인에서 시작하도록 한다.
-#     print(f"testing1...{i}s", end="\r")
-#     time.sleep(1)
+for i in range(2):
+    # print(f"\rtesting...{i}")
+    # newline(\n) 대신 carriage return(\r) 를 사용해 다음 출력도 같은 라인에서 시작하도록 한다.
+    print(f"testing1...{i}s", end="\r")
+    time.sleep(1)
 
-# for i in range(2):
-#     sys.stdout.write(f"\rtesting2...{i}s")
-#     sys.stdout.flush()
-#     time.sleep(1)
+for i in range(2):
+    sys.stdout.write(f"\rtesting2...{i}s")
+    sys.stdout.flush()
+    time.sleep(1)
 
 
 # curses 를 사용하면 좀더 세밀하게 조정할 수 있다.
 stdscr = curses.initscr()
+curses.start_color()
 curses.noecho()
 stdscr.clear()
 
@@ -31,11 +32,13 @@ def signal_handler(sig, frame):
 
 
 signal.signal(signal.SIGINT, signal_handler)
+curses.init_pair(1, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_YELLOW)
 for i in range(3):
-    stdscr.addstr(0, 0, f"testing1....{i}s\n")
-    stdscr.addstr(1, 0, f"testing2....{i}s")
+    stdscr.addstr(0, 0, f"testing1....{i}s\n", curses.color_pair(1))
+    stdscr.addstr(1, 0, f"testing2....{i}s", curses.color_pair(2))
     lastyx = stdscr.getyx()
-    stdscr.addstr(2,0, f"last positiion(y:{lastyx[0]}, x:{lastyx[1]})")
+    stdscr.addstr(2, 0, f"last positiion(y:{lastyx[0]}, x:{lastyx[1]})")
     stdscr.refresh()
     time.sleep(1)
 
