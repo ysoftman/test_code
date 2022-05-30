36df281ca1c1d3d08f624e141733006096a283ad
ysoftman <ysoftman@gmail.com>
2022-05-30 16:42:52 +0900

    update

--- python/human_readable_timeformat.py
+++ python/human_readable_timeformat.py
@@ -1,13 +1,18 @@
 from calendar import month, week
 from datetime import datetime, timedelta
+from sqlite3 import Timestamp
 
 # docker cli 참고 https://github.com/docker/cli/blob/master/vendor/github.com/docker/go-units/duration.go#L12
 def to_human_readable_timeformat(timestr: str) -> str:
     if timestr is None:
         return None
+
+    # tz = datetime.strptime(timestr, "%Y-%m-%dT%H:%M:%S%z").utcoffset()
     d = (
-        datetime.now().timestamp()
-        - datetime.strptime(timestr, "%Y-%m-%dT%H:%M:%S%z").timestamp()
+        datetime.utcnow().timestamp()
+        - datetime.utcfromtimestamp(
+            datetime.strptime(timestr, "%Y-%m-%dT%H:%M:%S%z").timestamp()
+        ).timestamp()
     )
     seconds = int(d)
     minutes = int(seconds / 60)
@@ -38,14 +43,97 @@ def to_human_readable_timeformat(timestr: str) -> str:
 
 print(to_human_readable_timeformat("2010-12-31T17:25:10+09:00"))
 # print((datetime.now() - timedelta(days=10)).strftime("%Y-%m-%dT%H:%M:%S+09:00"))
-print(to_human_readable_timeformat((datetime.now() - timedelta(weeks=120)).strftime("%Y-%m-%dT%H:%M:%S+09:00")))
-print(to_human_readable_timeformat((datetime.now() - timedelta(weeks=50)).strftime("%Y-%m-%dT%H:%M:%S+09:00")))
-print(to_human_readable_timeformat((datetime.now() - timedelta(weeks=3)).strftime("%Y-%m-%dT%H:%M:%S+09:00")))
-print(to_human_readable_timeformat((datetime.now() - timedelta(days=100)).strftime("%Y-%m-%dT%H:%M:%S+09:00")))
-print(to_human_readable_timeformat((datetime.now() - timedelta(days=12)).strftime("%Y-%m-%dT%H:%M:%S+09:00")))
-print(to_human_readable_timeformat((datetime.now() - timedelta(hours=5)).strftime("%Y-%m-%dT%H:%M:%S+09:00")))
-print(to_human_readable_timeformat((datetime.now() - timedelta(minutes=30)).strftime("%Y-%m-%dT%H:%M:%S+09:00")))
-print(to_human_readable_timeformat((datetime.now() - timedelta(seconds=90)).strftime("%Y-%m-%dT%H:%M:%S+09:00")))
-print(to_human_readable_timeformat((datetime.now() - timedelta(seconds=59)).strftime("%Y-%m-%dT%H:%M:%S+09:00")))
-print(to_human_readable_timeformat((datetime.now() - timedelta(seconds=0.2)).strftime("%Y-%m-%dT%H:%M:%S+09:00")))
-print(to_human_readable_timeformat((datetime.now() - timedelta(milliseconds=100)).strftime("%Y-%m-%dT%H:%M:%S+09:00")))
+print(
+    to_human_readable_timeformat(
+        (datetime.now() - timedelta(weeks=120)).strftime("%Y-%m-%dT%H:%M:%S+09:00")
+    )
+)
+print(
+    to_human_readable_timeformat(
+        (datetime.now() - timedelta(weeks=50)).strftime("%Y-%m-%dT%H:%M:%S+09:00")
+    )
+)
+print(
+    to_human_readable_timeformat(
+        (datetime.now() - timedelta(weeks=3)).strftime("%Y-%m-%dT%H:%M:%S+09:00")
+    )
+)
+print(
+    to_human_readable_timeformat(
+        (datetime.now() - timedelta(days=100)).strftime("%Y-%m-%dT%H:%M:%S+09:00")
+    )
+)
+print(
+    to_human_readable_timeformat(
+        (datetime.now() - timedelta(days=12)).strftime("%Y-%m-%dT%H:%M:%S+09:00")
+    )
+)
+print(
+    to_human_readable_timeformat(
+        (datetime.now() - timedelta(hours=5)).strftime("%Y-%m-%dT%H:%M:%S+09:00")
+    )
+)
+print(
+    to_human_readable_timeformat(
+        (datetime.now() - timedelta(minutes=30)).strftime("%Y-%m-%dT%H:%M:%S+09:00")
+    )
+)
+print(
+    to_human_readable_timeformat(
+        (datetime.now() - timedelta(seconds=90)).strftime("%Y-%m-%dT%H:%M:%S+09:00")
+    )
+)
+print(
+    to_human_readable_timeformat(
+        (datetime.now() - timedelta(seconds=59)).strftime("%Y-%m-%dT%H:%M:%S+09:00")
+    )
+)
+print(
+    to_human_readable_timeformat(
+        (datetime.now() - timedelta(seconds=0.2)).strftime("%Y-%m-%dT%H:%M:%S+09:00")
+    )
+)
+print(
+    to_human_readable_timeformat(
+        (datetime.now() - timedelta(milliseconds=100)).strftime(
+            "%Y-%m-%dT%H:%M:%S+09:00"
+        )
+    )
+)
+
+
+print("-----test-----")
+print("utcnow():", datetime.utcnow())
+print("utcnow().timestamp():", datetime.utcnow().timestamp())
+print("now():", datetime.now())
+print("now().timestamp():", datetime.now().timestamp())
+# localtime -> utc
+print(
+    "utcfromtimestamp(utcnow):",
+    datetime.utcfromtimestamp(datetime.utcnow().timestamp()),
+)
+print("utcfromtimestamp(now):", datetime.utcfromtimestamp(datetime.now().timestamp()))
+print(
+    "utcfromtimestamp(now+9:00):",
+    datetime.utcfromtimestamp(datetime.now().timestamp() + 60 * 60 * 9),
+)
+
+print(datetime.strptime("2010-12-31T17:25:10+09:00", "%Y-%m-%dT%H:%M:%S%z").utcoffset())
+
+a = datetime.utcfromtimestamp(
+    datetime.strptime("2010-12-31T17:25:10-09:00", "%Y-%m-%dT%H:%M:%S%z").timestamp()
+)
+print(a)
+print(a.timestamp())
+
+a = datetime.utcfromtimestamp(
+    datetime.strptime("2010-12-31T17:25:10+09:00", "%Y-%m-%dT%H:%M:%S%z").timestamp()
+)
+print(a)
+print(a.timestamp())
+
+a = datetime.utcfromtimestamp(
+    datetime.strptime("2010-12-31T17:25:10Z", "%Y-%m-%dT%H:%M:%S%z").timestamp()
+)
+print(a)
+print(a.timestamp())
