5a9f3ada822143bb13b950c5752ad6f1db6ee4a2
ysoftman <ysoftman@gmail.com>
2022-06-08 10:14:19 +0900

    update

--- react/ysoftman-react-app/src/App.js
+++ react/ysoftman-react-app/src/App.js
@@ -14,12 +14,37 @@ function Create(props) {
       props.onCreate(name, cost)
     }}>
       <p><input type="text" name="name" placeholder='title'></input></p>
-      <p><textarea name="cost" placeholder='body'></textarea></p>
+      <p><textarea name="cost" placeholder='cost'></textarea></p>
       <p><input type="submit" value="Create"></input></p>
     </form>
   </article>
 }
 
+// Update Component, (컴포넌트는 대문자로 시작해야 한다.)
+function Update(props) {
+  const [name, setName] = useState(props.name)
+  const [cost, setCost] = useState(props.cost)
+  return <article>
+    <h2>Update</h2>
+    <form onSubmit={event => {
+      event.preventDefault() // reload 방지
+      const name = event.target.name.value // target(form)의 태그(name)의 값(value)
+      const cost = event.target.cost.value // target(form)의 태그(name)의 값(value)
+      props.onUpdate(name, cost)
+    }}>
+      <p><input type="text" name="name" placeholder='name' value={name} onChange={event => {
+        // console.log("event:", event.target.value)
+        setName(event.target.value)
+      }}></input></p>
+      <p><textarea name="cost" placeholder='cost' value={cost} onChange={event => {
+        // console.log("event:", event.target.value)
+        setCost(event.target.value)
+      }}></textarea></p>
+
+      <p><input type="submit" value="Update"></input></p>
+    </form>
+  </article>
+}
 // MyHeader Component, (컴포넌트는 대문자로 시작해야 한다.)
 function MYHeader(props) {
   return <header>
@@ -38,9 +63,9 @@ function MyContents(props) {
     let l = props.items[i]
     // console.log("l:", l)
     mylist.push(<li key={l.id}>
-      <a name={l.name} href="/" onClick={(event) => {
+      <a id={l.id} name={l.name} href="/" onClick={(event) => {
         event.preventDefault()
-        props.onChangeMode222(event.target.name) // target=>여기선 a 태그
+        props.onChangeMode222(event.target.id, event.target.name) // target=>여기선 a 태그
       }}>{l.name} - {l.cost}</a>
     </li>)
   }
@@ -52,26 +77,41 @@ function MyContents(props) {
     </ol>
   </article>
 }
-
 function App() {
   // 초기값 lemon 을 갖는 state 생성
   // state 는 2길이의 0번째는 값, 1번째는 설정함수를 가진다.
-  const [mode, setMode] = useState("lemon")
-  const [click_name, setId] = useState(null)
+  const [mode, setMode] = useState("")
+  const [click_id, setClickID] = useState(null)
   const [mylist, setMylist] = useState([
     { id: 1, name: 'lemon', cost: 100 },
     { id: 2, name: 'apple', cost: 200 },
     { id: 3, name: 'orange', cost: 300 },
   ])
   const [nextId, setNextId] = useState(4);
-
+  let updateDeleteComponent = null
   let message = null
-  if (mode === "lemon") {
-    message = "mode:lemon Yellow, click_id:" + click_name
-  } else if (mode === "apple") {
-    message = "mode:apple, RED, click_id:" + click_name
-  } else if (mode === "orange") {
-    message = "mode:orange, Orange, click_id:" + click_name
+  if (mode === "read") {
+    message = "click_id:" + click_id
+    // <> 빈태그를 사용하면 그룹핑 할 수 있다.
+    updateDeleteComponent = <>
+      <li><a href={'/' + click_id} onClick={event => {
+        event.preventDefault()
+        setMode("update")
+      }}>upate</a></li>
+      <li><input type="button" value="delete" onClick={() => {
+        // 삭제할 아이템 빼고 새로우 리스트 구성
+        const newMylist = []
+        console.log("click_id:", click_id)
+        for (let i = 0; i < mylist.length; i++) {
+          if (mylist[i].id !== Number(click_id)) {
+            newMylist.push(mylist[i])
+          }
+        }
+        console.log("--->", newMylist)
+        setMylist(newMylist)
+        setMode("read")
+      }}></input></li>
+    </>
   } else if (mode === "create") {
     message = <Create onCreate={(_name, _cost) => {
       // 신규 아이템
@@ -82,8 +122,32 @@ function App() {
       newList.push(newItem)
       // mylist state 변경
       setMylist(newList)
+
+      // ID는 편의상 계속 증가하는 방식으로...
       setNextId(nextId + 1)
     }}></Create> // create 컴포넌트 사용
+  } else if (mode === "update") {
+    let _name, _cost = null
+    for (let i = 0; i < mylist.length; i++) {
+      if (mylist[i].id === Number(click_id)) {
+        _name = mylist[i].name
+        _cost = mylist[i].cost
+      }
+    }
+    message = <Update name={_name} cost={_cost} onUpdate={(_name, _cost) => {
+      const updatedItem = { id: Number(click_id), name: _name, cost: _cost }
+      // 기존 mylist 복제
+      const newList = [...mylist]
+      // 기존 아이템을 업데이트된 아이템으로 변경
+      for (let i = 0; i < mylist.length; i++) {
+        if (mylist[i].id === Number(click_id)) {
+          newList[i] = updatedItem
+          break
+        }
+      }
+      setMylist(newList)
+      setMode("read")
+    }}></Update>// update 컴포넌트 사용
   }
 
   // return default_App()
@@ -91,17 +155,22 @@ function App() {
     <MYHeader title123="my title" onChangeMode111={() => {
       alert("test")
     }}> </MYHeader>
-    <MyContents items={mylist} onChangeMode222={(val) => {
-      alert("val:" + val)
-      setMode(val) // App() 컴포넌트를 다시 실행한다.
-      setId(val)
+    <MyContents items={mylist} onChangeMode222={(id, name) => {
+      // console.log("id:", id, "name:", name)
+      setMode("read") // App() 컴포넌트를 다시 실행한다.
+      setClickID(id) // click_id 변경
     }}></MyContents>
     {message}
     <br></br>
-    <a href="/" onClick={(event) => {
-      event.preventDefault()
-      setMode("create")
-    }}>create</a>
+    <ul>
+      <li>
+        <a href="/" onClick={(event) => {
+          event.preventDefault()
+          setMode("create")
+        }}>create</a>
+      </li>
+      {updateDeleteComponent}
+    </ul>
   </div>
 
 }
