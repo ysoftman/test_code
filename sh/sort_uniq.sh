58ec2977122c5527963a3eef5e4944ece4216459
ysoftman <ysoftman@gmail.com>
2022-07-08 13:33:54 +0900

    fix typo

--- sh/sort_uniq.sh
+++ sh/sort_uniq.sh
@@ -22,7 +22,7 @@ echo -e $out | sort -k 2
 echo '[after sort -t "=" -k 2]'
 echo -e $out | sort -t '=' -k 2
 
-# uniq 는 인접한 라인과 비교해 반복되는것은 필터링 시키디 때문에 sort 이후에 사용해야 한다.
+# uniq 는 인접한 라인과 비교해 반복되는것은 필터링 시키기 때문에 sort 이후에 사용해야 한다.
 # 빈라인도 하나의 uniq 로 구분된다.
 echo "lemon
 apple
