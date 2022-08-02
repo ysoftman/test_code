65c2245fd143e68f169b783459a505efa6440887
ysoftman <ysoftman@gmail.com>
2022-08-02 11:44:35 +0900

    rg test

---
+++ sh/rg.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# https://github.com/BurntSushi/ripgrep (rg) 관련 테스트
+
+# 사용법은 grep 과 비슷, 디폴트로 정규식 패턴을 사용한다.
+# 주요 옵션
+# -i, --ignore-case
+# -g, --glob GLOB ..
+# -c, --count
+# -t, --type TYPE ... 
+# -I, --no-filename
+# -A, --after-context NUM
+# -B, --before-context NUM
+# -C, --context NUM
+# -v, --invert-match
+# --hidden, Search hidden files and directories. By default, hidden files and directories are skipped.
+# --type-list Show all supported file types and their corresponding globs.
+
+# ysoftman 이 있는 라인 출력(디폴트 파일명도 같이 출력)
+rg ysoftman
+
+# ysoftman 이 있는 라인 출력, 파일명 출력 제외
+rg ysoftman --no-filename
+
+# .go 파일에서 easy or medium or hard 가 있는 라인 카운트
+rg -i "easy|medium|hard" --glob *.go -c
+
+# .go 파일에서 easy or medium or hard 가 없는 라인 카운트
+# -c 옵션에 zero(0)인 경우 출력되지 않아 
+# --files-without-match 를 사용해야 한다.
+rg -i "easy|medium|hard" --glob *.go --files-without-match
