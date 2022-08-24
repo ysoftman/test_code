81fc46dc7bfefcb2205097614efa352a205f2b81
ysoftman <ysoftman@gmail.com>
2022-08-24 12:32:07 +0900

    add disk info field

--- k8s/get_node_disk_usage.sh
+++ k8s/get_node_disk_usage.sh
@@ -5,6 +5,7 @@
 nodes=$(kubectl get nodes | grep -vi name | awk '{print $1}')
 for node in ${nodes}; do
     echo "[node:${node}]"
-    temp+=$(kubectl get --raw /api/v1/nodes/${node}/proxy/stats/summary | jq '.node | {"node": .nodeName, "fs_usedBytes": .fs.usedBytes, "runtime_imageFs_usedBytes": .runtime.imageFs.usedBytes}')
+    temp+=$(kubectl get --raw /api/v1/nodes/${node}/proxy/stats/summary | jq '.node | {"node": .nodeName, "fs_capacityBytes": .fs.capacityBytes, "fs_availableBytes": .fs.availableBytes, "fs_usedBytes": .fs.usedBytes, "runtime_imageFs_usedBytes": .runtime.imageFs.usedBytes}')
 done
-echo $temp | jq -s 'sort_by(.fs_usedBytes)'
+# echo $temp | jq -s 'sort_by(.fs_usedBytes)'
+echo $temp | jq
