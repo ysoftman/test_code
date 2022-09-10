8c434c26ef79dc50f2b557667effb5e9af1682f7
ysoftman <ysoftman@gmail.com>
2022-09-10 11:39:32 +0900

    update redirection test

--- sh/redirect_stdout_and_stderr.sh
+++ sh/redirect_stdout_and_stderr.sh
@@ -14,4 +14,6 @@ touch exist_file
 # rm -fv exist_file not_exist_file 2>&1 /dev/null
 # 다음과 같이 /dev/null 뒤에 2>&1 을 명시해서 stdout, stderr 리다이렉트할(버릴) 수 있다.
 # fd(/dev/null)이 먼저 파악되어 stdout > fd 로 갈 수 있도록 한 상태에서, stderr(2) > stdout(1) 로 보내야 한다.
-rm -fv exist_file not_exist_file > /dev/null 2>&1
+rm -fv exist_file > /dev/null 2>&1
+# rm -f 옵션을 사용하면 없는 파일도 에러메시지 없이 동작해서 뺏음
+rm -v not_exist_file > /dev/null 2>&1
