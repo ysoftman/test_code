1849ae8c5046c20a11fba809b3f259cb28dae804
ysoftman <ysoftman@gmail.com>
2022-10-21 18:59:55 +0900

    fix typo

--- sh/variable_global_local.sh
+++ sh/variable_global_local.sh
@@ -5,7 +5,7 @@
 global_val="lemon"
 
 function aaa() {
-    # local 을 사용하면 해등 스코프에서만 유효한 지역 변수가 된다.
+    # local 을 사용하면 해당 스코프에서만 유효한 지역 변수가 된다.
     local local_val="orange"
     echo ${global_val}
     echo ${local_val}
