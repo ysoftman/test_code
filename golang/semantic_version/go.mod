ce3fdd51660d57f923a72e131d1b4f84c8d72a23
ysoftman <ysoftman@gmail.com>
2022-11-04 10:07:58 +0900

    semantic version package test

---
+++ golang/semantic_version/go.mod
@@ -0,0 +1,15 @@
+module semantic_version
+
+go 1.19
+
+require (
+	github.com/Masterminds/semver v1.5.0
+	github.com/stretchr/testify v1.8.1
+	golang.org/x/mod v0.6.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ golang/semantic_version/semantic_version_test.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/Masterminds/semver"
+	mod_semver "golang.org/x/mod/semver"
+)
+
+func TestSemver0(t *testing.T) {
+	// "golang.org/x/mod/semver" 에서는 버전명은 v로 시작해야지 된다.
+	assert.Equal(t, false, mod_semver.IsValid("a.b.c"))
+	assert.Equal(t, false, mod_semver.IsValid("1"))
+	assert.Equal(t, false, mod_semver.IsValid("1.0"))
+	assert.Equal(t, false, mod_semver.IsValid("1.0.0"))
+	assert.Equal(t, false, mod_semver.IsValid("1.0.0-rc.0"))
+	assert.Equal(t, true, mod_semver.IsValid("v1"))
+	assert.Equal(t, true, mod_semver.IsValid("v1.0"))
+	assert.Equal(t, true, mod_semver.IsValid("v1.0.0"))
+	assert.Equal(t, false, mod_semver.IsValid("v1a.0"))
+
+	// "github.com/Masterminds/semver" 에서는 v 로 시작하지 않아도 된다.
+	_, err := semver.NewVersion("1.2.3-beta.1+build345")
+	assert.Equal(t, nil, err)
+	_, err = semver.NewVersion("a.b.c")
+	assert.NotEqual(t, nil, err)
+	_, err = semver.NewVersion("1")
+	assert.Equal(t, nil, err)
+	_, err = semver.NewVersion("1.0")
+	assert.Equal(t, nil, err)
+	_, err = semver.NewVersion("1.0.0")
+	assert.Equal(t, nil, err)
+	_, err = semver.NewVersion("1.0.0-rc.0")
+	assert.Equal(t, nil, err)
+	_, err = semver.NewVersion("v1")
+	assert.Equal(t, nil, err)
+	_, err = semver.NewVersion("v1.0")
+	assert.Equal(t, nil, err)
+	_, err = semver.NewVersion("v1.0.0")
+	assert.Equal(t, nil, err)
+	_, err = semver.NewVersion("v1a.0")
+	assert.NotEqual(t, nil, err)
+}
+
+func TestSemver1(t *testing.T) {
+	// "golang.org/x/mod/semver" 는 비버전인 경우 구분안된다.
+	versions := []string{"1.0", "v1", "1.2.3.4", "1.2.3-x.y.1", "a.b.c"}
+	fmt.Println("before sort versions", versions)
+	mod_semver.Sort(versions)
+	fmt.Println("after sort versions", versions)
+	assert.Equal(t, "v1", versions[len(versions)-1])
+
+	// "github.com/Masterminds/semver" NewVersion semantic version 여부를 구분할 수 있다.
+	raw := []string{"1.2.3", "1.0", "1.3", "2", "0.4.2", "a.b.c"}
+	fmt.Println("before sort versions", raw)
+	vs := []*semver.Version{}
+	for _, r := range raw {
+		v, err := semver.NewVersion(r)
+		if err != nil {
+			// t.Errorf("Error parsing version: %s", err)
+			continue
+		}
+		vs = append(vs, v)
+	}
+	fmt.Println("after sort versions", vs)
+	sort.Sort(semver.Collection(vs))
+}
