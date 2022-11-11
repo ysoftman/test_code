bb390567a2a31ba8abbe9fbc133197a3e4dcb933
ysoftman <ysoftman@gmail.com>
2022-11-11 18:20:18 +0900

    update

--- sh/parameter_expansion_flags.zsh
+++ sh/parameter_expansion_flags.zsh
@@ -13,7 +13,7 @@ for i in $a; do
     echo "[cnt:$cnt] $i"
 done
 
-# bash 로 실행하면 newline, space 로 구분해 원하는 결과를 얻을 수 있다.
+# bash 로 실행하면 newline, space 로 구분해 다음과 같은 결과를 얻을 수 있다.
 # [cnt:1] a
 # [cnt:2] b
 # [cnt:3] c
