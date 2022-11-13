1434228ca09b58380731972243c75a067cfefd08
ysoftman <ysoftman@gmail.com>
2022-11-13 19:02:38 +0900

    fix error

--- python/command_test.py
+++ python/command_test.py
@@ -6,27 +6,25 @@ import os
 import subprocess
 
 # 쉘 명령 실행
-print('----- test1 -----')
+print("----- test1 -----")
 # 종료코드 리턴
 ret = os.system("ls")
-print('exitcode=' + str(ret))
+print("exitcode=" + str(ret))
 
-print('----- test2 -----')
+print("----- test2 -----")
 ret = os.system("ll")
-print('exitcode=' + str(ret))
+print("exitcode=" + str(ret))
 
-print('----- test3 -----')
+print("----- test3 -----")
 ret = os.system("echo 'ysoftman'")
-print('exitcode=' + str(ret))
+print("exitcode=" + str(ret))
 
 # 쉘 명령 출력 결과 얻기
-# popen 은 python 2.6 부터 deprecated 되었다.
-# https://docs.python.org/2/library/os.html
-print('----- test4 -----')
+print("----- test4 -----")
 output = os.popen('find . -name "*.py"').read()
 print(output)
 # 라인구분으로 리스트로 만들기
-linelist = output.split('\n')
+linelist = output.split("\n")
 for line in linelist:
     print(line + " --> ok.")
 # 라인구분으로 리스토 얻어 오기 -> 각 요소마다 엔터 포함된다.
@@ -36,13 +34,15 @@ for line in lines:
     print(line + " --> ok2.")
 
 # subprocess.Popen 으로 실행하기
-print('----- test5 -----')
-args = ['ls', '-1']
+# 참고로 os.popen 은 python 2.6 부터 deprecated 되었다.
+# https://docs.python.org/2/library/os.html
+print("----- test5 -----")
+args = ["ls", "-1"]
 output = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 stdout, stderr = output.communicate()
-print('[stdout]\n', stdout)
-print('[stderr]\n', stderr)
+print("[stdout]\n", stdout)
+print("[stderr]\n", stderr)
 # 라인 구분하여 리스트화 시키기
-lines = stdout.split('\n')
+lines = str(stdout).split("\n")
 for i in lines:
-    print('line=' + i)
+    print("line=" + i)
