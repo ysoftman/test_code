be03a4a939bffaa2ed4858071c6a81accdbf9aa3
ysoftman <ysoftman@gmail.com>
2022-12-24 02:27:04 +0900

    tput test

---
+++ sh/tput_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# 터미널 창 크기 변경에 따른 colums, lines 크기 파악하기
+
+function output {
+    clear
+    echo "Width = $(tput cols) Height = $(tput lines)"
+}
+
+# SIGWINCH Window size change
+# 터미널 윈도우 크기 변경시 output 함수 실행
+trap output WINCH
+
+output
+while true; do
+    sleep 0.1
+    continue
+done
