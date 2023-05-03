87768ca711f9f6727cb64b45d875932efabcb787
ysoftman <ysoftman@gmail.com>
2023-05-03 10:02:18 +0900

    arg_max test

---
+++ cpp/arg_max.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <limits.h>
+
+// https://www.in-ulm.de/~mascheck/various/argmax/
+// getconf 명령으로 확인할 수 있다.
+// getconf ARG_MAX
+int main()
+{
+    printf("argsmax: %d\n", ARG_MAX);
+    return 0;
+}
