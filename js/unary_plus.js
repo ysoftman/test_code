81c5c9ef6fc57604e431efff54298e2bb0ad1ae0
ysoftman <ysoftman@gmail.com>
2024-01-15 05:18:41 +0900

    unary plus test

---
+++ js/unary_plus.js
@@ -0,0 +1,14 @@
+let val="100"
+
+console.log(val)
+// unary plus, prefix 로 +를 사용하면 숫자로 변환한다.
+console.log(+val)
+
+val = true
+console.log(val)
+console.log(+val)
+
+val = "ysoftman"
+console.log(val)
+console.log(+val)
+
