b7286256629a68d2bc00b42900e77976ecf3ccc0
ysoftman <ysoftman@gmail.com>
2024-04-01 17:35:01 +0900

    zap log test

---
+++ golang/zap/go.mod
@@ -0,0 +1,7 @@
+module zaptest
+
+go 1.22.0
+
+require go.uber.org/zap v1.27.0
+
+require go.uber.org/multierr v1.10.0 // indirect
---
+++ golang/zap/zap.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+func main() {
+	logConfig := zap.Config{
+		//An AtomicLevel is an atomically changeable, dynamic logging level. It lets you safely change the log level of a tree of loggers (the root logger and any children created by adding context) at runtime.
+		Level:            zap.NewAtomicLevelAt(zap.DebugLevel),
+		EncoderConfig:    zap.NewProductionEncoderConfig(),
+		Encoding:         "json",
+		OutputPaths:      []string{"stdout"},
+		ErrorOutputPaths: []string{"stderr"},
+	}
+	logger, err := logConfig.Build()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logger.Sync() // flushes buffer, if any
+	sugar := logger.Sugar()
+	url := "ysoftman.tes.com"
+	sugar.Infow("failed to fetch URL",
+		// Structured context as loosely typed key-value pairs.
+		"url", url,
+		"attempt", 3,
+		"backoff", time.Second,
+	)
+	sugar.Infof("Failed to fetch URL: %s", url)
+	sugar.Debugf("Failed to fetch URL: %s", url)
+}
