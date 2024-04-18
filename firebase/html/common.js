85d94f7c7f1d2bf2a5b065d336869a86f6cd493e
ysoftman <ysoftman@gmail.com>
2024-04-18 20:35:27 +0900

    update firebase version to 10

--- firebase/html/common.js
+++ firebase/html/common.js
@@ -1,7 +1,13 @@
 // ysoftman
-
 import {webApiKey} from "./web_api_key.js"
-const config = {
+import {initializeApp} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js"
+import {getAuth} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js"
+import {getDatabase} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-database.js"
+import {getFirestore, collection, doc, getDocs, setDoc, query, orderBy, runTransaction} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js"
+import {getMessaging} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-messaging.js"
+import {getStorage, ref, getDownloadURL, listAll} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-storage.js"
+
+const firebaseConfig = {
     apiKey: webApiKey(),
     authDomain: "ysoftman-firebase.firebaseapp.com",
     databaseURL: "https://ysoftman-firebase.firebaseio.com",
@@ -9,13 +15,11 @@ const config = {
     storageBucket: "ysoftman-firebase.appspot.com",
     messagingSenderId: ""
 };
-// firebase api 사용
-firebase.initializeApp(config);
 
-// Create a reference with an initial file path and name
-const storage = firebase.storage();
-const db = firebase.firestore();
-const auth = firebase.auth();
+const firebase = initializeApp(firebaseConfig);
+const storage = getStorage();
+const db = getFirestore();
+const auth = getAuth();
 const loginBoxID = "login_google";
 const loginAnonymousBoxID = "login_anonymous";
 let userToken = "";
@@ -34,7 +38,7 @@ export const loadImages = async function(htmlId, imageNames) {
     const imgURLs = []
     for (const name of imageNames) {
         let imgURL = await imageURL(name)
-        images += `<div class="nes-container with-title"><p class="title">`+name+` (<span id=`+imgURL+`_size></span>)</p><img src=`+imgURL+`></img></div>`
+        images += `<div class="nes-container with-title"><p class="title">`+name+` (<span id=`+imgURL+`_size></span>)</p><img loading="lazy" src=`+imgURL+`></img></div>`
         imgURLs.push(imgURL)
     }
     images += `</div>`
@@ -71,33 +75,51 @@ export const getMeta = (url, cb) => {
 
 // firestorage 에 저장된 이미지 url 불러오기
 export const imageURL = async function (imageName) {
-    //let pathReference = storage.ref('xelloss.jpg');
-    let storageRef = storage.refFromURL('gs://ysoftman-firebase.appspot.com/' + imageName);
+    let storageRef = ref(storage, 'gs://ysoftman-firebase.appspot.com/' + imageName);
     // getDownloadURL() 은 비동기라 await 로 순서를 보장한다
-    const url = await storageRef.getDownloadURL();
+    const url = await getDownloadURL(storageRef);
     return url
 }
 
+// firestorage 에 저장된 이미지 list
+//export const getImageList = function (path) {
+//    let listRef = ref(storage, 'gs://ysoftman-firebase.appspot.com/' + path);
+//    const imgList = []
+//    // Find all the prefixes and items.
+//    listAll(listRef)
+//      .then((res) => {
+//        res.prefixes.forEach((folderRef) => {
+//            // All the prefixes under listRef.
+//            // You may call listAll() recursively on them.
+//            console.error("aaaaaaaaaaaa", folderRef);
+//        });
+//        res.items.forEach((itemRef) => {
+//            imgList.push(itemRef)
+//            // All the items under listRef.
+//            console.error("-----", itemRef);
+//        });
+//      }).catch((error) => {
+//        console.error("Error getting image list: ", error);
+//      });
+//}
+//
 // firestore 문서 생성
-export const setFirestoreDoc = function (coll, doc) {
-    db.collection(coll).doc(doc).set({
+export const setFirestoreDoc = async function (coll, doc) {
+    const newdocRef = doc(collection(db, coll));
+    await setDoc(newdocRef, {
         name: doc,
         visitCnt: 0
-    }).then(function () {
-        console.log("Document written!");
-    }).catch(function (error) {
-        console.error("Error adding document: ", error);
-    });
+    })
 }
 
 // firestore 테스트 방문카운트 및 조회
-export const getFirestoreVisitCnt = function (coll, doc, htmlId) {
-    let docRef = db.collection(coll).doc(doc);
+export const getFirestoreVisitCnt = function (coll, docName, htmlId) {
+    let docRef = doc(db, coll, docName);
     // likeCnt 값을 읽어 1개 증가를 트랜젹션(원자적 읽기/쓰기)으로 처리한다.
-    db.runTransaction(function (transaction) {
+    runTransaction(db, function (transaction) {
         // This code may get re-run multiple times if there are conflicts.
         return transaction.get(docRef).then(function (doc1) {
-            if (!doc1.exists) {
+            if (!doc1.exists()) {
                 throw "Document doest not exist!";
             }
             let newCnt = doc1.data().visitCnt;
@@ -115,31 +137,31 @@ export const getFirestoreVisitCnt = function (coll, doc, htmlId) {
 }
 
 // firestore 컬렉션(판교식당) 문서있으면 업데이트 없으면 생성
-export const setRestaurantDoc = function (coll, doc) {
-    let docRef = db.collection(coll).doc(doc.name);
-    db.runTransaction(function (transaction) {
+export const setRestaurantDoc = function (coll, docData) {
+    let docRef = doc(db, coll, docData.name);
+    runTransaction(db, function (transaction) {
         // This code may get re-run multiple times if there are conflicts.
         return transaction.get(docRef).then(function (doc1) {
             if (doc1.exists) {
                 transaction.update(docRef, {
-                    name: doc.name,
-                    glyphicons: doc.glyphicons,
-                    location: doc.location,
-                    menu: doc.menu,
-                    detailInfo: "https://search.daum.net/search?w=tot&DA=YZR&t__nil_searchbox=btn&q=판교+"+doc.name
+                    name: docData.name,
+                    glyphicons: docData.glyphicons,
+                    location: docData.location,
+                    menu: docData.menu,
+                    detailInfo: "https://search.daum.net/search?w=tot&DA=YZR&t__nil_searchbox=btn&q=판교+"+docData.name
                 });
                 console.log("update RestaurantDoc", doc)
                 return
             }
             transaction.set(docRef, {
-                name: doc.name,
-                glyphicons: doc.glyphicons,
-                location: doc.location,
-                menu: doc.menu,
-                detailInfo: "https://search.daum.net/search?w=tot&DA=YZR&t__nil_searchbox=btn&q=판교+"+doc.name,
-                likeCntUsers: doc.likeCntUsers,
-                dislikeCntUsers: doc.dislikeCntUsers,
-                likeCnt: doc.likeCnt,
+                name: docData.name,
+                glyphicons: docData.glyphicons,
+                location: docData.location,
+                menu: docData.menu,
+                detailInfo: "https://search.daum.net/search?w=tot&DA=YZR&t__nil_searchbox=btn&q=판교+"+docData.name,
+                likeCntUsers: docData.likeCntUsers,
+                dislikeCntUsers: docData.dislikeCntUsers,
+                likeCnt: docData.likeCnt,
                 dislikeCnt: doc.dislikeCnt
             });
             console.log("set RestaurantDoc", doc)
@@ -151,79 +173,67 @@ export const setRestaurantDoc = function (coll, doc) {
     });
 }
 
-export const onLikeClick = function (doc, htmlId) {
-    incRestaurantCnt(coll, doc, 'likeCnt', htmlId);
+export const onLikeClick = function (docName, htmlId) {
+    incRestaurantCnt(coll, docName, 'likeCnt', htmlId);
 }
 
-export const onDisLikeClick = function (doc, htmlId) {
-    incRestaurantCnt(coll, doc, 'dislikeCnt', htmlId);
+export const onDisLikeClick = function (docName, htmlId) {
+    incRestaurantCnt(coll, docName, 'dislikeCnt', htmlId);
 }
 
-export const readRestaurantAll = function (coll) {
+export const readRestaurantAll = async function (coll) {
     // collection 전체 문서 가져오기
     // likeCnt 많은 순으로
-    db.collection(coll).orderBy("likeCnt", "desc").get().then((querySnapshot) => {
-        let docNames = [];
-        let html = `<div class="card-columns">`;
-        querySnapshot.forEach((doc) => {
-            docNames.push(`${doc.data().name}`);
-            html += `
+    const q = query(collection(db, coll), orderBy("likeCnt", "desc"))
+    const querySnapshot = await getDocs(q)
+    
+    let docNames = [];
+    let html = `<div class="card-columns">`;
+   
+    querySnapshot.forEach((doc) => {
+        docNames.push(`${doc.data().name}`);
+        html += `
 <div class="card">
-    <div class="card-body">
-        <h4 class="card-title">
-            ${doc.data().name}
-            <img src="glyphicons_free/glyphicons/png/${doc.data().glyphicons}">
-        </h4>
-        <p class="card-text">${doc.data().location}</p>
-        <p class="card-text">${doc.data().menu}</p>
-    </div>
-    <p class="text-center">
-        <a href="${doc.data().detailInfo}" target="_blank" class="btn btn-primary">상세정보</a>
-        <button type="button" id='${doc.data().name}_like' class="btn btn-success"><div id='${doc.data().name}_좋아요'>좋아요 ${doc.data().likeCnt}</div></button>
-        <button type="button" id='${doc.data().name}_dislike' class="btn btn-danger"><div id='${doc.data().name}_싫어요'>싫어요 ${doc.data().dislikeCnt}</div></button>
-    </p>
+<div class="card-body">
+    <h4 class="card-title">
+        ${doc.data().name}
+        <img src="glyphicons_free/glyphicons/png/${doc.data().glyphicons}">
+    </h4>
+    <p class="card-text">${doc.data().location}</p>
+    <p class="card-text">${doc.data().menu}</p>
+</div>
+<p class="text-center">
+    <a href="${doc.data().detailInfo}" target="_blank" class="btn btn-primary">상세정보</a>
+    <button type="button" id='${doc.data().name}_like' class="btn btn-success"><div id='${doc.data().name}_좋아요'>좋아요 ${doc.data().likeCnt}</div></button>
+    <button type="button" id='${doc.data().name}_dislike' class="btn btn-danger"><div id='${doc.data().name}_싫어요'>싫어요 ${doc.data().dislikeCnt}</div></button>
+</p>
 </div>
 `
-        });
-        html += `</div>`
-        document.getElementById('html_out').innerHTML = html;
-        for (const name of docNames) {
-            //console.log("name:", name)
-            //<button onClick="onLikeClick"  으로 하면 onLikeClick 함수를 찾지 못하는 에러가 발생한다.
-            //따라서 다음과 같이 이벤트 리스터를 추가한다.
-            document.getElementById(name+"_like").addEventListener("click", ()=>{onLikeClick(name, name+"_좋아요")});
-            document.getElementById(name+"_dislike").addEventListener("click",()=>{onDisLikeClick(name, name+"_싫어요")});
-        }
-        document.getElementById("restaurant_cnt").innerHTML = " ("+docNames.length+"개)";
-        //console.log(html);
     });
-}
 
-// firestore 컬렉션(판교식당) 문서이름으로 읽기
-export const readRestaurantCnt = function (coll, doc, cntType, htmlId) {
-    // collection->doc1 하나만 가져오기
-    // onSnapshot(콜백함수) 로 수신대기하면서 현재 내용(변수등)값들을 스냅샷(문서)으로 저장 한다.
-    // 그리고 내용이 변경될때마다 콜백함수(문서가져오기)가 실행되어 스냅샷을 업데이트한다.
-    // (주의) likeCnt++ 등변수값을 변경하게 되면 매번 내용이 변경되서 문서가져오기가 무한으로 수행돼 과금될 수 있다.
-    db.collection(coll).doc(doc).onSnapshot(function (doc1) {
-        console.log("Current data: ", doc1.data(), "->", htmlId);
-        if (cntType == 'likeCnt') {
-            document.getElementById(htmlId).innerHTML = `${doc1.data().likeCnt}`;
-        } else if (cntType == 'dislikeCnt') {
-            document.getElementById(htmlId).innerHTML = `${doc1.data().dislikeCnt}`;
-        }
-    });
+    html += `</div>`
+
+    document.getElementById('html_out').innerHTML = html;
+    for (const name of docNames) {
+        //console.log("name:", name)
+        //<button onClick="onLikeClick"  으로 하면 onLikeClick 함수를 찾지 못하는 에러가 발생한다.
+        //따라서 다음과 같이 이벤트 리스터를 추가한다.
+        document.getElementById(name+"_like").addEventListener("click", ()=>{onLikeClick(name, name+"_좋아요")});
+        document.getElementById(name+"_dislike").addEventListener("click",()=>{onDisLikeClick(name, name+"_싫어요")});
+    }
+    document.getElementById("restaurant_cnt").innerHTML = " ("+docNames.length+"개)";
+    //console.log(html);
 }
 
 // firestore 컬렉션(판교식당) 해당하는 문서 카운트 증가시키기
-export const incRestaurantCnt = function (coll, doc, cntType, htmlId) {
+export const incRestaurantCnt = function (coll, docName, cntType, htmlId) {
     if (!checkLogin()) {
         alert("로그인이 필요합니다.");
         return
     }
-    let docRef = db.collection(coll).doc(doc);
+    let docRef = doc(collection(db, coll), docName);
     // likeCnt 값을 읽어 1개 증가를 트랜젹션(원자적 읽기/쓰기)으로 처리한다.
-    db.runTransaction(function (transaction) {
+    runTransaction(db, function (transaction) {
         // This code may get re-run multiple times if there are conflicts.
         return transaction.get(docRef).then(function (doc1) {
             if (!doc1.exists) {
