1a5892b871b2bb3cbb3cd622f3d1a9798f63033a
ysoftman <ysoftman@gmail.com>
2024-05-10 01:55:20 +0900

    update test

--- golang/sql_db/go.mod
+++ golang/sql_db/go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/DATA-DOG/go-sqlmock v1.5.2
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/mitchellh/mapstructure v1.5.0
+	github.com/stretchr/testify v1.9.0
 	gopkg.in/square/go-jose.v2 v2.6.0
 	gorm.io/driver/mysql v1.0.2
 	gorm.io/driver/sqlite v1.5.5
@@ -16,7 +17,10 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- golang/sql_db/sql_mock_test.go
+++ golang/sql_db/sql_mock_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -33,42 +34,72 @@ func NewMockMysql() (*gorm.DB, sqlmock.Sqlmock) {
 	return gormDB, mock
 }
 
-// https://www.codingexplorations.com/blog/testing-gorm-with-sqlmock
-func TestGorm(t *testing.T) {
+func TestGormCreate(t *testing.T) {
 	db, mock := NewMockMysql()
 
+	// gorm Create 가 Transaction  으로 처리되서 expectBegin , expectcommit 처리
 	mock.ExpectBegin()
 	mock.ExpectExec("^INSERT INTO `users` (.+)$").WillReturnResult(sqlmock.NewResult(1, 1))
 	mock.ExpectCommit()
+
 	user := User{Name: "Jane Doe"}
-	if err := db.Create(&user).Error; err != nil {
-		t.Fatalf("Failed to insert user: %v", err)
-	}
+	err := db.Create(&user).Error
+	assert.Nil(t, err)
+	assert.Equal(t, "Jane Doe", user.Name)
+}
+
+func TestGormFind(t *testing.T) {
+	db, mock := NewMockMysql()
 
 	rows := sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "John Doe")
 	mock.ExpectQuery("^SELECT \\* FROM `users`$").WillReturnRows(rows)
+
 	var users []User
-	if err := db.Find(&users).Error; err != nil {
-		t.Fatalf("Error in finding users: %v", err)
-	}
+	err := db.Find(&users).Error
 	fmt.Println("-------", users)
-	if len(users) != 1 || users[0].Name != "John Doe" {
-		t.Fatalf("Unexpected user data retrieved: %v", users)
-	}
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(users))
+	assert.Equal(t, "John Doe", users[0].Name)
+}
+
+func TestGormSave(t *testing.T) {
+	db, mock := NewMockMysql()
 
 	mock.ExpectBegin()
 	mock.ExpectExec("^UPDATE `users` SET `name`=(.+) WHERE (.+)$").WillReturnResult(sqlmock.NewResult(1, 1))
 	mock.ExpectCommit()
-	user = User{ID: 1, Name: "Jane Smith"}
-	if err := db.Save(&user).Error; err != nil {
-		t.Fatalf("Failed to update user: %v", err)
-	}
+
+	user := User{ID: 1, Name: "Jane Smith"}
+	err := db.Save(&user).Error
+	assert.Nil(t, err)
+	assert.Equal(t, 1, int(user.ID))
+	assert.Equal(t, "Jane Smith", user.Name)
+}
+
+func TestGormDelete(t *testing.T) {
+	db, mock := NewMockMysql()
 
 	mock.ExpectBegin()
 	mock.ExpectExec("^DELETE FROM `users` WHERE (.+)$").WillReturnResult(sqlmock.NewResult(1, 1))
 	mock.ExpectCommit()
-	user = User{ID: 1}
-	if err := db.Delete(&user).Error; err != nil {
-		t.Fatalf("Failed to delete user: %v", err)
-	}
+	user := User{ID: 1}
+	err := db.Delete(&user).Error
+	assert.Nil(t, err)
+}
+
+func TestGormFirstOrCreate(t *testing.T) {
+	db, mock := NewMockMysql()
+
+	// FirstOrCreate 조회 결과 없으면 생성
+	mock.ExpectQuery("^SELECT \\* FROM `users` WHERE `name`=\\? ORDER BY `users`.`id` LIMIT \\?$").
+		WithArgs("bill", 1).
+		WillReturnRows(sqlmock.NewRows([]string{})) // 조회 결과가 없는 경우
+	mock.ExpectBegin()
+	mock.ExpectExec("^INSERT INTO `users` (.+)$").WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+
+	user := User{Name: "bill"}
+	result := db.Where("`name`=?", "bill").FirstOrCreate(&user)
+	assert.Nil(t, result.Error)
+	assert.Equal(t, int64(1), result.RowsAffected)
 }
