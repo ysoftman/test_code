f53da08fd483a3f5ddc5d0f11604aac62fa7826a
ysoftman <ysoftman@gmail.com>
2024-07-04 00:27:34 +0900

    update

--- sh/echo.sh
+++ sh/echo.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 set -x
-echo "apple\nlemon"
+
+# bash 에서는 -E 가 디폴트
+# zsh 에셔는 -e 가 디폴트
+echo "\"apple\nlemon\""
 
 # disable interpretation escape
-echo -E "apple\nlemon"
+echo -E "\"apple\nlemon\""
 
 # interpretation escape
 # https://www.gnu.org/software/coreutils/manual/html_node/echo-invocation.html
@@ -18,8 +21,8 @@ echo -E "apple\nlemon"
 # \t horizontal tab
 # \v vertical tab
 # \\ backslash
-echo -e "apple\nlemon"
+echo -e "\"apple\nlemon\""
 
 # without trailing newline
-echo -n "apple\nlemon"
+echo -n "\"apple\nlemon\""
 
