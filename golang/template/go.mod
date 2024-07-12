b9cb0c011b93d34e4f82a12dffd9fe6aa8b8ab0b
ysoftman <ysoftman@gmail.com>
2024-07-12 10:09:43 +0900

    add sprig template test

---
+++ golang/template/go.mod
@@ -0,0 +1,15 @@
+module template
+
+go 1.22.4
+
+require github.com/go-task/slim-sprig v2.20.0+incompatible
+
+require (
+	github.com/Masterminds/goutils v1.1.1 // indirect
+	github.com/Masterminds/semver v1.5.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/huandu/xstrings v1.5.0 // indirect
+	github.com/imdario/mergo v0.3.16 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	golang.org/x/crypto v0.25.0 // indirect
+)
--- golang/template/template.go
+++ golang/template/template.go
@@ -1,17 +1,18 @@
 // author : ysoftman
 // encoding : utf-8
-// title : html template 사용 테스트
-// html 영역에 값 전달시 template.HTML
-// script 영역에 값 전달시 template.JS 데이터 타입을 사용하자
+// title : template 사용 테스트
+
 package main
 
 import (
 	"fmt"
 	"html/template"
 	"os"
+
+	sprig "github.com/go-task/slim-sprig"
 )
 
-const htmlString = `
+const htmlTemplate = `
 <html>
 <body>
 html template 테스트입니다.
@@ -22,6 +23,10 @@ html template 테스트입니다.
 {{-공백 은 이전까지 공백(탭,뉴라인) trim
 공백-}}  은 이후의 공백(탭,뉴라인) trim
 */ -}}
+
+{{if ne .Message ""}}
+	{{ .Message }}
+{{end}}
 
 {{if .HasApple}}
 	apple
@@ -50,6 +55,16 @@ val3 = {{.Value3}}
 </html>
 `
 
+const sprigTemplate = `
+--- use spring tmeplate ---
+--- https://github.com/Masterminds/sprig ---
+{{/* sprig 패키지를 사용하면 다음과 같은 pipeline function 을 사용할 수 있다. */}}
+{{- .Message}}
+blabla
+{{.Message  | trim | upper | repeat 5}}
+{{.Message  | trim | indent 4}}
+`
+
 type UserInfo struct {
 	Value1 int
 
@@ -58,17 +73,21 @@ type UserInfo struct {
 	// html 영역에서 html 태그가 제대로 표시 되지 않는다.
 	//	value2 string
 	//	value3 string
+
+	// html 영역에 값 전달시 template.HTML
+	// script 영역에 값 전달시 template.JS 데이터 타입을 사용하자
 	Value2 template.JS
 	Value3 template.HTML
 
 	HasApple bool
+
+	Message string
 }
 
 var userInfo UserInfo
 
 func main() {
-
-	tpl, err := template.New("test_template").Parse(htmlString)
+	tpl, err := template.New("test_template").Parse(htmlTemplate)
 	if err != nil {
 		fmt.Println("parse template error ... " + err.Error())
 		return
@@ -84,11 +103,24 @@ func main() {
 	have a nice day4<br>
 	`
 	userInfo.HasApple = false
+	userInfo.Message = "     ysoftman"
 
 	//html 문서 처리
 	err = tpl.Execute(os.Stdout, userInfo)
 	if err != nil {
 		fmt.Println("execute template error ... " + err.Error())
+		return
+	}
+
+	//sprig template
+	sprigTpl, err := template.New("sprig_test_template").Funcs(sprig.FuncMap()).Parse(sprigTemplate)
+	if err != nil {
+		fmt.Println("parse sprig template error ... " + err.Error())
+		return
+	}
+	err = sprigTpl.Execute(os.Stdout, userInfo)
+	if err != nil {
+		fmt.Println("execute sprig template error ... " + err.Error())
 		return
 	}
 }
