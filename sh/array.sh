ffb7b1fc20a4b35b2defee8797a1e0afbed5bb28
ysoftman <ysoftman@gmail.com>
2024-10-18 09:53:33 +0900

    fix shellcheck lint warning

--- sh/array.sh
+++ sh/array.sh
@@ -1,45 +1,45 @@
-#/bin/bash
+#!/bin/bash
 # () 로 배열 만들기
 
 # 이건 배열 원소 1개가 된다.
 # arr=(1,23,4,"a")
-# echo $arr 
+# echo $arr
 
 # 원소 구분은 공백으로 한다.
 arr=(1 23 4 "a")
-echo '${arr}' ${arr}  # arr 로 접근하면 첫번째 원소값만 보인다.
+echo "${arr}" "${arr}"  # arr 로 접근하면 첫번째 원소값만 보인다.
 # bash 는 0, zsh 는 1이 시작 인덱스다.
-echo '${arr[0]}' ${arr[0]}
-echo '${arr[1]}' ${arr[1]}
-echo '${arr[2]}' ${arr[2]}
-echo '${arr[3]}' ${arr[3]}
-echo '${arr[4]}' ${arr[4]}
+echo "${arr[0]}" ${arr[0]}
+echo "${arr[1]}" ${arr[1]}
+echo "${arr[2]}" ${arr[2]}
+echo "${arr[3]}" ${arr[3]}
+echo "${arr[4]}" ${arr[4]}
 # 모든 원소 값 조회
-echo '${arr[@]}' ${arr[@]}
+echo "${arr[@]}" "${arr[@]}"
 # 모든 원소 index 조회
-echo '${!arr[@]}' ${!arr[@]}
+echo "${!arr[@]}" "${!arr[@]}"
 # 원소 개수
-echo '${#arr[@]}' ${#arr[@]}
+echo "${#arr[@]}" "${#arr[@]}"
 # 첫번째 원소의 크기
-echo '${#arr[1]}' ${#arr[1]}
+echo "${#arr[1]}" "${#arr[1]}"
 
 # 원소 값 변경
 arr[2]="ysoftman"
-echo '${arr[@]}' ${arr[@]}
+echo "${arr[@]}" "${arr[@]}"
 # 원소 추가
 arr+=("lemon")
 arr+=(777)
-echo '${arr[@]}' ${arr[@]}
+echo "${arr[@]}" "${arr[@]}"
 
 # 2번 인덱스부터 시작해서 3개원소 조회
-echo '${arr[@]:2:3}' ${arr[@]:2:3}
+echo "${arr[@]:2:3}" "${arr[@]:2:3}"
 # 0번 인덱스부터 시작해서 3개원소 조회
-echo '${arr[@]::3}' ${arr[@]::3}
+echo "${arr[@]::3}" "${arr[@]::3}"
 # 0번 인덱스부터 시작해서 999개원소 조회(원소가 있는것까지만 출력된다.)
-echo '${arr[@]::9999}' ${arr[@]::9999}
+echo "${arr[@]::9999}" "${arr[@]::9999}"
 
 echo "loop array test1"
-for i in ${arr[@]}
+for i in "${arr[@]}"
 do
     echo $i;
 done
@@ -53,7 +53,7 @@ done
 set -x
 echo "-----"
 a="lemon apple a 123"
-arr=($a)
+arr=("$a")
 echo ${arr[0]}
 echo ${arr[1]}
 echo ${arr[2]}
