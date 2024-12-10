8005be5b775fc9ed90aff06e0feee18beda4049e
ysoftman <ysoftman@gmail.com>
2024-12-10 09:21:57 +0900

    update testcase

--- sh/variable_arg.sh
+++ sh/variable_arg.sh
@@ -21,12 +21,20 @@ echo '$#:'$#
 # "arg1 arg2" arg3 처럼 "" 로 된 arg 를 하나의 arg 구분하지 못한다.
 echo '$*:'$*
 for a in $*; do echo "-$a"; done;
-for a in "$*"; do echo "double-quotes-$a"; done;
-
-# args 모두(n 개의 argument 가 유지되고, 각 argument는 문자로 취급)
-echo '$@:'$@
+for a in "$*"; do echo "error, double-quotes-$a"; done;
+# $@ 는"로 없어도 splie  되지만 ""  없으면 re-splitting 될 수 있다
 for a in $@; do echo "-$a"; done;
 for a in "$@"; do echo "double-quotes-$a"; done;
+
+# args 모두(n 개의 argument 가 유지되고, 각 argument는 문자로 취급)
+echo '$@:' "$@"
+# substring
+# 1번째 뒤로 모두
+echo "${@:1}"
+# 2번째 뒤로 모두
+echo "${@:2}"
+# 2번째 부터 2개까지
+echo "${@:2:1}"
 
 
 # 0번째 인자
