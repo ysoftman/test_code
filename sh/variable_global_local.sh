951c1e0e063edbdffdb4202456b55dfd6b3839b1
ysoftman <ysoftman@gmail.com>
2024-12-19 14:43:18 +0900

    add local test

--- sh/variable_global_local.sh
+++ sh/variable_global_local.sh
@@ -13,3 +13,24 @@ aaa() {
 aaa
 echo ${global_val}
 echo ${local_val} # 지역 변수라 출력되지 않음
+
+readonly_var() {
+    # -r : readonly 로컬 변수
+    local -r v=$1
+    # var="aaa"
+    echo $v
+}
+
+modify_var() {
+    # -n nameref (reference) 참조 변수로 값을 수정하면 원래 변수도 값이 수정된다.
+    local -n ref=$1
+    ref="modified"
+    echo "$ret"
+}
+
+original="original"
+readonly_var $original
+echo $original
+
+modify_var $original
+echo $original
