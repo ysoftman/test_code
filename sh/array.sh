ae7be60c4a16d4196dada2fe1b8afc051d6a3c2f
ysoftman <ysoftman@gmail.com>
2024-12-27 18:30:00 +0900

    update array test

--- sh/array.sh
+++ sh/array.sh
@@ -7,55 +7,45 @@
 
 # 원소 구분은 공백으로 한다.
 arr=(1 23 4 "a")
-echo "${arr}" "${arr}"  # arr 로 접근하면 첫번째 원소값만 보인다.
-# bash 는 0, zsh 는 1이 시작 인덱스다.
-echo "${arr[0]}" ${arr[0]}
-echo "${arr[1]}" ${arr[1]}
-echo "${arr[2]}" ${arr[2]}
-echo "${arr[3]}" ${arr[3]}
-echo "${arr[4]}" ${arr[4]}
-# 모든 원소 값 조회
-echo "${arr[@]}" "${arr[@]}"
-# 모든 원소 index 조회
-echo "${!arr[@]}" "${!arr[@]}"
-# 원소 개수
-echo "${#arr[@]}" "${#arr[@]}"
-# 첫번째 원소의 크기
-echo "${#arr[1]}" "${#arr[1]}"
-
-# 원소 값 변경
-arr[2]="ysoftman"
-echo "${arr[@]}" "${arr[@]}"
-# 원소 추가
-arr+=("lemon")
-arr+=(777)
-echo "${arr[@]}" "${arr[@]}"
-
-# 2번 인덱스부터 시작해서 3개원소 조회
-echo "${arr[@]:2:3}" "${arr[@]:2:3}"
-# 0번 인덱스부터 시작해서 3개원소 조회
-echo "${arr[@]::3}" "${arr[@]::3}"
-# 0번 인덱스부터 시작해서 999개원소 조회(원소가 있는것까지만 출력된다.)
-echo "${arr[@]::9999}" "${arr[@]::9999}"
-
-echo "loop array test1"
-for i in "${arr[@]}"
-do
-    echo $i;
-done
-
-echo "loop array test2"
-for ((i=0;i<${#arr[@]};i++))
-do
-    echo "[${i}] ${arr[$i]}";
-done
-
-set -x
-echo "-----"
-a="lemon apple a 123"
-arr=("$a")
+echo "${arr}" # arr 로 접근하면 첫번째 원소값만 보인다.
+echo "----- arr[0] ~ arr[4]"
 echo ${arr[0]}
 echo ${arr[1]}
 echo ${arr[2]}
 echo ${arr[3]}
 echo ${arr[4]}
+echo "----- all elements in array"
+echo "${arr[@]}"
+echo "----- all elements in array with newline"
+printf "%s\n" "${arr[@]}"
+echo "----- all elements in array index"
+echo "${!arr[@]}"
+echo "----- num of elements in array"
+echo "${#arr[@]}"
+echo "----- first element size(length)"
+echo "${#arr[1]}"
+
+echo "----- update array[2] to ysoftman"
+arr[2]="ysoftman"
+echo "${arr[@]}"
+echo "----- add element to array"
+arr+=("lemon and apple")
+arr+=(777)
+echo "${arr[@]}"
+
+echo "----- 2번 인덱스부터 시작해서 3개원소 조회"
+echo "${arr[@]:2:3}"
+echo "----- 0번 인덱스부터 시작해서 3개원소 조회"
+echo "${arr[@]::3}"
+echo "----- 0번 인덱스부터 시작해서 999개원소 조회(원소가 있는것까지만 출력된다.)"
+echo "${arr[@]::9999}"
+
+echo "----- loop array test1"
+for i in "${arr[@]}"; do
+    echo $i
+done
+
+echo "----- loop array test2"
+for ((i = 0; i < ${#arr[@]}; i++)); do
+    echo "[${i}] ${arr[$i]}"
+done
