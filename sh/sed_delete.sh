fa659cc589b94b80fa8f50afcc1e0eb3db917554
ysoftman <ysoftman@gmail.com>
2025-05-12 11:32:20 +0900

    update comments

--- sh/sed_delete.sh
+++ sh/sed_delete.sh
@@ -18,15 +18,16 @@ echo -e "$temp"
 # d 삭제
 # 라인2만 삭제하고 출력
 echo 'delete line 2 and print'
-echo -e "$temp" | sed  "2d;"
+echo -e "$temp" | sed "2d;"
 
-# ccc ~ ggg 패턴 사이의 내용만 삭제하고 출력
+# ccc ~ ggg 패턴 사이의 내용(라인들)만 삭제하고 출력
 echo 'delete pattern ccc ~ ggg and print'
 echo -e "$temp" | sed "/^ccc/,/^ggg/d"
 
 echo 'find ysoftman and delete'
 echo -e "$temp" | sed "s/ysoftman//g"
 
+# d 는 라인 단위로만 삭제 가능하다.
 echo 'find ysoftman and delete line'
 echo -e "$temp" | sed "/ysoftman/d"
 
