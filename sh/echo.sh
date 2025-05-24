a5199de51e6a92d3d6f728736f3db954184eeb1d
ysoftman <ysoftman@gmail.com>
2025-05-25 00:46:11 +0900

    echo and printf stderr test

--- sh/echo.sh
+++ sh/echo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x
+# set -x
 
 # bash 에서는 -E 가 디폴트
 # zsh 에셔는 -e 가 디폴트
@@ -9,7 +9,9 @@ echo "\"apple\n   lemon\""
 # bash 에서는 declare -x z="\"apple\\n   lemon\""
 # zsh 에서는 z='"apple\n   lemon"'
 # bash 에서 연속적인 공백문자를 출력하려면 변수를 "" 로 감싸야 한다
-export z="\"apple\n   lemon\"";  export | grep z=; echo -e "$z"
+export z="\"apple\n   lemon\""
+export | grep z=
+echo -e "$z"
 
 # disable interpretation escape
 echo -E "\"apple\n   lemon\""
@@ -30,4 +32,13 @@ echo -e "\"apple\n   lemon\""
 
 # without trailing newline
 echo -n "\"apple\n   lemon\""
+echo "banana"
 
+# stdout 확인
+# bash ./echo.sh 2> /dev/null
+echo "stout message"
+# stderr 확인
+# bash ./echo.sh > /dev/null
+# >&2: 출력 방향을 2번(=stderr)로 변경
+echo >&2 "stderr message1"
+echo "stderr message2" >&2
--- sh/printf.sh
+++ sh/printf.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 printf "%s\n" "~~ysoftman~~"
-# -문자 로 시작시 -옵션일 수 있어 -- 로 옵션이 끝났을을 설정해야 한다
+# -문자 로 시작시 -옵션일 수 있어 -- 로 옵션이 끝났음을 설정해야 한다
 # -% invalid option
 #printf "-%s\n" "~~ysoftman~~"
 printf -- "-%s\n" "~~ysoftman~~"
 function aaa() {
     # "$*" 로 ""로 감싸면 arguments 모두를 문자(word) 하나로 취급해 출력시 차이가 없다.
-    echo "$*"
+    echo '$*:' "$*"
     printf "$*\n"
 
-    echo "echo-arguments: $@"
+    # lint error) argument mixes string and arrayu
+    # echo "echo-arguments: $@"
+    echo "echo-arguments: $*"
     # printf 로 arguments 배열를 출력하면 각 원소 마다 printf format 으로 출력 된다.
     printf "printf-argument: %s\n" "$@"
 }
 
 aaa ysoftman lemon apple 1 2 3
+
+# stdout 확인
+# bash ./printf.sh 2> /dev/null
+printf "stdout message\n"
+# stderr 확인
+# bash ./printf.sh > /dev/null
+printf "stderr message1\n" >&2
+printf >&2 "stderr message2\n"
