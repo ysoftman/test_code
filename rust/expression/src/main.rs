2daeb92dca1f2398f5baf40cb0d79d56d5f4339e
ysoftman <ysoftman@gmail.com>
2025-06-16 20:04:18 +0900

    expression test

---
+++ rust/expression/src/main.rs
@@ -0,0 +1,14 @@
+fn func1(a: i32) -> i32 {
+    // 표현식(expression)은 리턴을 한다. ; 으로 끝나지 않는다.
+    a + 1
+}
+fn main() {
+    // 구문(statment)는 리턴이 없다. ;  으로 끝난다
+    // 다음과 같은 구문(리턴이 없으니) 사용할 수 없다.
+    // expected expression, found `let` statement
+    // let b = let a = 1;
+    // println!("b = {b}");
+    let x = 1;
+    println!("x = {x}");
+    println!("func1 = {}", func1(1));
+}
