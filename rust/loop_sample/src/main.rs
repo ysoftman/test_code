ef357f38c46c7949c2421f70fd1463a69431faae
ysoftman <ysoftman@gmail.com>
2025-06-17 20:11:33 +0900

    add test

--- rust/loop_sample/src/main.rs
+++ rust/loop_sample/src/main.rs
@@ -13,6 +13,18 @@ fn main() {
         i += 1;
     }
     i = 0;
+    let l = loop {
+        if i >= 7 {
+            break "브레이크로 루프를 빠져 나가고 이 문장을 리턴";
+        }
+        println!("--> arr[{}] : {}", i, arr[i]);
+        // i++는 사용 못함
+        // i++;
+        i += 1;
+    };
+    println!("break return: {}", l);
+
+    i = 0;
     while i < 7 {
         println!("arr[{}] : {}", i, arr[i]);
         i += 1;
