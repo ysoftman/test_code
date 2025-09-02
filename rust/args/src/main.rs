8b62df9930ecba99b939465cafd25225fa37372b
ysoftman <ysoftman@gmail.com>
2025-09-02 13:04:07 +0900

    update structopt(and atty) to new version of clap

--- rust/args/src/main.rs
+++ rust/args/src/main.rs
@@ -5,9 +5,12 @@
 // cargo build && ./target/debug/args -o foo.txt -dvvvs 1337 -l alice -l bob --nb-cars 4 bar.txt baz.txt
 
 use std::path::PathBuf;
-use structopt::StructOpt;
-
+// structopt 는 한때 표준처럼 쓰였지만, 현재는 유지보수 중단 상태입니다.
+// clap 3.x 버전에 structopt 기능이 통합되었기 때문에 더 이상 쓸 필요가 없습니다.
+// use structopt::StructOpt;
+use clap::{ArgAction, Parser};
 use std::env;
+
 //  사용하지 않는 코드 warning 발생하지 않게 하는 attribute
 #[allow(dead_code)]
 fn main() {
@@ -20,35 +23,81 @@ fn main() {
     println!("args[1..]: {:?}", &args[1..]);
     println!("--------------------------");
 
-    // strutopt crate 사용
-    #[derive(StructOpt, Debug)]
-    #[structopt(name = "basic")]
+    // structopt crate 사용(보안 취약점 있는 atty 사용하고, 유지보수 안되고 있어 사용않는다.)
+    // #[derive(StructOpt, Debug)]
+    // #[structopt(name = "basic")]
     // 여기에 cli 옵션(flag)으로 사용할 argument 를 명세한다.
+    // struct Opt {
+    //     #[structopt(short, long)]
+    //     debug: bool,
+    //
+    //     // The number of occurrences of the `v/verbose` flag
+    //     /// Verbose mode (-v, -vv, -vvv, etc.)
+    //     #[structopt(short, long, parse(from_occurrences))]
+    //     verbose: u8,
+    //
+    //     /// Set speed
+    //     #[structopt(short, long, default_value = "42")]
+    //     speed: f64,
+    //
+    //     /// Output file
+    //     #[structopt(short, long, parse(from_os_str))]
+    //     output: PathBuf,
+    //
+    //     // the long option will be translated by default to kebab case,
+    //     // i.e. `--nb-cars`.
+    //     /// Number of cars
+    //     #[structopt(short = "c", long)]
+    //     nb_cars: Option<i32>,
+    //
+    //     /// admin_level to consider
+    //     #[structopt(short, long)]
+    //     level: Vec<String>,
+    //     // Files to process
+    //     // - 가 없는 인자
+    //     // #[structopt(name = "FILE", parse(from_os_str))]
+    //     // files: Vec<PathBuf>,
+    //
+    //     // - 가 없는 인자
+    //     #[structopt(default_value = "lemon")]
+    //     name1: String,
+    //     // - 가 없는 인자
+    //     #[structopt(default_value = "orange")]
+    //     name2: String,
+    // }
+    // clap crate 사용
+    #[derive(Parser, Debug)]
+    #[command(name = "basic")]
     struct Opt {
-        #[structopt(short, long)]
+        // short 인자 사용 -d
+        // long : kebab-case 스타일의 long 인자 사용 --debug
+        #[arg(short, long)]
         debug: bool,
 
         // The number of occurrences of the `v/verbose` flag
-        /// Verbose mode (-v, -vv, -vvv, etc.)
-        #[structopt(short, long, parse(from_occurrences))]
+        // Verbose mode (-v, -vv, -vvv, etc.)
+        // v의 사용회에 따라 레벨 차이날 수 있도록
+        #[arg(short, long, action = ArgAction::Count)]
         verbose: u8,
 
-        /// Set speed
-        #[structopt(short, long, default_value = "42")]
+        // Set speed
+        #[arg(short, long, default_value = "42")]
         speed: f64,
 
-        /// Output file
-        #[structopt(short, long, parse(from_os_str))]
+        // Output file
+        // PathBuf, OsString 같은 타입들은 clap 이 기본적으로 지원하는 파서가 있어서
+        // 그냥value_parser 만 적어주면 된다.
+        #[arg(short, long, value_parser)]
         output: PathBuf,
 
         // the long option will be translated by default to kebab case,
-        // i.e. `--nb-cars`.
-        /// Number of cars
-        #[structopt(short = "c", long)]
+        // short = 'c'  -c 10
+        // long 자동 생성 --nb-cars 10
+        #[arg(short = 'c', long)]
         nb_cars: Option<i32>,
 
         /// admin_level to consider
-        #[structopt(short, long)]
+        #[arg(short, long)]
         level: Vec<String>,
         // Files to process
         // - 가 없는 인자
@@ -56,13 +105,15 @@ fn main() {
         // files: Vec<PathBuf>,
 
         // - 가 없는 인자
-        #[structopt(default_value = "lemon")]
+        #[arg(default_value = "lemon")]
         name1: String,
         // - 가 없는 인자
-        #[structopt(default_value = "orange")]
+        #[arg(default_value = "orange")]
         name2: String,
     }
-    let opt = Opt::from_args();
+
+    // let opt = Opt::from_args();  // structopt 사용시
+    let opt = Opt::parse();
     println!("{opt:#?}");
     // -d --debug 플래그 지정 여부
     if opt.debug {
