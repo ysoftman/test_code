6a56b87ca78183c157cf6ec804025b4bda415da1
ysoftman <ysoftman@gmail.com>
2026-02-10 13:24:14 +0900

    update

--- sh/sed_substitute.sh
+++ sh/sed_substitute.sh
@@ -9,6 +9,10 @@ echo "ysoftman @open (12345) /abc/def/ggg" | sed "s#/abc/def/ggg#zzzzz#"
 echo "ysoftman @open (12345) /abc/def/ggg" | sed "s,/abc/def/ggg,zzzzz,"
 echo "ysoftman @open (12345) /abc/def/ggg" | sed "s /abc/def/ggg zzzzz "
 echo "ysoftman @open (12345) /abc/def/ggg" | sed "s/@.*$//"
+# 여러개를 변경할 경우 ; 구분
+echo "ysoftman @open (12345) /abc/def/ggg" | sed "s/abc/aaa/;s/def/bbb/;s/ggg/zzz/"
+# 또는 -e 옵션 반복 사용
+echo "ysoftman @open (12345) /abc/def/ggg" | sed -e "s/abc/aaa/" -e "s/def/bbb/" -e "s/ggg/zzz/"
 
 # 공백@ 뒤로 삭제
 echo "ysoftman @open (12345)" | sed "s/ @.*$//"
@@ -20,4 +24,3 @@ green='\033[0;32m'
 echo -e "ysoftman ${green}color${reset_color} string " | sed "s/ color.*$//"
 # 중간에 컬러 문자를 제거해줘야 한다.
 echo -e "ysoftman ${green}color${reset_color} string " | sed "s,$(printf '\033')\\[[0-9;]*[a-zA-Z],,g" | sed "s/ color.*$//"
-
