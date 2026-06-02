4d29b3418f720d3ff5ba84cc998ddd3829bc4186
ysoftman <ysoftman@gmail.com>
2026-06-02 09:12:04 +0900

    rename ysoftman-project to ysoftman-vue-app
    - git mv directory and update package/manifest/lock names
    - vite.config: base /test_code/ysoftman-vue-app/
    - pages.yml: rename build/assemble steps and output dir
    - landing: update card href and title
    
    ########## added by prepare-commit-msg ##########
    # COMMIT_MSG_FILE: .git/COMMIT_EDITMSG
    # DATE: 2026-06-02 09:12:04
    
    ########## added by prepare-commit-msg ##########
    # COMMIT_MSG_FILE: .git/COMMIT_EDITMSG
    # DATE: 2026-06-02 09:12:34

--- test_web_page/ysoftman-vue-app/vite.config.js
+++ test_web_page/ysoftman-vue-app/vite.config.js
@@ -2,9 +2,9 @@ import vue from "@vitejs/plugin-vue";
 import path from "path";
 import { defineConfig } from "vite";
 
-// Served from GitHub Pages at /test_code/ysoftman-project/
+// Served from GitHub Pages at /test_code/ysoftman-vue-app/
 export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/test_code/ysoftman-project/" : "/",
+  base: command === "build" ? "/test_code/ysoftman-vue-app/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
