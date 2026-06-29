2dfe12d932ed0e794bada4c3c7390d0a8f3437ce
ysoftman <ysoftman@gmail.com>
2026-06-29 23:14:25 +0900

    update HelloWorld vue

--- test_web_page/vue-sample/src/components/HelloWorld.vue
+++ test_web_page/vue-sample/src/components/HelloWorld.vue
@@ -2,89 +2,14 @@
   <v-container fluid>
     <h1>{{ msg }}</h1>
 
-    <p>
-      For a guide and recipes on how to configure / customize this project,
-      <br />
-      check out the
-      <a
-        href="https://cli.vuejs.org"
-        target="_blank"
-        rel="noopener"
-      >
-        vue-cli documentation
-      </a>.
-    </p>
+    <p>hello world vue</p>
 
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-        >
-          babel
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa"
-          target="_blank"
-          rel="noopener"
-        >
-          pwa
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest"
-          target="_blank"
-          rel="noopener"
-        >
-          unit-jest
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-e2e-cypress"
-          target="_blank"
-          rel="noopener"
-        >
-          e2e-cypress
-        </a>
-      </li>
-    </ul>
-
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank">News</a></li>
-    </ul>
-
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank">vue-router</a></li>
-      <li><a href="https://pinia.vuejs.org" target="_blank">pinia</a></li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-        >
-          vue-devtools
-        </a>
-      </li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
   </v-container>
 </template>
 
 <script setup>
 defineProps({
-    msg: String,
+  msg: String,
 });
 </script>
 
